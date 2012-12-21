a5b547e2d27a246f1d395aebd8550b6ac1cfa321
John Engelman <john.r.engelman@gmail.com>
2012-12-20 18:40:26 -0600

    re-write to something less complicated and works better

---
+++ src/main/groovy/org/gradle/plugins/shadow/ShadowPlugin.groovy
@@ -0,0 +1,41 @@
+package org.gradle.plugins.shadow
+
+import org.gradle.plugins.shadow.tasks.ShadowTask
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPlugin
+
+class ShadowPlugin implements Plugin<Project>{
+
+    static final String SHADOW_WAR_TASK = "slimWar"
+    static final String SHADOW_WAR_TASK_DESC = "Creates war file with all dependencies and classes bundled inside one JAR"
+
+    @Override
+    public void apply(Project project) {
+        project.plugins.apply JavaPlugin
+        addShadow(project)
+        
+//        if(project.plugins.hasPlugin(WarPlugin)){
+//            War slimWar = project.tasks.add(SHADOW_WAR_TASK, War)
+//            slimWar.description = SHADOW_WAR_TASK_DESC
+//            slimWar.group = SHADOW_GROUP
+//            slimWar.dependsOn shadow
+//
+//            slimWar.conventionMapping.map("classpath") {
+//                project.files(fatJar.archivePath) +  project.configurations.runtime.copyRecursive {
+//                    it.ext.has('shadowExclude') && it.ext.get('shadowExclude')
+//                }
+//            }
+//
+//        }
+    }
+
+    void addShadow(Project project) {
+        project.extensions.create(ShadowTaskExtension.NAME, ShadowTaskExtension, project)
+        ShadowTask shadow = project.tasks.add(ShadowTask.NAME, ShadowTask)
+        shadow.description = ShadowTask.DESC
+        shadow.group = ShadowTask.GROUP
+        shadow.dependsOn project.tasks.jar
+
+    }
+}
---
+++ src/main/groovy/org/gradle/plugins/shadow/tasks/ShadowTask.groovy
@@ -0,0 +1,79 @@
+package org.gradle.plugins.shadow.tasks
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.FileTreeElement
+import org.gradle.api.file.RelativePath
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.util.PatternFilterable
+import org.gradle.api.tasks.util.PatternSet
+
+import java.util.jar.JarEntry
+import java.util.jar.JarOutputStream
+
+class ShadowTask extends DefaultTask {
+
+    static final String NAME = "shadow"
+    static final String DESC = "Combines all classpath resources into a single jar"
+    static final String GROUP = "Shadow"
+
+    @OutputFile
+    File outputJar = project.shadow.shadowJar
+
+    @Input
+    List<String> includes = project.shadow.includes
+
+    @Input
+    List<String> excludes = project.shadow.excludes
+
+    @InputFiles
+    List<File> artifacts = project.configurations.getByName("runtime").allArtifacts.files as List
+
+    @TaskAction
+    void shadow() {
+
+        JarOutputStream jos = new JarOutputStream(new FileOutputStream(outputJar))
+
+        List<RelativePath> existingPaths = []
+        jars.each { File jar ->
+            project.zipTree(jar).matching(filter).visit { FileTreeElement jarEntry ->
+                if (!jarEntry.isDirectory() && !existingPaths.contains(jarEntry.relativePath)) {
+                    existingPaths << jarEntry.relativePath
+                    writeJarEntry jos, jarEntry
+                }
+            }
+        }
+        jos.close()
+    }
+
+    PatternFilterable getFilter() {
+        PatternFilterable filter = new PatternSet()
+        filter.includes = includes
+        filter.excludes = excludes
+        filter
+    }
+
+    List<File> getJars() {
+        artifacts + dependencies
+    }
+
+    List<File> getDependencies() {
+        project.configurations.getByName("runtime").resolve() as List
+    }
+
+    static void writeJarEntry(JarOutputStream jos, String path, byte[] bytes) {
+        jos.putNextEntry(new JarEntry(path))
+        jos.write(bytes)
+        jos.closeEntry()
+    }
+
+    static void writeJarEntry(JarOutputStream jos, RelativePath rPath, byte[] bytes) {
+        writeJarEntry(jos, rPath.toString(), bytes)
+    }
+
+    static void writeJarEntry(JarOutputStream jos, FileTreeElement entry) {
+        writeJarEntry jos, entry.relativePath, entry.file.bytes
+    }
+}
