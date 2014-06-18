6ec5b61a9a4bd63151dc9b4873865f71564884c9
John Engelman <john.r.engelman@gmail.com>
2014-06-17 22:06:02 -0500

    add back "knows" task for fun

--- src/main/groovy/com/github/jengelman/gradle/plugins/shadow/ShadowPlugin.groovy
+++ src/main/groovy/com/github/jengelman/gradle/plugins/shadow/ShadowPlugin.groovy
@@ -1,5 +1,6 @@
 package com.github.jengelman.gradle.plugins.shadow
 
+import com.github.jengelman.gradle.plugins.shadow.tasks.KnowsTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.ApplicationPlugin
@@ -16,5 +17,9 @@ class ShadowPlugin implements Plugin<Project> {
         project.plugins.withType(ApplicationPlugin) {
             project.plugins.apply(ShadowApplicationPlugin)
         }
+
+        KnowsTask knows = project.tasks.create(KnowsTask.NAME, KnowsTask)
+        knows.group = ShadowJavaPlugin.SHADOW_GROUP
+        knows.description = KnowsTask.DESC
     }
 }
