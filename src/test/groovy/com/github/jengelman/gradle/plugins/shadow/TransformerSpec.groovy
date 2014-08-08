GradleUp/shadow

commit fdaa35566daec2537573545ddf4cf86710bdf83b

Issue #81: ServiceFileTransformer now supports include/exclude patterns; this required a change to the Transformer interface

src/test/groovy/com/github/jengelman/gradle/plugins/shadow/TransformerSpec.groovy
@@ -15,11 +15,17 @@ class TransformerSpec extends PluginSpecification {
 
     def 'service resource transformer'() {
         given:
-        File one = buildJar('one.jar').insertFile('META-INF/services/org.apache.maven.Shade',
-                'one # NOTE: No newline terminates this line/file').write()
+        File one = buildJar('one.jar')
+                .insertFile('META-INF/services/org.apache.maven.Shade',
+                        'one # NOTE: No newline terminates this line/file')
+                .insertFile('META-INF/services/com.acme.Foo', 'one')
+                .write()
 
-        File two = buildJar('two.jar').insertFile('META-INF/services/org.apache.maven.Shade',
-                'two # NOTE: No newline terminates this line/file').write()
+        File two = buildJar('two.jar')
+                .insertFile('META-INF/services/org.apache.maven.Shade',
+                        'two # NOTE: No newline terminates this line/file')
+                .insertFile('META-INF/services/com.acme.Foo', 'two')
+                .write()
 
         buildFile << """
             |task shadow(type: ${ShadowJar.name}) {
@@ -27,7 +33,9 @@ class TransformerSpec extends PluginSpecification {
             |    baseName = 'shadow'
             |    from('${escapedPath(one)}')
             |    from('${escapedPath(two)}')
-            |    transform(${ServiceFileTransformer.name})
+            |    transform(${ServiceFileTransformer.name}) {
+            |        exclude('META-INF/services/com.acme.*')
+            |    }
             |}
         """.stripMargin()
 
@@ -40,10 +48,15 @@ class TransformerSpec extends PluginSpecification {
         assert output.exists()
 
         and:
-        String text = getJarFileContents(output, 'META-INF/services/org.apache.maven.Shade')
-        assert text.split('(\r\n)|(\r)|(\n)').size() == 2
-        assert text == '''|one # NOTE: No newline terminates this line/file
+        String text1 = getJarFileContents(output, 'META-INF/services/org.apache.maven.Shade')
+        assert text1.split('(\r\n)|(\r)|(\n)').size() == 2
+        assert text1 == '''|one # NOTE: No newline terminates this line/file
                           |two # NOTE: No newline terminates this line/file'''.stripMargin()
+
+        and:
+        String text2 = getJarFileContents(output, 'META-INF/services/com.acme.Foo')
+        assert text2.split('(\r\n)|(\r)|(\n)').size() == 1
+        assert text2 == 'one'
     }
 
     def 'service resource transformer short syntax'() {

src/main/groovy/com/github/jengelman/gradle/plugins/shadow/tasks/ShadowCopyAction.groovy
@@ -173,17 +173,16 @@ public class ShadowCopyAction implements CopyAction {
         private void processArchive(FileCopyDetails fileDetails) {
             stats.startJar()
             ZipFile archive = new ZipFile(fileDetails.file)
-            List<RelativeArchivePath> archivePaths = archive.entries.collect {
-                new RelativeArchivePath(it, fileDetails)
+            List<ArchiveFileTreeElement> archiveElements = archive.entries.collect {
+                new ArchiveFileTreeElement(new RelativeArchivePath(it, fileDetails))
             }
             Spec<FileTreeElement> patternSpec = patternSet.getAsSpec()
-            List<RelativeArchivePath> filteredArchivePaths = archivePaths.findAll { RelativeArchivePath archivePath ->
-                FileTreeElement element = new ArchiveFileTreeElement(archivePath)
-                patternSpec.isSatisfiedBy(element)
+            List<ArchiveFileTreeElement> filteredArchiveElements = archiveElements.findAll { ArchiveFileTreeElement archiveElement ->
+                patternSpec.isSatisfiedBy(archiveElement)
             }
-            filteredArchivePaths.each { RelativeArchivePath relativePath ->
-                if (relativePath.file) {
-                    visitArchiveFile(relativePath, archive)
+            filteredArchiveElements.each { ArchiveFileTreeElement archiveElement ->
+                if (archiveElement.relativePath.file) {
+                    visitArchiveFile(archiveElement, archive)
                 }
             }
             archive.close()
@@ -197,13 +196,14 @@ public class ShadowCopyAction implements CopyAction {
             }
         }
 
-        private void visitArchiveFile(RelativeArchivePath archiveFile, ZipFile archive) {
+        private void visitArchiveFile(ArchiveFileTreeElement archiveFile, ZipFile archive) {
+            def archiveFilePath = archiveFile.relativePath
             if (archiveFile.classFile || !isTransformable(archiveFile)) {
-                if (recordVisit(archiveFile)) {
+                if (recordVisit(archiveFilePath)) {
                     if (!remapper.hasRelocators() || !archiveFile.classFile) {
-                        copyArchiveEntry(archiveFile, archive)
+                        copyArchiveEntry(archiveFilePath, archive)
                     } else {
-                        remapClass(archiveFile, archive)
+                        remapClass(archiveFilePath, archive)
                     }
                 }
             } else {
@@ -289,30 +289,23 @@ public class ShadowCopyAction implements CopyAction {
             }
         }
 
-        private void transform(RelativeArchivePath file, ZipFile archive) {
-            transform(file.pathString, archive.getInputStream(file.entry))
+        private void transform(ArchiveFileTreeElement element, ZipFile archive) {
+            transform(element, archive.getInputStream(element.relativePath.entry))
         }
 
-        private void transform(FileCopyDetails file) {
-            transform(file.path, file.file.newInputStream())
+        private void transform(FileCopyDetails details) {
+            transform(details, details.file.newInputStream())
         }
 
-        private void transform(String path, InputStream is) {
-            String mappedPath = remapper.map(path)
-            transformers.find { it.canTransformResource(path) }.transform(mappedPath, is, relocators)
+        private void transform(FileTreeElement element, InputStream is) {
+            String mappedPath = remapper.map(element.relativePath.pathString)
+            transformers.find { it.canTransformResource(element) }.transform(mappedPath, is, relocators)
         }
 
-        private boolean isTransformable(RelativeArchivePath file) {
-            return isTransformable(file.pathString)
+        private boolean isTransformable(FileTreeElement element) {
+            return transformers.any { it.canTransformResource(element) }
         }
 
-        private boolean isTransformable(FileCopyDetails file) {
-            return isTransformable(file.path)
-        }
-
-        private boolean isTransformable(String path) {
-            return transformers.any { it.canTransformResource(path) }
-        }
     }
 
     class RelativeArchivePath extends RelativePath {
@@ -347,6 +340,10 @@ public class ShadowCopyAction implements CopyAction {
 
         ArchiveFileTreeElement(RelativeArchivePath archivePath) {
             this.archivePath = archivePath
+        }
+
+        boolean isClassFile() {
+            return archivePath.classFile
         }
 
         @Override
@@ -395,7 +392,7 @@ public class ShadowCopyAction implements CopyAction {
         }
 
         @Override
-        RelativePath getRelativePath() {
+        RelativeArchivePath getRelativePath() {
             return archivePath
         }
 

src/main/groovy/com/github/jengelman/gradle/plugins/shadow/transformers/ApacheLicenseResourceTransformer.groovy
@@ -21,6 +21,7 @@ package com.github.jengelman.gradle.plugins.shadow.transformers
 
 import com.github.jengelman.gradle.plugins.shadow.relocation.Relocator
 import org.apache.tools.zip.ZipOutputStream
+import org.gradle.api.file.FileTreeElement
 
 /**
  * Prevents duplicate copies of the license
@@ -36,7 +37,8 @@ class ApacheLicenseResourceTransformer implements Transformer {
 
     private static final String LICENSE_TXT_PATH = "META-INF/LICENSE.txt"
 
-    boolean canTransformResource(String path) {
+    boolean canTransformResource(FileTreeElement element) {
+        def path = element.relativePath.pathString
         return LICENSE_PATH.equalsIgnoreCase(path) ||
                 LICENSE_TXT_PATH.regionMatches(true, 0, path, 0, LICENSE_TXT_PATH.length())
     }

src/main/groovy/com/github/jengelman/gradle/plugins/shadow/transformers/ApacheNoticeResourceTransformer.groovy
@@ -22,6 +22,7 @@ package com.github.jengelman.gradle.plugins.shadow.transformers
 import com.github.jengelman.gradle.plugins.shadow.relocation.Relocator
 import org.apache.tools.zip.ZipEntry
 import org.apache.tools.zip.ZipOutputStream
+import org.gradle.api.file.FileTreeElement
 import org.gradle.mvn3.org.codehaus.plexus.util.StringUtils
 
 import java.text.SimpleDateFormat
@@ -69,7 +70,8 @@ class ApacheNoticeResourceTransformer implements Transformer {
 
     private static final String NOTICE_TXT_PATH = "META-INF/NOTICE.txt"
 
-    boolean canTransformResource(String path) {
+    boolean canTransformResource(FileTreeElement element) {
+        def path = element.relativePath.pathString
         if (NOTICE_PATH.equalsIgnoreCase(path) || NOTICE_TXT_PATH.equalsIgnoreCase(path)) {
             return true
         }

src/main/groovy/com/github/jengelman/gradle/plugins/shadow/transformers/AppendingTransformer.groovy
@@ -22,6 +22,7 @@ package com.github.jengelman.gradle.plugins.shadow.transformers
 import com.github.jengelman.gradle.plugins.shadow.relocation.Relocator
 import org.apache.tools.zip.ZipEntry
 import org.apache.tools.zip.ZipOutputStream
+import org.gradle.api.file.FileTreeElement
 import org.gradle.mvn3.org.codehaus.plexus.util.IOUtil
 
 /**
@@ -37,7 +38,8 @@ class AppendingTransformer implements Transformer {
 
     ByteArrayOutputStream data = new ByteArrayOutputStream()
 
-    boolean canTransformResource(String path) {
+    boolean canTransformResource(FileTreeElement element) {
+        def path = element.relativePath.pathString
         if (resource != null && resource.equalsIgnoreCase(path)) {
             return true
         }

src/main/groovy/com/github/jengelman/gradle/plugins/shadow/transformers/ComponentsXmlResourceTransformer.groovy
@@ -22,6 +22,7 @@ package com.github.jengelman.gradle.plugins.shadow.transformers
 import com.github.jengelman.gradle.plugins.shadow.relocation.Relocator
 import org.apache.tools.zip.ZipEntry
 import org.apache.tools.zip.ZipOutputStream
+import org.gradle.api.file.FileTreeElement
 import org.gradle.mvn3.org.codehaus.plexus.util.IOUtil
 import org.gradle.mvn3.org.codehaus.plexus.util.ReaderFactory
 import org.gradle.mvn3.org.codehaus.plexus.util.WriterFactory
@@ -42,7 +43,8 @@ class ComponentsXmlResourceTransformer implements Transformer {
 
     static final String COMPONENTS_XML_PATH = "META-INF/plexus/components.xml"
 
-    boolean canTransformResource(String path) {
+    boolean canTransformResource(FileTreeElement element) {
+        def path = element.relativePath.pathString
         return COMPONENTS_XML_PATH.equals(path)
     }
 

src/main/groovy/com/github/jengelman/gradle/plugins/shadow/transformers/DontIncludeResourceTransformer.groovy
@@ -21,6 +21,7 @@ package com.github.jengelman.gradle.plugins.shadow.transformers
 
 import com.github.jengelman.gradle.plugins.shadow.relocation.Relocator
 import org.apache.tools.zip.ZipOutputStream
+import org.gradle.api.file.FileTreeElement
 
 /**
  * A resource processor that prevents the inclusion of an arbitrary
@@ -34,7 +35,8 @@ import org.apache.tools.zip.ZipOutputStream
 class DontIncludeResourceTransformer implements Transformer {
     String resource
 
-    boolean canTransformResource(String path) {
+    boolean canTransformResource(FileTreeElement element) {
+        def path = element.relativePath.pathString
         if (path.endsWith(resource)) {
             return true
         }

src/main/groovy/com/github/jengelman/gradle/plugins/shadow/transformers/IncludeResourceTransformer.groovy
@@ -22,6 +22,7 @@ package com.github.jengelman.gradle.plugins.shadow.transformers
 import com.github.jengelman.gradle.plugins.shadow.relocation.Relocator
 import org.apache.tools.zip.ZipEntry
 import org.apache.tools.zip.ZipOutputStream
+import org.gradle.api.file.FileTreeElement
 import org.gradle.mvn3.org.codehaus.plexus.util.IOUtil
 
 /**
@@ -38,7 +39,7 @@ public class IncludeResourceTransformer implements Transformer {
 
     String resource
 
-    public boolean canTransformResource(String path) {
+    public boolean canTransformResource(FileTreeElement element) {
         return false
     }
 

commit 63a530b5807f452e37d53d1745b38e6dc82639eb

Issue #81: Fix behavior of mergeServiceFiles(path) that was broken by fdaa35566daec2537573545ddf4cf86710bdf83b

src/main/groovy/com/github/jengelman/gradle/plugins/shadow/tasks/ShadowJar.groovy
@@ -99,7 +99,7 @@ class ShadowJar extends Jar implements ShadowSpec {
     }
 
     /**
-     * Syntatic sugar for merging service files in JARs
+     * Syntactic sugar for merging service files in JARs
      * @return
      */
     ShadowJar mergeServiceFiles() {
@@ -107,12 +107,12 @@ class ShadowJar extends Jar implements ShadowSpec {
     }
 
     /**
-     * Syntatic sugar for merging service files in JARs
+     * Syntactic sugar for merging service files in JARs
      * @return
      */
     ShadowJar mergeServiceFiles(String rootPath) {
         transform(ServiceFileTransformer) {
-            path = rootPath
+            setIncludes(["${rootPath}/**"])
         }
     }
 

src/test/groovy/com/github/jengelman/gradle/plugins/shadow/TransformerSpec.groovy
@@ -92,6 +92,39 @@ class TransformerSpec extends PluginSpecification {
                           |two # NOTE: No newline terminates this line/file'''.stripMargin()
     }
 
+    def 'service resource transformer alternate path'() {
+        given:
+            File one = buildJar('one.jar').insertFile('META-INF/foo/org.apache.maven.Shade',
+                    'one # NOTE: No newline terminates this line/file').write()
+
+            File two = buildJar('two.jar').insertFile('META-INF/foo/org.apache.maven.Shade',
+                    'two # NOTE: No newline terminates this line/file').write()
+
+            buildFile << """
+            |task shadow(type: ${ShadowJar.name}) {
+            |    destinationDir = new File(buildDir, 'libs')
+            |    baseName = 'shadow'
+            |    from('${escapedPath(one)}')
+            |    from('${escapedPath(two)}')
+            |    mergeServiceFiles('META-INF/foo')
+            |}
+        """.stripMargin()
+
+        when:
+            runner.arguments << 'shadow'
+            ExecutionResult result = runner.run()
+
+        then:
+            success(result)
+            assert output.exists()
+
+        and:
+            String text = getJarFileContents(output, 'META-INF/foo/org.apache.maven.Shade')
+            assert text.split('(\r\n)|(\r)|(\n)').size() == 2
+            assert text == '''|one # NOTE: No newline terminates this line/file
+                          |two # NOTE: No newline terminates this line/file'''.stripMargin()
+    }
+
     @Issue(['SHADOW-70', 'SHADOW-71'])
     def 'apply transformers to project resources'() {
         given: