097ccf994a9f4afc0f30e6cb752c5c57f79332f6
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2018-11-01 10:02:16 -0700

    Added tests for minimize with project exclusions

--- src/test/groovy/com/github/jengelman/gradle/plugins/shadow/ShadowPluginSpec.groovy
+++ src/test/groovy/com/github/jengelman/gradle/plugins/shadow/ShadowPluginSpec.groovy
@@ -319,6 +319,112 @@ class ShadowPluginSpec extends PluginSpecification {
     }
 
     /**
+     * 'Client', 'Server' and 'junit' are independent.
+     * Unused classes of 'client' and theirs dependencies shouldn't be removed.
+     */
+    def 'exclude a project from minimize '() {
+        given:
+        file('settings.gradle') << """
+            include 'client', 'server'
+        """.stripIndent()
+
+        file('client/src/main/java/client/Client.java') << """
+            package client;
+            public class Client {}
+        """.stripIndent()
+
+        file('client/build.gradle') << """
+            apply plugin: 'java'
+            repositories { maven { url "${repo.uri}" } }
+        """.stripIndent()
+
+        file('server/src/main/java/server/Server.java') << """
+            package server;
+            public class Server {}
+        """.stripIndent()
+
+        file('server/build.gradle') << """
+            apply plugin: 'java'
+            apply plugin: 'com.github.johnrengelman.shadow'
+
+            shadowJar {
+                minimize {
+                    exclude(project(':client'))
+                }
+            }
+
+            repositories { maven { url "${repo.uri}" } }
+            dependencies { compile project(':client') }
+        """.stripIndent()
+
+        File serverOutput = file('server/build/libs/server-all.jar')
+
+        when:
+        runner.withArguments(':server:shadowJar', '--stacktrace').withDebug(true).build()
+
+        then:
+        contains(serverOutput, [
+                'client/Client.class',
+                'server/Server.class'
+        ])
+    }
+
+    /**
+     * 'Client', 'Server' and 'junit' are independent.
+     * Unused classes of 'client' and theirs dependencies shouldn't be removed.
+     */
+    def 'exclude a project from minimize - excludes transitive dependencies that are not used in the project dependency'() {
+        given:
+        file('settings.gradle') << """
+            include 'client', 'server'
+        """.stripIndent()
+
+        file('client/src/main/java/client/Client.java') << """
+            package client;
+            public class Client {}
+        """.stripIndent()
+
+        file('client/build.gradle') << """
+            apply plugin: 'java'
+            repositories { maven { url "${repo.uri}" } }
+            dependencies { compile 'junit:junit:3.8.2' }
+        """.stripIndent()
+
+        file('server/src/main/java/server/Server.java') << """
+            package server;
+            public class Server {}
+        """.stripIndent()
+
+        file('server/build.gradle') << """
+            apply plugin: 'java'
+            apply plugin: 'com.github.johnrengelman.shadow'
+
+            shadowJar {
+                minimize {
+                    exclude(project(':client'))
+                }
+            }
+
+            repositories { maven { url "${repo.uri}" } }
+            dependencies { compile project(':client') }
+        """.stripIndent()
+
+        File serverOutput = file('server/build/libs/server-all.jar')
+
+        when:
+        runner.withArguments(':server:shadowJar', '--stacktrace').withDebug(true).build()
+
+        then:
+        contains(serverOutput, [
+                'client/Client.class',
+                'server/Server.class'
+        ])
+        doesNotContain(serverOutput, [ 'junit/framework/Test.class'])
+
+    }
+
+
+    /**
      * 'api' used as api for 'impl', and depended on 'lib'. 'junit' is independent.
      * The minimize shall remove 'junit', but not 'api'.
      * Unused classes of 'api' and theirs dependencies also shouldn't be removed.
