b1d87c23302d62f61d8cf544b2a6a5ede227c6b1
Goooler <wangzongler@gmail.com>
2023-02-27 07:13:06 +0800

    Replace deprecated ConfigureUtil
    https://docs.gradle.org/8.0.1/javadoc/org/gradle/util/ConfigureUtil.html

--- src/main/groovy/com/github/jengelman/gradle/plugins/shadow/tasks/InheritManifest.groovy
+++ src/main/groovy/com/github/jengelman/gradle/plugins/shadow/tasks/InheritManifest.groovy
@@ -1,10 +1,11 @@
 package com.github.jengelman.gradle.plugins.shadow.tasks
 
+import org.gradle.api.Project
 import org.gradle.api.java.archives.Manifest
 
 interface InheritManifest extends Manifest {
 
-    InheritManifest inheritFrom(Object... inheritPaths)
+    InheritManifest inheritFrom(Project project, Object... inheritPaths)
 
-    InheritManifest inheritFrom(Object inheritPaths, Closure closure)
+    InheritManifest inheritFrom(Project project, Object inheritPaths, Closure closure)
 }
