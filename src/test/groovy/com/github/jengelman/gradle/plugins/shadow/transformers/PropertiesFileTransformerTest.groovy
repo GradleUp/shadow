fbcd41ee9707dffa076c4d98dc713c33c50b9e9c
Antonio Gascón <3040982+agascon@users.noreply.github.com>
2023-04-18 15:16:14 +0200

    Add test to validate PropertiesFileTransformer supports reproducible builds

--- src/test/groovy/com/github/jengelman/gradle/plugins/shadow/transformers/PropertiesFileTransformerTest.groovy
+++ src/test/groovy/com/github/jengelman/gradle/plugins/shadow/transformers/PropertiesFileTransformerTest.groovy
@@ -40,21 +40,42 @@ final class PropertiesFileTransformerTest extends TransformerTestSupport {
     void testTransformation() {
         transformer.transform(new TransformerContext(MANIFEST_NAME, getResourceStream(MANIFEST_NAME), Collections.<Relocator>emptyList(), new ShadowStats()))
 
+        def testableZipFile = doTransformAndGetTransformedFile(transformer, false)
+        def targetLines = readFrom(testableZipFile, MANIFEST_NAME)
+
+        assertFalse(targetLines.isEmpty())
+
+        assertTrue(targetLines.contains("Manifest-Version=1.0"))
+    }
+
+    @Test
+    void testTransformationPropertiesAreReproducible() {
+        transformer.transform(new TransformerContext(MANIFEST_NAME, getResourceStream(MANIFEST_NAME), Collections.<Relocator>emptyList(), new ShadowStats()))
+
+        def firstRunTransformedFile = doTransformAndGetTransformedFile(transformer, true)
+        def firstRunTargetLines = readFrom(firstRunTransformedFile, MANIFEST_NAME)
+
+        Thread.sleep(1000) // wait for 1sec to ensure timestamps in properties would change
+
+        def secondRunTransformedFile = doTransformAndGetTransformedFile(transformer, true)
+        def secondRunTargetLines = readFrom(secondRunTransformedFile, MANIFEST_NAME)
+
+        assertEquals(firstRunTargetLines, secondRunTargetLines)
+    }
+
+    static File doTransformAndGetTransformedFile(final PropertiesFileTransformer transformer, final boolean  preserveFileTimestamps) {
         def testableZipFile = File.createTempFile("testable-zip-file-", ".jar")
         def fileOutputStream = new FileOutputStream(testableZipFile)
         def bufferedOutputStream = new BufferedOutputStream(fileOutputStream)
         def zipOutputStream = new ZipOutputStream(bufferedOutputStream)
 
         try {
-            transformer.modifyOutputStream(zipOutputStream, false)
+            transformer.modifyOutputStream(zipOutputStream, preserveFileTimestamps)
         } finally {
             zipOutputStream.close()
         }
-        def targetLines = readFrom(testableZipFile, MANIFEST_NAME)
 
-        assertFalse(targetLines.isEmpty())
-
-        assertTrue(targetLines.contains("Manifest-Version=1.0"))
+        return testableZipFile;
     }
 
     static List<String> readFrom(File jarFile, String resourceName) {
