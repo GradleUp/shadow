6917d4b3868d848e31094ba90072c6d0e625ca7a
Goooler <wangzongler@gmail.com>
2024-09-02 14:54:42 +0800

    Revert "Use named configuration"
    This reverts commit 80ca3e8e0247c290e2bb3fe425440e6f8fb52550.

--- src/main/groovy/com/github/jengelman/gradle/plugins/shadow/ShadowExtension.groovy
+++ src/main/groovy/com/github/jengelman/gradle/plugins/shadow/ShadowExtension.groovy
@@ -15,8 +15,8 @@ class ShadowExtension {
 
     ShadowExtension(Project project) {
         archiveFile = project.provider { project.tasks.withType(ShadowJar).getByName("shadowJar").archiveFile }
-        allDependencies = project.configurations.named("shadow").map { configuration ->
-            configuration.allDependencies.collect {
+        allDependencies = project.provider {
+            project.configurations.getByName("shadow").allDependencies.collect {
                 if ((it instanceof ProjectDependency) || !(it instanceof SelfResolvingDependency)) {
                     new Dep(it.group, it.name, it.version)
                 }
