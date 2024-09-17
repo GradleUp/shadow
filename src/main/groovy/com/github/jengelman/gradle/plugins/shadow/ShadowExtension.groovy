54cb4d13b604d2b450dc5d11e5cd3ee7769fd94e
Goooler <wangzongler@gmail.com>
2024-09-17 20:31:15 +0800

    Still mark ShadowExtension Deprecated

--- src/main/groovy/com/github/jengelman/gradle/plugins/shadow/ShadowExtension.groovy
+++ src/main/groovy/com/github/jengelman/gradle/plugins/shadow/ShadowExtension.groovy
@@ -4,6 +4,7 @@ import org.gradle.api.Project
 import org.gradle.api.component.SoftwareComponentContainer
 import org.gradle.api.publish.maven.MavenPublication
 
+@Deprecated
 class ShadowExtension {
     private final SoftwareComponentContainer components
 
@@ -12,8 +13,7 @@ class ShadowExtension {
     }
 
     /**
-     * @param publication
-     * @deprecated configure publication using component.shadow directly
+     * @deprecated configure publication using component.shadow directly.
      */
     @Deprecated
     void component(MavenPublication publication) {
