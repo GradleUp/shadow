de157614e3ff1a0777bb23539beef3dc3f734aef
Goooler <wangzongler@gmail.com>
2024-09-19 23:28:18 +0800

    Convert CacheableTransformer

---
+++ src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/transformers/CacheableTransformer.kt
@@ -0,0 +1,10 @@
+package com.github.jengelman.gradle.plugins.shadow.transformers
+
+/**
+ * Marks that a given instance of [Transformer] is compatible with the Gradle build cache.
+ * In other words, it has its appropriate inputs annotated so that Gradle can consider them when
+ * determining the cache key.
+ */
+@Retention(AnnotationRetention.RUNTIME)
+@Target
+annotation class CacheableTransformer
