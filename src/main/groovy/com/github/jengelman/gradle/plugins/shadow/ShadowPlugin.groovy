aaacaf24341b9dcafce817d7ffeebbd4d93e97c0
Goooler <wangzongler@gmail.com>
2024-09-19 14:18:54 +0800

    Remove configureBuildScan

--- src/main/groovy/com/github/jengelman/gradle/plugins/shadow/ShadowPlugin.groovy
+++ src/main/groovy/com/github/jengelman/gradle/plugins/shadow/ShadowPlugin.groovy
@@ -1,7 +1,6 @@
 package com.github.jengelman.gradle.plugins.shadow
 
 import com.github.jengelman.gradle.plugins.shadow.legacy.LegacyShadowPlugin
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.ApplicationPlugin
@@ -19,28 +18,6 @@ class ShadowPlugin implements Plugin<Project> {
             }
             plugins.withType(ApplicationPlugin) {
                 plugins.apply(ShadowApplicationPlugin)
-            }
-
-            // Legacy build scan support for Gradle Enterprise, users should migrate to develocity plugin.
-            rootProject.plugins.withId('com.gradle.enterprise') {
-                configureBuildScan(rootProject)
-            }
-            rootProject.plugins.withId('com.gradle.develocity') {
-                configureBuildScan(rootProject)
-            }
-        }
-    }
-
-    private void configureBuildScan(Project rootProject) {
-        rootProject.buildScan.buildFinished {
-            def shadowTasks = tasks.withType(ShadowJar)
-            shadowTasks.each { task ->
-                if (task.didWork) {
-                    task.stats.buildScanData.each { k, v ->
-                        rootProject.buildScan.value "shadow.${task.path}.${k}", v.toString()
-                    }
-                    rootProject.buildScan.value "shadow.${task.path}.configurations", task.configurations*.name.join(", ")
-                }
             }
         }
     }
