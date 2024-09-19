dd9826753a55e31dba5f5216106c95d7d24eee20
Goooler <wangzongler@gmail.com>
2024-09-19 14:21:35 +0800

    Convert ShadowPlugin

---
+++ src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/ShadowPlugin.kt
@@ -0,0 +1,23 @@
+package com.github.jengelman.gradle.plugins.shadow
+
+import com.github.jengelman.gradle.plugins.shadow.legacy.LegacyShadowPlugin
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.plugins.ApplicationPlugin
+import org.gradle.api.plugins.JavaPlugin
+
+class ShadowPlugin : Plugin<Project> {
+
+  override fun apply(project: Project) {
+    with(project.plugins) {
+      apply(ShadowBasePlugin::class.java)
+      apply(LegacyShadowPlugin::class.java)
+      withType(JavaPlugin::class.java) {
+        apply(ShadowJavaPlugin::class.java)
+      }
+      withType(ApplicationPlugin::class.java) {
+        apply(ShadowApplicationPlugin::class.java)
+      }
+    }
+  }
+}
