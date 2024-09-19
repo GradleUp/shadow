0a94723be15d54b3c11f5c6cd334f601736ce22d
Goooler <wangzongler@gmail.com>
2024-09-19 15:00:10 +0800

    Convert DefaultDependencyFilter

---
+++ src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/internal/DefaultDependencyFilter.kt
@@ -0,0 +1,18 @@
+package com.github.jengelman.gradle.plugins.shadow.internal
+
+import org.gradle.api.Project
+import org.gradle.api.artifacts.ResolvedDependency
+
+internal class DefaultDependencyFilter(project: Project) : AbstractDependencyFilter(project) {
+  override fun resolve(
+    dependencies: MutableSet<ResolvedDependency>,
+    includedDependencies: MutableSet<ResolvedDependency>,
+    excludedDependencies: MutableSet<ResolvedDependency>,
+  ) {
+    dependencies.forEach {
+      if (if (isIncluded(it)) includedDependencies.add(it) else excludedDependencies.add(it)) {
+        resolve(it.children, includedDependencies, excludedDependencies)
+      }
+    }
+  }
+}
