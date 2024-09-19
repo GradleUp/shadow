6a392c83ca9e7b804cf9df67e2633a5c7782ea76
Goooler <wangzongler@gmail.com>
2024-09-19 12:54:15 +0800

    Declare compilerOptions.jvmTarget

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
   kotlin("jvm") version "2.0.20"
   groovy
@@ -10,6 +12,10 @@ plugins {
 java {
   sourceCompatibility = JavaVersion.VERSION_1_8
   targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+kotlin {
+  compilerOptions.jvmTarget = JvmTarget.JVM_1_8
 }
 
 spotless {
