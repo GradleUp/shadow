dcdf56126da908f511e028ffef0701a586322404
Goooler <wangzongler@gmail.com>
2024-09-19 12:57:40 +0800

    Rename params to action

--- src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/tasks/ShadowSpec.kt
+++ src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/tasks/ShadowSpec.kt
@@ -13,9 +13,9 @@ import org.gradle.api.file.CopySpec
 internal interface ShadowSpec : CopySpec {
   fun minimize(): ShadowSpec
 
-  fun minimize(configureClosure: Action<DependencyFilter>): ShadowSpec
+  fun minimize(action: Action<DependencyFilter>): ShadowSpec
 
-  fun dependencies(configure: Action<DependencyFilter>): ShadowSpec
+  fun dependencies(action: Action<DependencyFilter>): ShadowSpec
 
   @Throws(
     InstantiationException::class,
@@ -31,7 +31,7 @@ internal interface ShadowSpec : CopySpec {
     NoSuchMethodException::class,
     InvocationTargetException::class,
   )
-  fun <T : Transformer> transform(clazz: Class<T>, configure: Action<T>): ShadowSpec
+  fun <T : Transformer> transform(clazz: Class<T>, action: Action<T>): ShadowSpec
 
   fun transform(transformer: Transformer): ShadowSpec
 
@@ -39,7 +39,7 @@ internal interface ShadowSpec : CopySpec {
 
   fun mergeServiceFiles(rootPath: String): ShadowSpec
 
-  fun mergeServiceFiles(configureClosure: Action<ServiceFileTransformer>): ShadowSpec
+  fun mergeServiceFiles(action: Action<ServiceFileTransformer>): ShadowSpec
 
   fun mergeGroovyExtensionModules(): ShadowSpec
 
@@ -47,7 +47,7 @@ internal interface ShadowSpec : CopySpec {
 
   fun relocate(pattern: String, destination: String): ShadowSpec
 
-  fun relocate(pattern: String, destination: String, configure: Action<SimpleRelocator>): ShadowSpec
+  fun relocate(pattern: String, destination: String, action: Action<SimpleRelocator>): ShadowSpec
 
   fun relocate(relocator: Relocator): ShadowSpec
 
@@ -65,7 +65,7 @@ internal interface ShadowSpec : CopySpec {
     NoSuchMethodException::class,
     InvocationTargetException::class,
   )
-  fun <R : Relocator> relocate(clazz: Class<R>, configure: Action<R>): ShadowSpec
+  fun <R : Relocator> relocate(clazz: Class<R>, action: Action<R>): ShadowSpec
 
   val stats: ShadowStats
 }
