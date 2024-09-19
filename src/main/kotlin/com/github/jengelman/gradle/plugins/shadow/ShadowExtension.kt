aac5e879b6063ee049e15117bd17c512bb5f7445
Goooler <wangzongler@gmail.com>
2024-09-19 14:12:34 +0800

    Convert ShadowExtension

---
+++ src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/ShadowExtension.kt
@@ -0,0 +1,14 @@
+package com.github.jengelman.gradle.plugins.shadow
+
+import org.gradle.api.Project
+import org.gradle.api.publish.maven.MavenPublication
+
+@Deprecated("This is deprecated from 8.3.2")
+abstract class ShadowExtension(project: Project) {
+  private val components = project.components
+
+  @Deprecated("configure publication using component.shadow directly.")
+  fun component(publication: MavenPublication) {
+    publication.from(components.findByName("shadow"))
+  }
+}
