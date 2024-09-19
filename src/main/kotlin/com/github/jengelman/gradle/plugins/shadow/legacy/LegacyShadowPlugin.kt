41d78035e0963f1399431b2677a9b307d1e05e99
Goooler <wangzongler@gmail.com>
2024-09-19 13:59:57 +0800

    Convert LegacyShadowPlugin

--- src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/legacy/LegacyShadowPlugin.kt
+++ src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/legacy/LegacyShadowPlugin.kt
@@ -4,13 +4,10 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 /**
- * Empty plugin to still have the com.github.johnrengelman.shadow plugin applied.
+ * Empty plugin to still have the `com.github.johnrengelman.shadow` plugin applied.
  *
  * This allows older build logic to keep on working as if that old plugin ID was applied.
  */
-class LegacyShadowPlugin implements Plugin<Project> {
-    @Override
-    void apply(Project target) {
-        // Do nothing
-    }
+class LegacyShadowPlugin : Plugin<Project> {
+  override fun apply(project: Project) = Unit
 }
