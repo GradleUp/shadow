6728b96fb10e269f6e09398c3981421f0ecdeb5e
Goooler <wangzongler@gmail.com>
2024-09-19 15:15:14 +0800

    Convert ZipCompressor

---
+++ src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/internal/ZipCompressor.kt
@@ -0,0 +1,5 @@
+package com.github.jengelman.gradle.plugins.shadow.internal
+
+import org.gradle.api.internal.file.archive.compression.ArchiveOutputStreamFactory
+
+internal interface ZipCompressor : ArchiveOutputStreamFactory
