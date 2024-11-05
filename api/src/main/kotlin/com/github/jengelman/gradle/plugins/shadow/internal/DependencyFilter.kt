e146807f28e6bedc547152a7e5b2bd55c61a383b
Goooler <wangzongler@gmail.com>
2024-11-05 13:19:02 +0800

    Convert DependencyFilter

---
+++ api/src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/internal/DependencyFilter.kt
@@ -0,0 +1,54 @@
+package com.github.jengelman.gradle.plugins.shadow.internal
+
+import groovy.lang.Closure
+import org.gradle.api.artifacts.Dependency
+import org.gradle.api.artifacts.ResolvedDependency
+import org.gradle.api.file.FileCollection
+import org.gradle.api.specs.Spec
+
+public interface DependencyFilter {
+  /**
+   * Resolve a FileCollection against the include/exclude rules in the filter.
+   */
+  public fun resolve(configuration: FileCollection): FileCollection
+
+  /**
+   * Resolve all FileCollections against the include/exclude ruels in the filter and combine the results.
+   */
+  public fun resolve(configurations: MutableCollection<FileCollection>): FileCollection
+
+  /**
+   * Exclude dependencies that match the provided spec.
+   */
+  public fun exclude(spec: Spec<in ResolvedDependency>): DependencyFilter
+
+  /**
+   * Include dependencies that match the provided spec.
+   */
+  public fun include(spec: Spec<in ResolvedDependency>): DependencyFilter
+
+  /**
+   * Create a spec that matches the provided project notation on group, name, and version.
+   */
+  public fun project(notation: Map<String, *>): Spec<in ResolvedDependency>
+
+  /**
+   * Create a spec that matches the default configuration for the provided project path on group, name, and version.
+   */
+  public fun project(notation: String): Spec<in ResolvedDependency>
+
+  /**
+   * Create a spec that matches dependencies using the provided notation on group, name, and version.
+   */
+  public fun dependency(notation: Any): Spec<in ResolvedDependency>
+
+  /**
+   * Create a spec that matches the provided dependency on group, name, and version.
+   */
+  public fun dependency(dependency: Dependency): Spec<in ResolvedDependency>
+
+  /**
+   * Create a spec that matches the provided closure.
+   */
+  public fun dependency(spec: Closure<*>): Spec<in ResolvedDependency>
+}
