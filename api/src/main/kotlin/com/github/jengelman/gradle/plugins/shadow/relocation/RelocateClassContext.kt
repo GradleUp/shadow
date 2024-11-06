23c2c652c40e726d1470ca235f0c68df69c57de9
Goooler <wangzongler@gmail.com>
2024-11-06 16:18:28 +0800

    Convert RelocateClassContext

---
+++ api/src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/relocation/RelocateClassContext.kt
@@ -0,0 +1,22 @@
+package com.github.jengelman.gradle.plugins.shadow.relocation
+
+import com.github.jengelman.gradle.plugins.shadow.ShadowStats
+
+public data class RelocateClassContext(
+  val className: String,
+  val stats: ShadowStats,
+) {
+  public class Builder {
+    private var className: String = ""
+    private var stats: ShadowStats = ShadowStats()
+
+    public fun className(className: String): Builder = apply { this.className = className }
+    public fun stats(stats: ShadowStats): Builder = apply { this.stats = stats }
+    public fun build(): RelocateClassContext = RelocateClassContext(className, stats)
+  }
+
+  public companion object {
+    @JvmStatic
+    public fun builder(): Builder = Builder()
+  }
+}
