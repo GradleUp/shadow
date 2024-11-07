74e6e6be4ffb5e1f022ecd519d91a1c3f22d565f
Goooler <wangzongler@gmail.com>
2024-11-07 14:52:16 +0800

    Convert MinimizeDependencyFilter

---
+++ api/src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/internal/MinimizeDependencyFilter.kt
@@ -0,0 +1,30 @@
+package com.github.jengelman.gradle.plugins.shadow.internal
+
+import org.gradle.api.Project
+import org.gradle.api.artifacts.ResolvedDependency
+
+public class MinimizeDependencyFilter(
+  project: Project,
+) : AbstractDependencyFilter(project) {
+  override fun resolve(
+    dependencies: Set<ResolvedDependency>,
+    includedDependencies: MutableSet<ResolvedDependency>,
+    excludedDependencies: MutableSet<ResolvedDependency>,
+  ) {
+    dependencies.forEach {
+      if (it.isIncluded() && !isParentExcluded(excludedDependencies, it)) {
+        includedDependencies.add(it)
+      } else {
+        excludedDependencies.add(it)
+      }
+      resolve(it.children, includedDependencies, excludedDependencies)
+    }
+  }
+
+  private fun isParentExcluded(
+    excludedDependencies: Set<ResolvedDependency>,
+    dependency: ResolvedDependency,
+  ): Boolean {
+    return excludedDependencies.any { it in dependency.parents }
+  }
+}
