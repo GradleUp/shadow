c9af800c00d3927fe24969927f70f5700860d9b4
Goooler <wangzongler@gmail.com>
2024-11-07 15:05:04 +0800

    Convert UnusedTracker

---
+++ api/src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/internal/UnusedTracker.kt
@@ -0,0 +1,91 @@
+package com.github.jengelman.gradle.plugins.shadow.internal
+
+import java.io.File
+import org.gradle.api.Project
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.artifacts.Dependency
+import org.gradle.api.artifacts.ProjectDependency
+import org.gradle.api.artifacts.SelfResolvingDependency
+import org.gradle.api.file.FileCollection
+import org.gradle.api.tasks.InputFiles
+import org.vafer.jdependency.Clazzpath
+import org.vafer.jdependency.ClazzpathUnit
+
+/** Tracks unused classes in the project classpath. */
+public class UnusedTracker private constructor(
+  classDirs: Iterable<File>,
+  classJars: FileCollection,
+  private val _toMinimize: FileCollection
+) {
+  private val projectUnits: List<ClazzpathUnit>
+  private val cp = Clazzpath()
+
+  init {
+    projectUnits = classDirs.map { cp.addClazzpathUnit(it) } + classJars.map { cp.addClazzpathUnit(it) }
+  }
+
+  @get:InputFiles
+  public val toMinimize: FileCollection get() = _toMinimize
+
+  public fun findUnused(): Set<String> {
+    val unused = cp.clazzes.toMutableSet()
+    for (cpu in projectUnits) {
+      unused.removeAll(cpu.clazzes)
+      unused.removeAll(cpu.transitiveDependencies)
+    }
+    return unused.map { it.name }.toSet()
+  }
+
+  public fun addDependency(jarOrDir: File) {
+    if (_toMinimize.contains(jarOrDir)) {
+      cp.addClazzpathUnit(jarOrDir)
+    }
+  }
+
+  public companion object {
+    @JvmStatic
+    public fun forProject(
+      apiJars: FileCollection,
+      sourceSetsClassesDirs: Iterable<File>,
+      toMinimize: FileCollection
+    ): UnusedTracker {
+      return UnusedTracker(sourceSetsClassesDirs, apiJars, toMinimize)
+    }
+
+    @JvmStatic
+    public fun getApiJarsFromProject(project: Project): FileCollection {
+      val apiDependencies = project.configurations.findByName("api")?.dependencies
+        ?: return project.files()
+      val runtimeConfiguration = project.configurations.findByName("runtimeClasspath")
+        ?: project.configurations.getByName("runtime")
+      val apiJars = mutableListOf<File>()
+      apiDependencies.forEach { dep ->
+        when (dep) {
+          is ProjectDependency -> {
+            apiJars.addAll(getApiJarsFromProject(dep.dependencyProject))
+            addJar(runtimeConfiguration, dep, apiJars)
+          }
+          is SelfResolvingDependency -> {
+            apiJars.addAll(dep.resolve())
+          }
+          else -> {
+            addJar(runtimeConfiguration, dep, apiJars)
+            apiJars.add(runtimeConfiguration.find { it.name.startsWith("${dep.name}-") } as File)
+          }
+        }
+      }
+      return project.files(apiJars)
+    }
+
+    private fun addJar(config: Configuration, dep: Dependency, result: MutableList<File>) {
+      config.find { isProjectDependencyFile(it, dep) }?.let { result.add(it) }
+    }
+
+    private fun isProjectDependencyFile(file: File, dep: Dependency): Boolean {
+      val fileName = file.name
+      val dependencyName = dep.name
+      return fileName == "$dependencyName.jar" ||
+        (fileName.startsWith("$dependencyName-") && fileName.endsWith(".jar"))
+    }
+  }
+}
