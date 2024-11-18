3ee9d6545e21505a4f4737917cd60cc83b4972c5
Goooler <wangzongler@gmail.com>
2024-11-18 20:42:46 +0800

    Convert DontIncludeResourceTransformer

---
+++ api/src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/transformers/DontIncludeResourceTransformer.kt
@@ -0,0 +1,23 @@
+package com.github.jengelman.gradle.plugins.shadow.transformers
+
+import org.gradle.api.file.FileTreeElement
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Optional
+
+/**
+ * A resource processor that prevents the inclusion of an arbitrary resource into the shaded JAR.
+ *
+ * Modified from `org.apache.maven.plugins.shade.resource.DontIncludeResourceTransformer.java`
+ *
+ * @author John Engelman
+ */
+open class DontIncludeResourceTransformer : Transformer by NoOpTransformer {
+    @get:Optional
+    @get:Input
+    var resource: String? = null
+
+    override fun canTransformResource(element: FileTreeElement): Boolean {
+        val path = element.relativePath.pathString
+        return !resource.isNullOrEmpty() && path.endsWith(resource!!)
+    }
+}
