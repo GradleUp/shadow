48a348283822b3241136d220195348bf9ed4da5e
Zongle Wang <wangzongler@gmail.com>
2024-11-20 08:39:01 -0500

    Kotlin migration part 6 (#1024)
    * Convert GroovyExtensionModuleTransformer
    
    * Convert Log4j2PluginsCacheFileTransformer
    
    * Simplify relocatePlugins
    
    * Convert SimpleRelocator
    
    * Simplify SimpleRelocator

---
+++ api/src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/transformers/GroovyExtensionModuleTransformer.kt
@@ -0,0 +1,106 @@
+package com.github.jengelman.gradle.plugins.shadow.transformers
+
+import com.github.jengelman.gradle.plugins.shadow.transformers.TransformerContext.Companion.getEntryTimestamp
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
+import java.util.Properties
+import org.apache.tools.zip.ZipEntry
+import org.apache.tools.zip.ZipOutputStream
+import org.gradle.api.file.FileTreeElement
+
+/**
+ * Modified from `eu.appsatori.gradle.fatjar.tasks.PrepareFiles.groovy`
+ *
+ * Resource transformer that merges Groovy extension module descriptor files into a single file.
+ * Groovy extension module descriptor files have the name org.codehaus.groovy.runtime.ExtensionModule
+ * and live in the META-INF/services (Groovy up to 2.4) or META-INF/groovy (Groovy 2.5+) directory.
+ * See [GROOVY-8480](https://issues.apache.org/jira/browse/GROOVY-8480) for more details of the change.
+ *
+ * If there are several descriptor files spread across many JARs the individual
+ * entries will be merged into a single descriptor file which will be
+ * packaged into the resultant JAR produced by the shadowing process.
+ * It will live in the legacy directory (META-INF/services) if all the processed descriptor
+ * files came from the legacy location, otherwise it will be written into the now standard location (META-INF/groovy).
+ * Note that certain JDK9+ tooling will break when using the legacy location.
+ */
+@CacheableTransformer
+open class GroovyExtensionModuleTransformer : Transformer {
+    private val module = Properties()
+
+    /**
+     * default to Groovy 2.4 or earlier
+     */
+    private var legacy = true
+
+    override fun canTransformResource(element: FileTreeElement): Boolean {
+        val path = element.relativePath.pathString
+        if (path == GROOVY_EXTENSION_MODULE_DESCRIPTOR_PATH) {
+            // Groovy 2.5+
+            legacy = false
+            return true
+        }
+        return path == GROOVY_LEGACY_EXTENSION_MODULE_DESCRIPTOR_PATH
+    }
+
+    override fun transform(context: TransformerContext) {
+        val props = Properties()
+        props.load(context.inputStream)
+        props.forEach { key, value ->
+            when (key as String) {
+                MODULE_NAME_KEY -> handle(key, value as String) {
+                    module.setProperty(key, MERGED_MODULE_NAME)
+                }
+                MODULE_VERSION_KEY -> handle(key, value as String) {
+                    module.setProperty(key, MERGED_MODULE_VERSION)
+                }
+                EXTENSION_CLASSES_KEY,
+                STATIC_EXTENSION_CLASSES_KEY,
+                -> handle(key, value as String) { existingValue ->
+                    module.setProperty(key, "$existingValue,$value")
+                }
+            }
+        }
+    }
+
+    private fun handle(key: String, value: String, mergeValue: (String) -> Unit) {
+        val existingValue = module.getProperty(key)
+        if (existingValue != null) {
+            mergeValue(existingValue)
+        } else {
+            module.setProperty(key, value)
+        }
+    }
+
+    override fun hasTransformedResource(): Boolean = module.isNotEmpty()
+
+    override fun modifyOutputStream(os: ZipOutputStream, preserveFileTimestamps: Boolean) {
+        val name = if (legacy) GROOVY_LEGACY_EXTENSION_MODULE_DESCRIPTOR_PATH else GROOVY_EXTENSION_MODULE_DESCRIPTOR_PATH
+        val entry = ZipEntry(name)
+        entry.time = getEntryTimestamp(preserveFileTimestamps, entry.time)
+        os.putNextEntry(entry)
+        module.inputStream().use {
+            it.copyTo(os)
+        }
+        os.closeEntry()
+    }
+
+    private companion object {
+        private fun Properties.inputStream(): InputStream {
+            val os = ByteArrayOutputStream()
+            store(os, null)
+            return ByteArrayInputStream(os.toByteArray())
+        }
+
+        private const val GROOVY_LEGACY_EXTENSION_MODULE_DESCRIPTOR_PATH =
+            "META-INF/services/org.codehaus.groovy.runtime.ExtensionModule"
+        private const val GROOVY_EXTENSION_MODULE_DESCRIPTOR_PATH =
+            "META-INF/groovy/org.codehaus.groovy.runtime.ExtensionModule"
+        private const val MODULE_NAME_KEY = "moduleName"
+        private const val MODULE_VERSION_KEY = "moduleVersion"
+        private const val EXTENSION_CLASSES_KEY = "extensionClasses"
+        private const val STATIC_EXTENSION_CLASSES_KEY = "staticExtensionClasses"
+        private const val MERGED_MODULE_NAME = "MergedByShadowJar"
+        private const val MERGED_MODULE_VERSION = "1.0.0"
+    }
+}
---
+++ api/src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/transformers/Log4j2PluginsCacheFileTransformer.kt
@@ -0,0 +1,90 @@
+package com.github.jengelman.gradle.plugins.shadow.transformers
+
+import com.github.jengelman.gradle.plugins.shadow.ShadowStats
+import com.github.jengelman.gradle.plugins.shadow.relocation.RelocateClassContext
+import com.github.jengelman.gradle.plugins.shadow.relocation.Relocator
+import com.github.jengelman.gradle.plugins.shadow.transformers.TransformerContext.Companion.getEntryTimestamp
+import java.io.File
+import java.net.URL
+import java.util.Collections
+import java.util.Enumeration
+import org.apache.commons.io.output.CloseShieldOutputStream
+import org.apache.logging.log4j.core.config.plugins.processor.PluginCache
+import org.apache.logging.log4j.core.config.plugins.processor.PluginProcessor
+import org.apache.tools.zip.ZipEntry
+import org.apache.tools.zip.ZipOutputStream
+import org.gradle.api.file.FileTreeElement
+
+/**
+ * Modified from the maven equivalent to work with gradle
+ *
+ * @author Paul Nelson Baker
+ * @see [LinkedIn](https://www.linkedin.com/in/paul-n-baker/)
+ * @see [GitHub](https://github.com/paul-nelson-baker/)
+ * @see [PluginsCacheFileTransformer.java](https://github.com/edwgiz/maven-shaded-log4j-transformer/blob/master/src/main/java/com/github/edwgiz/mavenShadePlugin/log4j2CacheTransformer/PluginsCacheFileTransformer.java)
+ */
+@CacheableTransformer
+open class Log4j2PluginsCacheFileTransformer : Transformer {
+    private val temporaryFiles = mutableListOf<File>()
+    private val relocators = mutableListOf<Relocator>()
+    private var stats: ShadowStats? = null
+
+    override fun canTransformResource(element: FileTreeElement): Boolean {
+        return PluginProcessor.PLUGIN_CACHE_FILE == element.name
+    }
+
+    override fun transform(context: TransformerContext) {
+        val temporaryFile = File.createTempFile("Log4j2Plugins", ".dat")
+        temporaryFile.deleteOnExit()
+        temporaryFiles.add(temporaryFile)
+        val fos = temporaryFile.outputStream()
+        context.inputStream.use {
+            it.copyTo(fos)
+        }
+
+        relocators.addAll(context.relocators)
+
+        if (stats == null) {
+            stats = context.stats
+        }
+    }
+
+    override fun hasTransformedResource(): Boolean {
+        // This functionality matches the original plugin, however, I'm not clear what
+        // the exact logic is. From what I can tell temporaryFiles should be never be empty
+        // if anything has been performed.
+        val hasTransformedMultipleFiles = temporaryFiles.size > 1
+        val hasAtLeastOneFileAndRelocator = temporaryFiles.isNotEmpty() && relocators.isNotEmpty()
+        return hasTransformedMultipleFiles || hasAtLeastOneFileAndRelocator
+    }
+
+    override fun modifyOutputStream(os: ZipOutputStream, preserveFileTimestamps: Boolean) {
+        val pluginCache = PluginCache()
+        pluginCache.loadCacheFiles(urlEnumeration)
+        relocatePlugins(pluginCache)
+        val entry = ZipEntry(PluginProcessor.PLUGIN_CACHE_FILE)
+        entry.time = getEntryTimestamp(preserveFileTimestamps, entry.time)
+        os.putNextEntry(entry)
+        pluginCache.writeCache(CloseShieldOutputStream.wrap(os))
+        temporaryFiles.clear()
+    }
+
+    private fun relocatePlugins(pluginCache: PluginCache) {
+        pluginCache.allCategories.values.forEach { currentMap ->
+            currentMap.values.forEach { currentPluginEntry ->
+                val className = currentPluginEntry.className
+                val relocateClassContext = RelocateClassContext(className, requireNotNull(stats))
+                relocators.firstOrNull { it.canRelocateClass(className) }?.let { relocator ->
+                    // Then we perform that relocation and update the plugin entry to reflect the new value.
+                    currentPluginEntry.className = relocator.relocateClass(relocateClassContext)
+                }
+            }
+        }
+    }
+
+    private val urlEnumeration: Enumeration<URL>
+        get() {
+            val urls = temporaryFiles.map { it.toURI().toURL() }
+            return Collections.enumeration(urls)
+        }
+}
