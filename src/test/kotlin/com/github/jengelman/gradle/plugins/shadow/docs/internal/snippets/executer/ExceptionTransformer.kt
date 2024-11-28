b18ee2f39c725b7e95a9c7ca7c5685c864457dd1
Goooler <wangzongler@gmail.com>
2024-11-28 15:13:07 +0800

    Migrate ExceptionTransformer

---
+++ src/test/kotlin/com/github/jengelman/gradle/plugins/shadow/docs/internal/snippets/executer/ExceptionTransformer.kt
@@ -0,0 +1,26 @@
+package com.github.jengelman.gradle.plugins.shadow.docs.internal.snippets.executer
+
+class ExceptionTransformer(
+  private val sourceClassName: String,
+  private val sourceFileName: String,
+  private val lineNumber: Int,
+) {
+  fun transform(throwable: Throwable, offset: Int): Throwable {
+    var errorLine = 0
+    if (throwable is CompileException) {
+      errorLine = throwable.lineNo
+    } else {
+      val frame = throwable.stackTrace.find { it.fileName == sourceClassName }
+        ?: throwable.stackTrace.find { it.fileName == "Example.java" }
+      if (frame != null) {
+        errorLine = frame.lineNumber
+      }
+    }
+    errorLine -= offset
+    throwable.stackTrace = buildList {
+      add(StackTraceElement(sourceClassName, "javadoc", sourceFileName, lineNumber + errorLine))
+      addAll(throwable.stackTrace)
+    }.toTypedArray()
+    return throwable
+  }
+}
