1aa9c3c77629e05115fdc0d6db157a9929224e6f
Goooler <wangzongler@gmail.com>
2024-11-29 11:38:29 +0800

    Remove CompileException

--- src/integrationTest/kotlin/com/github/jengelman/gradle/plugins/shadow/doc/exception/ExceptionTransformer.kt
+++ src/integrationTest/kotlin/com/github/jengelman/gradle/plugins/shadow/doc/exception/ExceptionTransformer.kt
@@ -7,14 +7,10 @@ class ExceptionTransformer(
 ) {
   fun transform(throwable: Throwable, offset: Int): Throwable {
     var errorLine = 0
-    if (throwable is CompileException) {
-      errorLine = throwable.lineNo
-    } else {
-      val frame = throwable.stackTrace.find { it.fileName == sourceClassName }
-        ?: throwable.stackTrace.find { it.fileName == "Example.java" }
-      if (frame != null) {
-        errorLine = frame.lineNumber
-      }
+    val frame = throwable.stackTrace.find { it.fileName == sourceClassName }
+      ?: throwable.stackTrace.find { it.fileName == "Example.java" }
+    if (frame != null) {
+      errorLine = frame.lineNumber
     }
     errorLine -= offset
     throwable.stackTrace = buildList {
