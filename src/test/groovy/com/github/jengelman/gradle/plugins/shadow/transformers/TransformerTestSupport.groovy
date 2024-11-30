acb1c0e69b1d724c7724aa8a670257ed21d17009
Goooler <wangzongler@gmail.com>
2024-11-30 16:54:35 +0800

    Reformat unit test sources for less diffs

--- src/test/groovy/com/github/jengelman/gradle/plugins/shadow/transformers/TransformerTestSupport.groovy
+++ src/test/groovy/com/github/jengelman/gradle/plugins/shadow/transformers/TransformerTestSupport.groovy
@@ -6,10 +6,10 @@ import org.gradle.api.internal.file.DefaultFileTreeElement
 import org.gradle.testfixtures.ProjectBuilder
 
 abstract class TransformerTestSupport<T extends Transformer> {
-    protected static T transformer
-    protected static final def objectFactory = ProjectBuilder.builder().build().objects
+  protected static T transformer
+  protected static final def objectFactory = ProjectBuilder.builder().build().objects
 
-    protected static FileTreeElement getFileElement(String path) {
-        return new DefaultFileTreeElement(null, RelativePath.parse(true, path), null, null)
-    }
+  protected static FileTreeElement getFileElement(String path) {
+    return new DefaultFileTreeElement(null, RelativePath.parse(true, path), null, null)
+  }
 }
