6d03a5dcde798613db3ade048443633ccb3369fb
Zongle Wang <wangzongler@gmail.com>
2024-11-29 19:07:23 -0500

    Add integrationTest task and migrate doc tests to Kotlin (#1055)
    * Rearrange
    
    * Migrate SnippetExecutor
    
    * Migrate NoopExecutor
    
    * Migrate CompileException
    
    * Migrate GradleBuildExecutor
    
    * Migrate ManualSnippetExtractor
    
    * Migrate GroovyDslFixture
    
    * Migrate GroovyScriptFixture and SnippetFixture
    
    * Migrate TestCodeSnippet
    
    * Migrate ExceptionTransformer
    
    * Migrate ManualCodeSnippetTests
    
    * Fix typo in package
    
    * Add integrationTest task and move sources
    
    * Remove guava and tweak deps
    
    * Fix and cleanups
    
    * Split packages
    
    * Fix and remove outdated logic
    
    * Remove CompileException
    
    * Use createTempDirectory
    
    * Rename ManualCodeSnippetTests
    
    * Optimize relativeDocPath
    
    * Replace arguments
    
    * Fix
    
    * Add an unknown property for testing
    
    * Better stacktrace transforming in ExceptionTransformer
    
    * Simplify SnippetExecutor
    
    * Rename classes
    
    * Remove ExceptionTransformer
    
    * Improve error line message
    
    * Revert "Add an unknown property for testing"
    
    This reverts commit f722bb7086ee56b58d7ca335fb9289e8c6cc30d9.
    
    * Extends testImplementation and testRuntimeOnly
    
    * Remove unused junit-platform-suite-engine
    
    * Mark more blocks no-run
    
    * MarK GroovyDslFixture as an object
    
    * Simplify SnippetFixture
    
    * Rearrange packages
    
    * Rename

---
+++ src/integrationTest/kotlin/com/github/jengelman/gradle/plugins/shadow/doc/executor/GroovyBuildExecutor.kt
@@ -0,0 +1,63 @@
+package com.github.jengelman.gradle.plugins.shadow.doc.executor
+
+import com.github.jengelman.gradle.plugins.shadow.doc.fixture.SnippetFixture
+import java.nio.file.Path
+import kotlin.io.path.createDirectory
+import kotlin.io.path.writeText
+import org.gradle.testkit.runner.GradleRunner
+import org.intellij.lang.annotations.Language
+
+class GroovyBuildExecutor(
+  override val fixture: SnippetFixture,
+  private val importExtractor: (String) -> List<String>,
+  private val arguments: Array<String> = arrayOf("build", "-m"),
+) : SnippetExecutor {
+
+  override fun execute(tempDir: Path, snippet: String) {
+    tempDir.resolve("settings.gradle").writeText(settingsBuildText)
+    tempDir.addSubProject("api", projectBuildText)
+
+    val (imports, snippetWithoutImports) = importExtractor(snippet)
+    val fullSnippet = imports + fixture.pre + '\n' + snippetWithoutImports + '\n' + fixture.post
+
+    tempDir.addSubProject("main", fullSnippet)
+
+    try {
+      GradleRunner.create()
+        .withProjectDir(tempDir.toFile())
+        .withPluginClasspath()
+        .forwardOutput()
+        .withArguments(*arguments)
+        .build()
+    } catch (t: Throwable) {
+      throw RuntimeException("Failed to execute snippet:\n\n$fullSnippet", t)
+    }
+  }
+
+  private fun Path.addSubProject(project: String, buildScriptText: String) {
+    resolve(project)
+      .createDirectory()
+      .resolve("build.gradle")
+      .writeText(buildScriptText)
+  }
+
+  private companion object {
+    @Language("Groovy")
+    private val settingsBuildText = """
+      rootProject.name = 'shadowTest'
+      include 'api', 'main'
+    """.trimIndent()
+
+    @Language("Groovy")
+    private val projectBuildText = """
+      plugins {
+        id 'java'
+        id 'com.gradleup.shadow'
+      }
+      repositories {
+        mavenLocal()
+        mavenCentral()
+      }
+    """.trimIndent()
+  }
+}
