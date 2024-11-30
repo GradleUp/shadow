fb31cafdcef109ffb8d806b6414038a56b49b50f
Goooler <wangzongler@gmail.com>
2024-11-30 16:07:27 +0800

    Migrate XmlAppendingTransformerTest

---
+++ src/intiTest/kotlin/com/github/jengelman/gradle/plugins/shadow/unit/transformers/XmlAppendingTransformerTest.kt
@@ -0,0 +1,34 @@
+package com.github.jengelman.gradle.plugins.shadow.unit.transformers
+
+import assertk.assertThat
+import assertk.assertions.isFalse
+import assertk.assertions.isTrue
+import com.github.jengelman.gradle.plugins.shadow.transformers.XmlAppendingTransformer
+import java.util.Locale
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class XmlAppendingTransformerTest : TransformerTestSupport<XmlAppendingTransformer>() {
+
+  init {
+    /*
+     * NOTE: The Turkish locale has an usual case transformation for the letters "I" and "i", making it a prime
+     * choice to test for improper case-less string comparisions.
+     */
+    Locale.setDefault(Locale("tr"))
+  }
+
+  @BeforeEach
+  fun setUp() {
+    transformer = XmlAppendingTransformer(objectFactory)
+  }
+
+  @Test
+  fun testCanTransformResource() {
+    transformer.resource.set("abcdefghijklmnopqrstuvwxyz")
+
+    assertThat(transformer.canTransformResource(getFileElement("abcdefghijklmnopqrstuvwxyz"))).isTrue()
+    assertThat(transformer.canTransformResource(getFileElement("ABCDEFGHIJKLMNOPQRSTUVWXYZ"))).isTrue()
+    assertThat(transformer.canTransformResource(getFileElement("META-INF/MANIFEST.MF"))).isFalse()
+  }
+}
