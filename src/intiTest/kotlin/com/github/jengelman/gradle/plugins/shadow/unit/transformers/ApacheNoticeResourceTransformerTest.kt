a0a216f1693424d2363a5e0846380c7cbb4930b3
Goooler <wangzongler@gmail.com>
2024-11-30 15:45:03 +0800

    Migrate ApacheNoticeResourceTransformerTest

---
+++ src/intiTest/kotlin/com/github/jengelman/gradle/plugins/shadow/unit/transformers/ApacheNoticeResourceTransformerTest.kt
@@ -0,0 +1,87 @@
+package com.github.jengelman.gradle.plugins.shadow.unit.transformers
+
+import assertk.assertThat
+import assertk.assertions.isFalse
+import assertk.assertions.isTrue
+import assertk.fail
+import com.github.jengelman.gradle.plugins.shadow.ShadowStats
+import com.github.jengelman.gradle.plugins.shadow.transformers.ApacheNoticeResourceTransformer
+import com.github.jengelman.gradle.plugins.shadow.transformers.TransformerContext
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+/**
+ * Modified from [org.apache.maven.plugins.shade.resource.ApacheNoticeResourceTransformerParameterTests.java](https://github.com/apache/maven-shade-plugin/blob/master/src/test/java/org/apache/maven/plugins/shade/resource/ApacheNoticeResourceTransformerParameterTests.java).
+ */
+class ApacheNoticeResourceTransformerTest : TransformerTestSupport<ApacheNoticeResourceTransformer>() {
+
+  @BeforeEach
+  fun setUp() {
+    transformer = ApacheNoticeResourceTransformer(objectFactory)
+    stats = ShadowStats()
+  }
+
+  @Test
+  fun testCanTransformResource() {
+    assertThat(transformer.canTransformResource(getFileElement("META-INF/NOTICE"))).isTrue()
+    assertThat(transformer.canTransformResource(getFileElement("META-INF/NOTICE.TXT"))).isTrue()
+    assertThat(transformer.canTransformResource(getFileElement("META-INF/Notice.txt"))).isTrue()
+    assertThat(transformer.canTransformResource(getFileElement("META-INF/NOTICE.md"))).isTrue()
+    assertThat(transformer.canTransformResource(getFileElement("META-INF/Notice.md"))).isTrue()
+    assertThat(transformer.canTransformResource(getFileElement("META-INF/MANIFEST.MF"))).isFalse()
+  }
+
+  @Test
+  fun testNoParametersShouldNotThrowNullPointerWhenNoInput() {
+    processAndFailOnNullPointer("")
+  }
+
+  @Test
+  fun testNoParametersShouldNotThrowNullPointerWhenNoLinesOfInput() {
+    processAndFailOnNullPointer("Some notice text")
+  }
+
+  @Test
+  fun testNoParametersShouldNotThrowNullPointerWhenOneLineOfInput() {
+    processAndFailOnNullPointer("Some notice text\n")
+  }
+
+  @Test
+  fun testNoParametersShouldNotThrowNullPointerWhenTwoLinesOfInput() {
+    processAndFailOnNullPointer("Some notice text\nSome notice text\n")
+  }
+
+  @Test
+  fun testNoParametersShouldNotThrowNullPointerWhenLineStartsWithSlashSlash() {
+    processAndFailOnNullPointer("Some notice text\n//Some notice text\n")
+  }
+
+  @Test
+  fun testNoParametersShouldNotThrowNullPointerWhenLineIsSlashSlash() {
+    processAndFailOnNullPointer("//\n")
+  }
+
+  @Test
+  fun testNoParametersShouldNotThrowNullPointerWhenLineIsEmpty() {
+    processAndFailOnNullPointer("\n")
+  }
+
+  private fun processAndFailOnNullPointer(noticeText: String) {
+    try {
+      transformer.transform(
+        TransformerContext.builder()
+          .path(NOTICE_RESOURCE)
+          .inputStream(noticeText.toByteArray().inputStream())
+          .stats(stats)
+          .build(),
+      )
+    } catch (ignored: NullPointerException) {
+      fail("Null pointer should not be thrown when no parameters are set.")
+    }
+  }
+
+  companion object {
+    private const val NOTICE_RESOURCE = "META-INF/NOTICE"
+    private lateinit var stats: ShadowStats
+  }
+}
