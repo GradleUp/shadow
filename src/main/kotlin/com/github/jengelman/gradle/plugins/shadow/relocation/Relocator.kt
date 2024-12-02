09a6d930877a30706b30631c675c722d21c45ace
Goooler <wangzongler@gmail.com>
2024-12-02 09:13:58 +0800

    Mark Relocator extends org.apache.maven.plugins.shade.relocation.Relocator

--- src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/relocation/Relocator.kt
+++ src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/relocation/Relocator.kt
@@ -1,21 +1,28 @@
 package com.github.jengelman.gradle.plugins.shadow.relocation
 
+public typealias MavenRelocator = org.apache.maven.plugins.shade.relocation.Relocator
+
 /**
  * Modified from [org.apache.maven.plugins.shade.relocation.Relocator.java](https://github.com/apache/maven-shade-plugin/blob/master/src/main/java/org/apache/maven/plugins/shade/relocation/Relocator.java).
  *
  * @author Jason van Zyl
  * @author John Engelman
  */
-public interface Relocator {
-  public fun canRelocatePath(path: String): Boolean
+@JvmDefaultWithCompatibility
+public interface Relocator : MavenRelocator {
+  public override fun canRelocatePath(path: String): Boolean
+
+  public override fun relocatePath(clazz: String): String = relocatePath(RelocatePathContext(clazz))
+
+  public override fun canRelocateClass(className: String): Boolean
+
+  public override fun relocateClass(clazz: String): String = relocateClass(RelocateClassContext(clazz))
+
+  public override fun applyToSourceContent(sourceContent: String): String
 
   public fun relocatePath(context: RelocatePathContext): String
 
-  public fun canRelocateClass(className: String): Boolean
-
   public fun relocateClass(context: RelocateClassContext): String
-
-  public fun applyToSourceContent(sourceContent: String): String
 
   public companion object {
     public val ROLE: String = Relocator::class.java.name
