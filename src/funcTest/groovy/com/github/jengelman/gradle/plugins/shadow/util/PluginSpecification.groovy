e87fd0c35d5e5d7b26ac37dff189a8e8fef844ec
Goooler <wangzongler@gmail.com>
2024-12-03 15:59:04 +0800

    Move FileExtensions into PluginSpecification

--- src/funcTest/groovy/com/github/jengelman/gradle/plugins/shadow/util/PluginSpecification.groovy
+++ src/funcTest/groovy/com/github/jengelman/gradle/plugins/shadow/util/PluginSpecification.groovy
@@ -186,4 +186,29 @@ abstract class PluginSpecification extends Specification {
         }
         return new File(gradleUserHome, "testkit")
     }
+
+    /**
+     * TODO: this is used as extensions for Groovy, could be replaced after migrated to Kotlin.
+     *  Registered in resources/META-INF/services/org.codehaus.groovy.runtime.ExtensionModule.
+     */
+    static final class FileExtensions {
+        static final File resolve(File file, String relativePath) {
+            try {
+                return new File(file, relativePath)
+            } catch (RuntimeException e) {
+                throw new RuntimeException(String.format("Could not locate file '%s' relative to '%s'.", Arrays.toString(relativePath), file), e)
+            }
+        }
+
+        static final File createDir(File file) {
+            if (file.mkdirs()) {
+                return file
+            }
+            if (file.isDirectory()) {
+                return file
+            }
+            throw new AssertionError("Problems creating dir: " + this
+                + ". Diagnostics: exists=" + file.exists() + ", isFile=" + file.isFile() + ", isDirectory=" + file.isDirectory())
+        }
+    }
 }
