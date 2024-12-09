10f6fa333aac31e45172d39fc645698c46100251
Goooler <wangzongler@gmail.com>
2024-12-09 20:45:37 +0800

    Convert MavenRepository

---
+++ src/intiTest/kotlin/com/github/jengelman/gradle/plugins/shadow/util/repo/maven/MavenRepository.kt
@@ -0,0 +1,14 @@
+package com.github.jengelman.gradle.plugins.shadow.util.repo.maven
+
+import java.net.URI
+
+/**
+ * A fixture for dealing with Maven repositories.
+ */
+interface MavenRepository {
+  val uri: URI
+
+  fun module(groupId: String, artifactId: String): MavenModule
+
+  fun module(groupId: String, artifactId: String, version: String): MavenModule
+}
