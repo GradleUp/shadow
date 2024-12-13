099865d50d78e25efe3937bf26d050ecadd891ba
Zongle Wang <wangzongler@gmail.com>
2024-12-12 22:34:16 -0500

    Compat deprecated getDependencyProject usage (#1106)

--- src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/internal/GradleCompat.kt
+++ src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/internal/GradleCompat.kt
@@ -2,6 +2,7 @@ package com.github.jengelman.gradle.plugins.shadow.internal
 
 import org.gradle.api.Project
 import org.gradle.api.artifacts.Configuration
+import org.gradle.api.artifacts.ProjectDependency
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.plugins.JavaPlugin
@@ -32,5 +33,17 @@ internal fun ConfigurableFileCollection.conventionCompat(vararg paths: Any): Con
   } else {
     setFrom(paths)
     this
+  }
+}
+
+/**
+ * TODO: this could be removed after bumping the min Gradle requirement to 8.11 or above.
+ */
+internal fun ProjectDependency.dependencyProjectCompat(project: Project): Project {
+  return if (GradleVersion.current() >= GradleVersion.version("8.11")) {
+    project.project(path)
+  } else {
+    @Suppress("DEPRECATION")
+    dependencyProject
   }
 }
