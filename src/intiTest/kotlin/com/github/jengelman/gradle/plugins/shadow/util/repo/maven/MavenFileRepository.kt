4f4940ac6a54a6e20a8c729ccc4ba4623a3b57d7
Goooler <wangzongler@gmail.com>
2024-12-17 11:25:50 +0800

    Convert MavenFileRepository

---
+++ src/intiTest/kotlin/com/github/jengelman/gradle/plugins/shadow/util/repo/maven/MavenFileRepository.kt
@@ -0,0 +1,21 @@
+package com.github.jengelman.gradle.plugins.shadow.util.repo.maven
+
+import java.io.File
+import java.net.URI
+
+/**
+ * A fixture for dealing with file Maven repositories.
+ */
+open class MavenFileRepository(protected val rootDir: File) : MavenRepository {
+
+  override val uri: URI = rootDir.toURI()
+
+  override fun module(groupId: String, artifactId: String): MavenFileModule {
+    return module(groupId = groupId, artifactId = artifactId, version = "0.0.0")
+  }
+
+  override fun module(groupId: String, artifactId: String, version: String): MavenFileModule {
+    val artifactDir = rootDir.resolve("${groupId.replace('.', '/')}/$artifactId/$version")
+    return MavenFileModule(artifactDir, groupId, artifactId, version)
+  }
+}
