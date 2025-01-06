e95d2b5508c3c2bb5e4d74c46f9634d82e803aec
Goooler <wangzongler@gmail.com>
2025-01-06 16:39:02 +0800

    Remove unused things in BasePluginSpecification

--- src/funcTest/groovy/com/github/jengelman/gradle/plugins/shadow/BasePluginSpecification.groovy
+++ src/funcTest/groovy/com/github/jengelman/gradle/plugins/shadow/BasePluginSpecification.groovy
@@ -1,10 +1,8 @@
 package com.github.jengelman.gradle.plugins.shadow
 
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import com.github.jengelman.gradle.plugins.shadow.util.AppendableJar
 import com.github.jengelman.gradle.plugins.shadow.util.AppendableMavenFileRepository
 import org.apache.commons.lang3.StringUtils
-import org.codehaus.plexus.util.IOUtil
 import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.GradleRunner
 import spock.lang.Specification
@@ -12,8 +10,6 @@ import spock.lang.TempDir
 
 import java.nio.file.Path
 import java.nio.file.Paths
-import java.util.function.Function
-import java.util.jar.JarEntry
 import java.util.jar.JarFile
 
 abstract class BasePluginSpecification extends Specification {
@@ -24,7 +20,7 @@ abstract class BasePluginSpecification extends Specification {
     AppendableMavenFileRepository repo
 
     def setup() {
-        repo = repo()
+        repo = new AppendableMavenFileRepository(root.resolve('maven-repo'))
         repo.module('junit', 'junit', '3.8.2')
             .use(Paths.get(this.class.classLoader.getResource('junit-3.8.2.jar').toURI()))
             .publish()
@@ -84,31 +80,15 @@ abstract class BasePluginSpecification extends Specification {
         runner.withArguments(["--warning-mode=fail", "--configuration-cache", "--stacktrace"] + tasks.toList())
     }
 
-    BuildResult run(String... tasks) {
-        run(tasks.toList())
-    }
-
-    BuildResult run(List<String> tasks, Function<GradleRunner, GradleRunner> runnerFunction = { it }) {
-        def result = runnerFunction.apply(runner(tasks)).build()
-        assertNoDeprecationWarnings(result)
-        return result
-    }
-
-    BuildResult runWithFailure(List<String> tasks, Function<GradleRunner, GradleRunner> runnerFunction = { it }) {
-        def result = runnerFunction.apply(runner(tasks)).buildAndFail()
-        assertNoDeprecationWarnings(result)
-        return result
-    }
-
-    private static void assertNoDeprecationWarnings(BuildResult result) {
-        result.output.eachLine {
-            assert !containsDeprecationWarning(it)
+    BuildResult run(List<String> tasks) {
+        def result = runner(tasks).build()
+        result.output.eachLine { output ->
+            assert !(
+                output.contains("has been deprecated and is scheduled to be removed in Gradle") ||
+                    output.contains("has been deprecated. This is scheduled to be removed in Gradle")
+            )
         }
-    }
-
-    private static boolean containsDeprecationWarning(String output) {
-        output.contains("has been deprecated and is scheduled to be removed in Gradle") ||
-            output.contains("has been deprecated. This is scheduled to be removed in Gradle")
+        return result
     }
 
     File getProjectScriptFile() {
@@ -137,20 +117,6 @@ abstract class BasePluginSpecification extends Specification {
         return f
     }
 
-    AppendableMavenFileRepository repo(String path = 'maven-repo') {
-        new AppendableMavenFileRepository(root.resolve(path))
-    }
-
-    String getJarFileContents(File f, String path) {
-        JarFile jf = new JarFile(f)
-        def is = jf.getInputStream(new JarEntry(path))
-        StringWriter sw = new StringWriter()
-        IOUtil.copy(is, sw)
-        is.close()
-        jf.close()
-        return sw.toString()
-    }
-
     void assertContains(File f, List<String> paths) {
         JarFile jar = new JarFile(f)
         paths.each { path ->
@@ -167,32 +133,15 @@ abstract class BasePluginSpecification extends Specification {
         jar.close()
     }
 
-    /**
-     * Helper method to allow scoping variables into a closure in a spock test
-     * Prevents variable expansion
-     * When using this you *must* include explicit `assert` statements as Spock will not do it for you
-     */
-    void assertions(Closure closure) {
-        closure()
-    }
-
-    AppendableJar buildJar(String path) {
-        return new AppendableJar(file(path).toPath())
-    }
-
     File getOutputShadowJar() {
         file('build/libs/shadow-1.0-all.jar')
     }
 
-    static File getTestKitDir() {
+    private static File getTestKitDir() {
         def gradleUserHome = System.getenv("GRADLE_USER_HOME")
         if (!gradleUserHome) {
             gradleUserHome = new File(System.getProperty("user.home"), ".gradle").absolutePath
         }
         return new File(gradleUserHome, "testkit")
-    }
-
-    static String escapedPath(Path path) {
-        return path.toString().replaceAll('\\\\', '\\\\\\\\')
     }
 }
