5964eb7ecf4883b283cfa0d3e483fc95a785133b
Goooler <wangzongler@gmail.com>
2025-01-08 23:09:33 +0800

    Clean up TransformCachingSpec

--- src/funcTest/groovy/com/github/jengelman/gradle/plugins/shadow/caching/TransformCachingSpec.groovy
+++ src/funcTest/groovy/com/github/jengelman/gradle/plugins/shadow/caching/TransformCachingSpec.groovy
@@ -4,51 +4,19 @@ import com.github.jengelman.gradle.plugins.shadow.transformers.AppendingTransfor
 import com.github.jengelman.gradle.plugins.shadow.transformers.GroovyExtensionModuleTransformer
 import com.github.jengelman.gradle.plugins.shadow.transformers.ServiceFileTransformer
 import com.github.jengelman.gradle.plugins.shadow.transformers.XmlAppendingTransformer
-import spock.lang.Unroll
 
 class TransformCachingSpec extends AbstractCachingSpec {
-    /**
-     * Ensure that that caching is disabled when transforms are used
-     */
     def 'shadowJar is not cached when custom transforms are used'() {
         given:
         file('src/main/java/server/Server.java') << """
             package server;
-
             public class Server {}
         """.stripIndent()
 
         projectScriptFile << """
-            import com.github.jengelman.gradle.plugins.shadow.transformers.Transformer
-            import com.github.jengelman.gradle.plugins.shadow.transformers.TransformerContext
-            import org.apache.tools.zip.ZipOutputStream
-            import org.gradle.api.file.FileTreeElement
-
-            class CustomTransformer implements Transformer {
-                @Override
-                boolean canTransformResource(FileTreeElement element) {
-                    return false
-                }
-
-                @Override
-                void transform(TransformerContext context) {
-
-                }
-
-                @Override
-                boolean hasTransformedResource() {
-                    return false
-                }
-
-                @Override
-                void modifyOutputStream(ZipOutputStream jos, boolean preserveFileTimestamps) {
-
-                }
-            }
-
             $shadowJar {
-                notCompatibleWithConfigurationCache('CustomTransformer is not cacheable')
-                transform(CustomTransformer)
+                // Use NoOpTransformer to mock a custom transformer here.
+                transform(com.github.jengelman.gradle.plugins.shadow.transformers.NoOpTransformer.INSTANCE)
             }
         """.stripIndent()
 
@@ -71,15 +39,10 @@ class TransformCachingSpec extends AbstractCachingSpec {
         ])
     }
 
-    /**
-     * Ensure that we get a cache miss when ServiceFileTransformer transforms are added and caching works when ServiceFileTransformer transforms are present
-     */
-    @Unroll
     def 'shadowJar is cached correctly when using ServiceFileTransformer'() {
         given:
         file('src/main/java/server/Server.java') << """
             package server;
-
             public class Server {}
         """.stripIndent()
 
@@ -145,16 +108,11 @@ class TransformCachingSpec extends AbstractCachingSpec {
         ])
     }
 
-    /**
-     * Ensure that we get a cache miss when AppendingTransformer transforms are added and caching works when AppendingTransformer transforms are present
-     */
-    @Unroll
     def 'shadowJar is cached correctly when using AppendingTransformer'() {
         given:
         file('src/main/resources/foo/bar.properties') << "foo=bar"
         file('src/main/java/server/Server.java') << """
             package server;
-
             public class Server {}
         """.stripIndent()
 
@@ -226,16 +184,11 @@ class TransformCachingSpec extends AbstractCachingSpec {
         ])
     }
 
-    /**
-     * Ensure that we get a cache miss when XmlAppendingTransformer transforms are added and caching works when XmlAppendingTransformer transforms are present
-     */
-    @Unroll
     def 'shadowJar is cached correctly when using XmlAppendingTransformer'() {
         given:
         file('src/main/resources/foo/bar.xml') << "<foo>bar</foo>"
         file('src/main/java/server/Server.java') << """
             package server;
-
             public class Server {}
         """.stripIndent()
 
@@ -307,15 +260,10 @@ class TransformCachingSpec extends AbstractCachingSpec {
         ])
     }
 
-    /**
-     * Ensure that we get a cache miss when GroovyExtensionModuleTransformer transforms are added and caching works when GroovyExtensionModuleTransformer transforms are present
-     */
-    @Unroll
     def 'shadowJar is cached correctly when using GroovyExtensionModuleTransformer'() {
         given:
         file('src/main/java/server/Server.java') << """
             package server;
-
             public class Server {}
         """.stripIndent()
 
