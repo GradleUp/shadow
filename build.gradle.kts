5c6c08e2a77aebaffea9a84c495e7c02414928ad
Goooler <wangzongler@gmail.com>
2025-01-11 10:44:41 +0800

    Remove java block

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,11 +12,6 @@ plugins {
   id("shadow.convention.deploy")
 }
 
-java {
-  sourceCompatibility = JavaVersion.VERSION_1_8
-  targetCompatibility = JavaVersion.VERSION_1_8
-}
-
 kotlin {
   explicitApi()
   compilerOptions {
