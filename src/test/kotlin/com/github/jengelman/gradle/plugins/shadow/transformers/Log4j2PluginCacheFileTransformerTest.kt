fc60777212cbb99aa500852385f4885211bcb036
Goooler <wangzongler@gmail.com>
2025-01-23 10:31:53 +0800

    Fix Log4j2PluginCacheFileTransformerTest

--- src/test/kotlin/com/github/jengelman/gradle/plugins/shadow/transformers/Log4j2PluginCacheFileTransformerTest.kt
+++ src/test/kotlin/com/github/jengelman/gradle/plugins/shadow/transformers/Log4j2PluginCacheFileTransformerTest.kt
@@ -5,112 +5,58 @@ import assertk.assertions.isEqualTo
 import assertk.assertions.isFalse
 import assertk.assertions.isTrue
 import assertk.fail
-import com.github.jengelman.gradle.plugins.shadow.relocation.Relocator
+import com.github.jengelman.gradle.plugins.shadow.internal.requireResourceAsStream
+import com.github.jengelman.gradle.plugins.shadow.relocation.SimpleRelocator
 import com.github.jengelman.gradle.plugins.shadow.util.SimpleRelocator
-import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
-import java.io.IOException
 import java.net.URL
 import java.util.Collections
 import java.util.jar.JarInputStream
-import org.apache.commons.io.IOUtils
 import org.apache.logging.log4j.core.config.plugins.processor.PluginCache
 import org.apache.logging.log4j.core.config.plugins.processor.PluginProcessor.PLUGIN_CACHE_FILE
 import org.apache.tools.zip.ZipOutputStream
-import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class Log4j2PluginCacheFileTransformerTest {
-
-  private var pluginUrl: URL? = null
-
-  @BeforeAll
-  fun setUp() {
-    pluginUrl = Log4j2PluginCacheFileTransformerTest::class.java.getClassLoader()
-      .getResource(PLUGIN_CACHE_FILE)
-  }
-
-  @AfterAll
-  fun tearDown() {
-    pluginUrl = null
-  }
-
+class Log4j2PluginCacheFileTransformerTest : BaseTransformerTest<Log4j2PluginsCacheFileTransformer>() {
   @Test
-  @Throws(Exception::class)
-  fun test() {
-    val transformer = Log4j2PluginsCacheFileTransformer()
+  fun transformAndModifyOutputStream() {
     assertThat(transformer.hasTransformedResource()).isFalse()
 
-    val expectedYoungestResourceTime = 1605922127000L // Sat Nov 21 2020 01:28:47
-    javaClass.getClassLoader().getResourceAsStream(PLUGIN_CACHE_FILE)
-      .use { log4jCacheFileInputStream ->
-        transformer.transform(
-          TransformerContext.builder()
-            .path(PLUGIN_CACHE_FILE)
-            .inputStream(requireNotNull(log4jCacheFileInputStream))
-            .build(),
-        )
-      }
+    transformer.transform(context())
+    assertThat(transformer.hasTransformedResource()).isTrue()
+    transformer.transform(context())
     assertThat(transformer.hasTransformedResource()).isTrue()
 
-    javaClass.getClassLoader().getResourceAsStream(PLUGIN_CACHE_FILE)
-      .use { log4jCacheFileInputStream ->
-        transformer.transform(
-          TransformerContext.builder()
-            .path(PLUGIN_CACHE_FILE)
-            .inputStream(requireNotNull(log4jCacheFileInputStream))
-            .build(),
-        )
-      }
-    assertThat(transformer.hasTransformedResource()).isTrue()
-
-    assertTransformedCacheFile(transformer, expectedYoungestResourceTime, 1911442937)
-  }
-
-  @Throws(IOException::class)
-  private fun assertTransformedCacheFile(
-    transformer: Log4j2PluginsCacheFileTransformer,
-    expectedTime: Long,
-    expectedHash: Long,
-  ) {
     val jarBuff = ByteArrayOutputStream()
-    ZipOutputStream(jarBuff).use { out ->
-      transformer.modifyOutputStream(requireNotNull(out), false)
+    ZipOutputStream(jarBuff).use {
+      transformer.modifyOutputStream(it, false)
     }
-    JarInputStream(ByteArrayInputStream(jarBuff.toByteArray())).use { inputStream ->
+    JarInputStream(jarBuff.toByteArray().inputStream()).use { inputStream ->
       while (true) {
         val jarEntry = inputStream.nextJarEntry
         if (jarEntry == null) {
-          fail("No expected resource in the output jar")
-        } else if (jarEntry.getName() == PLUGIN_CACHE_FILE) {
-          assertThat(expectedTime).isEqualTo(jarEntry.getTime())
-          assertThat(expectedHash).isEqualTo(IOUtils.toByteArray(inputStream).contentHashCode().toLong())
+          fail("No expected resource in the output jar.")
+        } else if (jarEntry.name == PLUGIN_CACHE_FILE) {
+          @Suppress("Since15")
+          assertThat(1911442937L).isEqualTo(inputStream.readAllBytes().contentHashCode().toLong())
           break
         }
       }
     }
   }
 
-  @Test
-  @Throws(IOException::class)
-  fun testRelocation() {
-    // test with matching relocator
-    testRelocation("org.apache.logging", "new.location.org.apache.logging", "new.location.org.apache.logging")
-
-    // test without matching relocator
-    testRelocation("com.apache.logging", "new.location.com.apache.logging", "org.apache.logging")
-  }
-
-  @Throws(IOException::class)
-  private fun testRelocation(src: String?, pattern: String?, target: String) {
-    val transformer = Log4j2PluginsCacheFileTransformer()
-    val log4jRelocator: Relocator = SimpleRelocator(src, pattern)
-    val aggregator = PluginCache()
-    aggregator.loadCacheFiles(Collections.enumeration<URL?>(mutableListOf<URL?>(pluginUrl)))
-
+  @ParameterizedTest
+  @MethodSource("relocationParameters")
+  fun relocations(pattern: String, shadedPattern: String, target: String) {
+    val log4jRelocator = SimpleRelocator(pattern, shadedPattern)
+    val aggregator = PluginCache().apply {
+      loadCacheFiles(Collections.enumeration<URL>(listOf(pluginCacheUrl)))
+    }
+    // Init stats to avoid NPE.
+    transformer.transform(context())
     transformer.relocatePlugin(listOf(log4jRelocator), aggregator.allCategories)
 
     for (pluginEntryMap in aggregator.allCategories.values) {
@@ -118,5 +64,21 @@ class Log4j2PluginCacheFileTransformerTest {
         assertThat(entry.className.startsWith(target)).isTrue()
       }
     }
+  }
+
+  private fun context(vararg relocator: SimpleRelocator): TransformerContext {
+    return TransformerContext(PLUGIN_CACHE_FILE, requireResourceAsStream(PLUGIN_CACHE_FILE), relocator.toSet())
+  }
+
+  private companion object {
+    val pluginCacheUrl: URL = requireNotNull(this::class.java.getClassLoader().getResource(PLUGIN_CACHE_FILE))
+
+    @JvmStatic
+    fun relocationParameters() = listOf(
+      // test with matching relocator
+      Arguments.of("org.apache.logging", "new.location.org.apache.logging", "new.location.org.apache.logging"),
+      // test without matching relocator
+      Arguments.of("com.apache.logging", "new.location.com.apache.logging", "org.apache.logging"),
+    )
   }
 }
