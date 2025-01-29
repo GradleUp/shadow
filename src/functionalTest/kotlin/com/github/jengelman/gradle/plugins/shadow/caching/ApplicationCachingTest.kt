3a44831512727a7ed21ec72d73b215ebe59a0ddb
Goooler <wangzongler@gmail.com>
2025-01-29 09:10:47 +0800

    Test runShadowCachedCorrectlyAfterShadowJarChanged

---
+++ src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/caching/ApplicationCachingTest.kt
@@ -0,0 +1,79 @@
+package com.github.jengelman.gradle.plugins.shadow.caching
+
+import kotlin.io.path.appendText
+import kotlin.io.path.writeText
+import org.junit.jupiter.api.Test
+
+class ApplicationCachingTest : BaseCachingTest() {
+  override val taskPath: String = runShadowTask
+
+  @Test
+  fun runShadowCachedCorrectlyAfterShadowJarChanged() {
+    prepare()
+    val resourcePath = path("src/main/resources/my/resource.txt")
+    resourcePath.writeText(
+      """
+        Hello, World! %s from resource 1
+      """.trimIndent(),
+    )
+    path("src/main/java/my/App.java").writeText(
+      """
+        package my;
+        public class App {
+          public static void main(String[] args) throws Exception {
+            if (args.length == 0) {
+              throw new IllegalArgumentException("No arguments provided.");
+            }
+            var is = App.class.getResourceAsStream("resource.txt");
+            String content = String.format(new String(is.readAllBytes()), (Object[]) args);
+            System.out.println(content);
+          }
+        }
+      """.trimIndent(),
+    )
+
+    assertExecutionSuccess("Hello, World! foo from resource 1")
+    assertExecutionsFromCacheAndUpToDate("Hello, World! foo from resource 1")
+
+    resourcePath.writeText(
+      """
+        Hello, World! %s from resource 2
+      """.trimIndent(),
+    )
+
+    assertExecutionSuccess("Hello, World! foo from resource 2")
+    assertExecutionsFromCacheAndUpToDate("Hello, World! foo from resource 2")
+  }
+
+  private fun prepare(
+    projectBlock: String = "",
+    applicationBlock: String = "",
+    settingsBlock: String = "",
+    dependenciesBlock: String = "",
+    runShadowBlock: String = "",
+  ) {
+    projectScriptPath.appendText(
+      """
+        apply plugin: 'application'
+        $projectBlock
+        application {
+          mainClass = 'my.App'
+          $applicationBlock
+        }
+        dependencies {
+          $dependenciesBlock
+        }
+        $runShadow {
+          args 'foo'
+          $runShadowBlock
+        }
+      """.trimIndent(),
+    )
+    settingsScriptPath.writeText(
+      getDefaultSettingsBuildScript(
+        startBlock = settingsBlock,
+        endBlock = "rootProject.name = 'myapp'",
+      ),
+    )
+  }
+}
