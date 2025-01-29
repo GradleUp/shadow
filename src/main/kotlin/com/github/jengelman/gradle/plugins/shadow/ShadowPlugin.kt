3f897081efa8fabb01ad0829ee3a92479741e10c
Goooler <wangzongler@gmail.com>
2025-01-29 15:32:34 +0800

    Simplify ShadowPlugin.apply

--- src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/ShadowPlugin.kt
+++ src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/ShadowPlugin.kt
@@ -9,22 +9,22 @@ import org.gradle.api.plugins.JavaPlugin
 public abstract class ShadowPlugin : Plugin<Project> {
 
   override fun apply(project: Project) {
-    project.run {
-      plugins.apply(ShadowBasePlugin::class.java)
+    with(project.plugins) {
+      apply(ShadowBasePlugin::class.java)
       @Suppress("WithTypeWithoutConfigureEach")
-      plugins.withType(JavaPlugin::class.java) {
-        plugins.apply(ShadowJavaPlugin::class.java)
+      withType(JavaPlugin::class.java) {
+        apply(ShadowJavaPlugin::class.java)
       }
       @Suppress("WithTypeWithoutConfigureEach")
-      plugins.withType(ApplicationPlugin::class.java) {
-        plugins.apply(ShadowApplicationPlugin::class.java)
+      withType(ApplicationPlugin::class.java) {
+        apply(ShadowApplicationPlugin::class.java)
       }
       // Apply the legacy plugin last
       // Because we apply the ShadowJavaPlugin/ShadowApplication plugin in a withType callback for the
       // respective JavaPlugin/ApplicationPlugin, it may still apply before the shadowJar task is created etc.
       // If the user applies shadow before those plugins. However, this is fine, because this was also
       // the behavior with the old plugin when applying in that order.
-      plugins.apply(LegacyShadowPlugin::class.java)
+      apply(LegacyShadowPlugin::class.java)
     }
   }
 }
