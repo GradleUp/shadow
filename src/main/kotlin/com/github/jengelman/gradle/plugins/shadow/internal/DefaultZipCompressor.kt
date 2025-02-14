f48dc806d5e1a6080adf9b9bae58d4b0dafa7b06
Goooler <wangzongler@gmail.com>
2025-02-14 11:26:43 +0800

    Update DefaultZipCompressor from https://github.com/gradle/gradle/blob/b893c2b085046677cf858fb3d5ce00e68e556c3a/platforms/core-configuration/file-operations/src/main/java/org/gradle/api/internal/file/copy/DefaultZipCompressor.java

--- src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/internal/DefaultZipCompressor.kt
+++ src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/internal/DefaultZipCompressor.kt
@@ -5,6 +5,9 @@ import org.apache.tools.zip.Zip64Mode
 import org.apache.tools.zip.ZipOutputStream
 import org.gradle.api.UncheckedIOException
 
+/**
+ * Modified from [org.gradle.api.internal.file.copy.DefaultZipCompressor.java](https://github.com/gradle/gradle/blob/b893c2b085046677cf858fb3d5ce00e68e556c3a/platforms/core-configuration/file-operations/src/main/java/org/gradle/api/internal/file/copy/DefaultZipCompressor.java).
+ */
 internal class DefaultZipCompressor(
   allowZip64Mode: Boolean,
   private val entryCompressionMethod: Int,
@@ -12,8 +15,8 @@ internal class DefaultZipCompressor(
   private val zip64Mode = if (allowZip64Mode) Zip64Mode.AsNeeded else Zip64Mode.Never
 
   override fun createArchiveOutputStream(destination: File): ZipOutputStream {
-    try {
-      return ZipOutputStream(destination).apply {
+    return try {
+      ZipOutputStream(destination).apply {
         setUseZip64(zip64Mode)
         setMethod(entryCompressionMethod)
       }
--- src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/internal/ZipCompressor.kt
+++ src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/internal/ZipCompressor.kt
@@ -2,4 +2,7 @@ package com.github.jengelman.gradle.plugins.shadow.internal
 
 import org.gradle.api.internal.file.archive.compression.ArchiveOutputStreamFactory
 
+/**
+ * Modified from [org.gradle.api.internal.file.copy.ZipCompressor.java](https://github.com/gradle/gradle/blob/73091267320cd330bcb3457903436579bac354ce/platforms/core-configuration/file-operations/src/main/java/org/gradle/api/internal/file/copy/ZipCompressor.java).
+ */
 public interface ZipCompressor : ArchiveOutputStreamFactory
