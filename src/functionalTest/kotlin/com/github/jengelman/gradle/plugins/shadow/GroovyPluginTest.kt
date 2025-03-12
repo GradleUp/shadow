f87f7e2f0bb94d4607ad349d07eba1f95bb3a7de
Goooler <wangzongler@gmail.com>
2025-03-12 16:15:48 +0800

    Add GroovyPluginTest

---
+++ src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/GroovyPluginTest.kt
@@ -0,0 +1,44 @@
+package com.github.jengelman.gradle.plugins.shadow
+
+import assertk.assertThat
+import com.github.jengelman.gradle.plugins.shadow.util.JvmLang
+import com.github.jengelman.gradle.plugins.shadow.util.containsEntries
+import kotlin.io.path.appendText
+import kotlin.io.path.writeText
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class GroovyPluginTest : BasePluginTest() {
+  @BeforeEach
+  override fun setup() {
+    super.setup()
+    val projectBuildScript = getDefaultProjectBuildScript(
+      plugin = "groovy",
+      withGroup = true,
+      withVersion = true,
+    )
+    projectScriptPath.writeText(projectBuildScript)
+  }
+
+  @Test
+  fun compatGroovy() {
+    val mainClassEntry = writeClass(withImports = true, jvmLang = JvmLang.Groovy)
+    projectScriptPath.appendText(
+      """
+        dependencies {
+          compileOnly localGroovy()
+          implementation 'junit:junit:3.8.2'
+        }
+      """.trimIndent(),
+    )
+
+    run(shadowJarTask)
+
+    assertThat(outputShadowJar).useAll {
+      containsEntries(
+        mainClassEntry,
+        *junitEntries,
+      )
+    }
+  }
+}
