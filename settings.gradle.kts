b3305264448d077367a7c8a3c1afe792d4fafa07
Zongle Wang <wangzongler@gmail.com>
2025-03-23 14:44:56 +0800

    Update TODO

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,7 +22,7 @@ develocity {
   buildScan {
     termsOfUseUrl = "https://gradle.com/terms-of-service"
     termsOfUseAgree = "yes"
-    // TODO: workaround for https://github.com/gradle/gradle/issues/22879.
+    // TODO: https://github.com/gradle/gradle/issues/22879
     val isCI = providers.environmentVariable("CI").isPresent
     publishing.onlyIf { isCI }
   }
