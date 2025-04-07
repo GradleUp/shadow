51ced57126bc7af4bf76666f726441506e132fae
Zongle Wang <wangzongler@gmail.com>
2025-04-07 10:18:37 -0400

    Rearrange ShadowJar logic in ShadowKmpPlugin (#1386)

--- src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/ShadowKmpPlugin.kt
+++ src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/ShadowKmpPlugin.kt
@@ -2,7 +2,6 @@ package com.github.jengelman.gradle.plugins.shadow
 
 import com.github.jengelman.gradle.plugins.shadow.ShadowJavaPlugin.Companion.registerShadowJarCommon
 import com.github.jengelman.gradle.plugins.shadow.internal.mainClassAttributeKey
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import kotlin.collections.contains
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -12,40 +11,39 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinVersion as KgpVersion
 import org.jetbrains.kotlin.gradle.targets.jvm.KotlinJvmTarget
 
 public abstract class ShadowKmpPlugin : Plugin<Project> {
-  private lateinit var kmpExtension: KotlinMultiplatformExtension
 
   override fun apply(project: Project) {
     with(project) {
-      kmpExtension = extensions.getByType(KotlinMultiplatformExtension::class.java)
-      kmpExtension.targets.configureEach { target ->
+      extensions.getByType(KotlinMultiplatformExtension::class.java).targets.configureEach { target ->
         if (target !is KotlinJvmTarget) return@configureEach
 
-        val kotlinJvmMain = target.compilations.named("main")
-        registerShadowJarCommon { task ->
-          task.from(kotlinJvmMain.map { it.output.allOutputs })
-          task.configurations.convention(
-            provider {
-              listOf(configurations.getByName(kotlinJvmMain.get().runtimeDependencyConfigurationName))
-            },
-          )
-          configureMainClass(target.name, task)
-        }
+        configureShadowJar(target)
       }
     }
   }
 
-  private fun Project.configureMainClass(targetName: String, task: ShadowJar) {
-    if (KgpVersion.DEFAULT < KgpVersion.KOTLIN_2_1) return
+  private fun Project.configureShadowJar(target: KotlinJvmTarget) {
+    val kotlinJvmMain = target.compilations.named("main")
+    registerShadowJarCommon { task ->
+      task.from(kotlinJvmMain.map { it.output.allOutputs })
+      task.configurations.convention(
+        provider {
+          listOf(configurations.getByName(kotlinJvmMain.get().runtimeDependencyConfigurationName))
+        },
+      )
 
-    @OptIn(ExperimentalKotlinGradlePluginApi::class)
-    kmpExtension.jvm(targetName).mainRun {
-      // Fix cannot serialize object of type 'org.jetbrains.kotlin.gradle.targets.jvm.tasks.KotlinJvmRun'.
-      val mainClassName = provider { mainClass }
-      task.inputs.property("mainClassName", mainClassName)
-      task.doFirst {
-        val realClass = mainClassName.get().orNull
-        if (!task.manifest.attributes.contains(mainClassAttributeKey) && !realClass.isNullOrEmpty()) {
-          task.manifest.attributes[mainClassAttributeKey] = realClass
+      if (KgpVersion.DEFAULT < KgpVersion.KOTLIN_2_1) return@registerShadowJarCommon
+
+      @OptIn(ExperimentalKotlinGradlePluginApi::class)
+      target.mainRun {
+        // Fix cannot serialize object of type 'org.jetbrains.kotlin.gradle.targets.jvm.tasks.KotlinJvmRun'.
+        val mainClassName = provider { mainClass }
+        task.inputs.property("mainClassName", mainClassName)
+        task.doFirst {
+          val realClass = mainClassName.get().orNull
+          if (!task.manifest.attributes.contains(mainClassAttributeKey) && !realClass.isNullOrEmpty()) {
+            task.manifest.attributes[mainClassAttributeKey] = realClass
+          }
         }
       }
     }
