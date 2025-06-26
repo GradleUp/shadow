c0512b1d815f9ebf3076ab613bcd222118afac41
Zongle Wang <wangzongler@gmail.com>
2025-06-26 16:06:17 +0800

    Expose Ant as compile scope (#1488)
    (cherry picked from commit 8eeb64e969ce68ba82d864602cc2d120898866bd)

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,10 +76,10 @@ publishing.publications.withType<MavenPublication>().configureEach {
 }
 
 dependencies {
+  api("org.apache.ant:ant:1.10.15") // Types from Ant are exposed in the public API.
   implementation("org.jdom:jdom2:2.0.6.1")
   implementation("org.ow2.asm:asm-commons:9.8")
   implementation("commons-io:commons-io:2.19.0")
-  implementation("org.apache.ant:ant:1.10.15")
   implementation("org.codehaus.plexus:plexus-utils:4.0.2")
   implementation("org.codehaus.plexus:plexus-xml:4.1.0")
   implementation("org.apache.logging.log4j:log4j-core:2.24.1")
