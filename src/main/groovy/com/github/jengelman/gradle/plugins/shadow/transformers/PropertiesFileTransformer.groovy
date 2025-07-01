228e33682d16e0fe339d1c21edf3f3df3db91c65
Zongle Wang <wangzongler@gmail.com>
2025-07-01 21:07:00 +0800

    Apply suggestions from code review

--- src/main/groovy/com/github/jengelman/gradle/plugins/shadow/transformers/PropertiesFileTransformer.groovy
+++ src/main/groovy/com/github/jengelman/gradle/plugins/shadow/transformers/PropertiesFileTransformer.groovy
@@ -137,7 +137,7 @@ class PropertiesFileTransformer implements Transformer {
 
     @Internal
     Closure<String> keyTransformer = new Closure<String>("") {
-        String doCall(Object arguments) { arguments }
+        String doCall(Object arguments) { arguments } // We can't use Closure#IDENTITY instead, for being compatible with Groovy 3 and 4.
     }
 
     @Override
