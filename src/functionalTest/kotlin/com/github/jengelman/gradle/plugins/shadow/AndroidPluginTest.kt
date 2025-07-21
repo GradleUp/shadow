e8468e3fafa250f0b48a31755fa338c9c3d03dbc
Goooler <wangzongler@gmail.com>
2025-07-21 10:25:42 +0800

    Throw exceptions if mixed Shadow with AGP

---
+++ src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/AndroidPluginTest.kt
@@ -0,0 +1,38 @@
+package com.github.jengelman.gradle.plugins.shadow
+
+import assertk.assertThat
+import assertk.assertions.contains
+import kotlin.io.path.appendText
+import kotlin.io.path.writeText
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+
+class AndroidPluginTest : BasePluginTest() {
+  override fun setup() {
+    super.setup()
+    settingsScriptPath.writeText(
+      // Add Google's Maven repository for AGP.
+      getDefaultSettingsBuildScript("google()"),
+    )
+  }
+
+  @ParameterizedTest
+  @MethodSource("androidIdsProvider")
+  fun doesNotCompatAgp(pluginId: String) {
+    projectScriptPath.writeText(getDefaultProjectBuildScript(plugin = pluginId))
+
+    assertThat(runWithFailure().output).contains(
+      "Shadow does not support using with AGP, you may need Android Fused Library plugin instead.",
+    )
+  }
+
+  private companion object {
+    @JvmStatic
+    fun androidIdsProvider() = listOf(
+      "com.android.application",
+      "com.android.library",
+      "com.android.test",
+      "com.android.dynamic-feature",
+    )
+  }
+}
