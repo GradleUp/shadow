c658b498cece6802383db864aecda8707f1a21f5
Goooler <wangzongler@gmail.com>
2025-08-03 16:03:30 +0800

    Test `setManifestAttrsFromJvmTargetJar`

--- src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/KotlinPluginsTest.kt
+++ src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/KotlinPluginsTest.kt
@@ -174,7 +174,7 @@ class KotlinPluginsTest : BasePluginTest() {
 
   @ParameterizedTest
   @ValueSource(booleans = [false, true])
-  fun canSetMainClassAttributeFromMainRun(useShadowAttr: Boolean) {
+  fun setMainClassAttributeFromMainRun(useShadowAttr: Boolean) {
     val mainClassEntry = writeClass(sourceSet = "jvmMain", jvmLang = JvmLang.Kotlin)
     val main2ClassEntry = writeClass(sourceSet = "jvmMain", jvmLang = JvmLang.Kotlin, className = "Main2")
     val mainClassName = "my.Main"
@@ -241,6 +241,43 @@ class KotlinPluginsTest : BasePluginTest() {
       "$SHADOW_JAR_TASK_NAME task already exists, skipping configuration for target: $jvmTargetName", // Logged from Shadow.
       "Declaring multiple Kotlin Targets of the same type is not supported.", // Thrown from KGP.
     )
+  }
+
+  @ParameterizedTest
+  @ValueSource(booleans = [false, true])
+  fun setManifestAttrsFromJvmTargetJar(useShadowAttr: Boolean) {
+    val mainClassEntry = writeClass(sourceSet = "jvmMain", jvmLang = JvmLang.Kotlin)
+    val main2ClassEntry = writeClass(sourceSet = "jvmMain", jvmLang = JvmLang.Kotlin, className = "Main2")
+    val mainClassName = "my.Main"
+    val main2ClassName = "my.Main2"
+    val mainAttr = if (useShadowAttr) "attributes '$mainClassAttributeKey': '$main2ClassName'" else ""
+    projectScriptPath.appendText(
+      """
+        kotlin {
+          jvm()
+        }
+        tasks.named('jvmJar', Jar) {
+          manifest {
+            attributes '$mainClassAttributeKey': '$mainClassName'
+          }
+        }
+        $shadowJar {
+          manifest {
+            $mainAttr
+          }
+        }
+      """.trimIndent(),
+    )
+
+    run(shadowJarTask)
+
+    assertThat(outputShadowJar).useAll {
+      containsAtLeast(
+        mainClassEntry,
+        main2ClassEntry,
+      )
+      getMainAttr(mainClassAttributeKey).isEqualTo(if (useShadowAttr) main2ClassName else mainClassName)
+    }
   }
 
   private fun compileOnlyStdlib(exclude: Boolean): String {
