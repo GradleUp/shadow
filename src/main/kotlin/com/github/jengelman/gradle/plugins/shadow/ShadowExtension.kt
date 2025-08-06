83a553d202cfe23de75ac7322a0c8b4ba7da32b1
Goooler <wangzongler@gmail.com>
2025-08-06 08:10:55 +0800

    Update API

--- src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/ShadowExtension.kt
+++ src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/ShadowExtension.kt
@@ -1,14 +1,4 @@
 package com.github.jengelman.gradle.plugins.shadow
 
-import org.gradle.api.Project
-import org.gradle.api.publish.maven.MavenPublication
-
 @Deprecated("This is deprecated since 8.3.2")
-public abstract class ShadowExtension(project: Project) {
-  private val components = project.components
-
-  @Deprecated("Configure publication using `components.shadow` directly.")
-  public fun component(publication: MavenPublication) {
-    publication.from(components.findByName(ShadowBasePlugin.COMPONENT_NAME))
-  }
-}
+public interface ShadowExtension
