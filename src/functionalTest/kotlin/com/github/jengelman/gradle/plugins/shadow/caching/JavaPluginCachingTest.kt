56b34cddfee1c02ade39bd7ca677b717ced7b575
Goooler <wangzongler@gmail.com>
2025-08-07 22:12:01 +0800

    Revert "Regroup"
    This reverts commit 30e01487

--- src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/caching/JavaPluginCachingTest.kt
+++ src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/caching/JavaPluginCachingTest.kt
@@ -3,6 +3,7 @@ package com.github.jengelman.gradle.plugins.shadow.caching
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import com.github.jengelman.gradle.plugins.shadow.internal.MinimizeDependencyFilter
+import com.github.jengelman.gradle.plugins.shadow.internal.mainClassAttributeKey
 import com.github.jengelman.gradle.plugins.shadow.util.containsOnly
 import com.github.jengelman.gradle.plugins.shadow.util.getMainAttr
 import kotlin.io.path.appendText
@@ -174,5 +175,64 @@ class JavaPluginCachingTest : BaseCachingTest() {
     projectScriptPath.writeText(replaced)
 
     assertions("Foo3", "Bar3")
+  }
+
+  @Test
+  fun kotlinMainRunChanged() {
+    val mainClassName = "my.Main"
+    val main2ClassName = "my.Main2"
+
+    val projectBuildScript = getDefaultProjectBuildScript(
+      plugin = "org.jetbrains.kotlin.multiplatform",
+      withGroup = true,
+      withVersion = true,
+    )
+    projectScriptPath.writeText(
+      """
+        $projectBuildScript
+        kotlin {
+          jvm().mainRun {
+            it.mainClass.set('$mainClassName')
+          }
+        }
+      """.trimIndent(),
+    )
+
+    assertCompositeExecutions {
+      getMainAttr(mainClassAttributeKey).isEqualTo(mainClassName)
+    }
+
+    val replaced = projectScriptPath.readText().replace(mainClassName, main2ClassName)
+    projectScriptPath.writeText(replaced)
+
+    assertCompositeExecutions {
+      getMainAttr(mainClassAttributeKey).isEqualTo(main2ClassName)
+    }
+  }
+
+  @Test
+  fun applicationChanged() {
+    val mainClassName = "my.Main"
+    val main2ClassName = "my.Main2"
+
+    projectScriptPath.appendText(
+      """
+        apply plugin: 'application'
+        application {
+          mainClass = '$mainClassName'
+        }
+      """.trimIndent(),
+    )
+
+    assertCompositeExecutions {
+      getMainAttr(mainClassAttributeKey).isEqualTo(mainClassName)
+    }
+
+    val replaced = projectScriptPath.readText().replace(mainClassName, main2ClassName)
+    projectScriptPath.writeText(replaced)
+
+    assertCompositeExecutions {
+      getMainAttr(mainClassAttributeKey).isEqualTo(main2ClassName)
+    }
   }
 }
