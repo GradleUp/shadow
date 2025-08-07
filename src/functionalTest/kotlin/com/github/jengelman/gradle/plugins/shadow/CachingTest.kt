64f519399fa542e35c090e5e921bbdb417e05d46
Zongle Wang <wangzongler@gmail.com>
2025-08-07 23:35:29 +0800

    Group all caching tests into a new class (#1607)

---
+++ src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/CachingTest.kt
@@ -0,0 +1,573 @@
+package com.github.jengelman.gradle.plugins.shadow
+
+import assertk.Assert
+import assertk.assertFailure
+import assertk.assertThat
+import assertk.assertions.contains
+import assertk.assertions.isEmpty
+import assertk.assertions.isEqualTo
+import assertk.assertions.isInstanceOf
+import com.github.jengelman.gradle.plugins.shadow.internal.MinimizeDependencyFilter
+import com.github.jengelman.gradle.plugins.shadow.internal.mainClassAttributeKey
+import com.github.jengelman.gradle.plugins.shadow.transformers.ResourceTransformer
+import com.github.jengelman.gradle.plugins.shadow.transformers.ServiceFileTransformer
+import com.github.jengelman.gradle.plugins.shadow.util.Issue
+import com.github.jengelman.gradle.plugins.shadow.util.JarPath
+import com.github.jengelman.gradle.plugins.shadow.util.containsOnly
+import com.github.jengelman.gradle.plugins.shadow.util.getMainAttr
+import kotlin.io.path.ExperimentalPathApi
+import kotlin.io.path.appendText
+import kotlin.io.path.isDirectory
+import kotlin.io.path.name
+import kotlin.io.path.readText
+import kotlin.io.path.walk
+import kotlin.io.path.writeText
+import org.gradle.api.file.DuplicatesStrategy
+import org.gradle.testkit.runner.TaskOutcome
+import org.junit.jupiter.api.Test
+
+class CachingTest : BasePluginTest() {
+  private var taskPath: String = shadowJarTask
+
+  /**
+   * Ensure that a basic usage reuses an output from cache and then gets a cache miss when the content changes.
+   */
+  @Test
+  fun dependenciesChanged() {
+    projectScriptPath.appendText(
+      """
+        dependencies {
+          ${implementationFiles(artifactAJar, artifactBJar)}
+        }
+      """.trimIndent(),
+    )
+
+    assertCompositeExecutions {
+      containsOnly(
+        *entriesInAB,
+        *manifestEntries,
+      )
+    }
+
+    val replaced = projectScriptPath.readText().lines()
+      .filterNot { it == implementationFiles(artifactBJar) }
+      .joinToString(lineSeparator)
+    projectScriptPath.writeText(replaced)
+
+    assertCompositeExecutions {
+      containsOnly(
+        *entriesInA,
+        *manifestEntries,
+      )
+    }
+  }
+
+  @Test
+  fun outputFileChanged() {
+    projectScriptPath.appendText(
+      """
+        dependencies {
+          ${implementationFiles(artifactAJar, artifactBJar)}
+        }
+      """.trimIndent() + lineSeparator,
+    )
+
+    assertCompositeExecutions {
+      containsOnly(
+        *entriesInAB,
+        *manifestEntries,
+      )
+    }
+
+    projectScriptPath.appendText(
+      """
+        $shadowJar {
+          archiveBaseName = "foo"
+        }
+      """.trimIndent(),
+    )
+
+    assertExecutionsFromCacheAndUpToDate()
+    assertThat(jarPath("build/libs/foo-1.0-all.jar")).useAll {
+      containsOnly(
+        *entriesInAB,
+        *manifestEntries,
+      )
+    }
+  }
+
+  @Test
+  fun dependencyFilterChanged() {
+    publishArtifactCD()
+    projectScriptPath.appendText(
+      """
+        dependencies {
+          implementation 'my:d:1.0'
+        }
+      """.trimIndent() + lineSeparator,
+    )
+    val assertions = {
+      assertCompositeExecutions {
+        containsOnly(
+          "c.properties",
+          "d.properties",
+          *manifestEntries,
+        )
+      }
+    }
+
+    assertions()
+
+    projectScriptPath.appendText(
+      """
+        $shadowJar {
+          dependencyFilter = new ${MinimizeDependencyFilter::class.java.name}(project)
+        }
+      """.trimIndent(),
+    )
+
+    assertions()
+  }
+
+  @Test
+  fun duplicatesStrategyChanged() {
+    listOf(
+      DuplicatesStrategy.EXCLUDE,
+      DuplicatesStrategy.INCLUDE,
+      DuplicatesStrategy.WARN,
+    ).forEach { strategy ->
+      projectScriptPath.writeText(
+        getDefaultProjectBuildScript(withGroup = true, withVersion = true) +
+          """
+            $shadowJar {
+              duplicatesStrategy = DuplicatesStrategy.$strategy
+            }
+          """.trimIndent(),
+      )
+
+      assertCompositeExecutions()
+    }
+  }
+
+  @Test
+  fun manifestAttrsChanged() {
+    projectScriptPath.appendText(
+      """
+        jar {
+          manifest {
+            attributes 'Foo': 'Foo1'
+          }
+        }
+        $shadowJar {
+          manifest {
+            attributes 'Bar': 'Bar1'
+          }
+        }
+      """.trimIndent(),
+    )
+
+    val assertions = { valueFoo: String, valueBar: String ->
+      assertCompositeExecutions {
+        getMainAttr("Foo").isEqualTo(valueFoo)
+        getMainAttr("Bar").isEqualTo(valueBar)
+      }
+    }
+
+    assertions("Foo1", "Bar1")
+
+    var replaced = projectScriptPath.readText().replace("Foo1", "Foo2")
+    projectScriptPath.writeText(replaced)
+
+    assertions("Foo2", "Bar1")
+
+    replaced = projectScriptPath.readText().replace("Bar1", "Bar2")
+    projectScriptPath.writeText(replaced)
+
+    assertions("Foo2", "Bar2")
+
+    replaced = projectScriptPath.readText()
+      .replace("Foo2", "Foo3")
+      .replace("Bar2", "Bar3")
+    projectScriptPath.writeText(replaced)
+
+    assertions("Foo3", "Bar3")
+  }
+
+  @Test
+  fun kotlinMainRunChanged() {
+    val mainClassName = "my.Main"
+    val main2ClassName = "my.Main2"
+
+    val projectBuildScript = getDefaultProjectBuildScript(
+      plugin = "org.jetbrains.kotlin.multiplatform",
+      withGroup = true,
+      withVersion = true,
+    )
+    projectScriptPath.writeText(
+      """
+        $projectBuildScript
+        kotlin {
+          jvm().mainRun {
+            it.mainClass.set('$mainClassName')
+          }
+        }
+      """.trimIndent(),
+    )
+
+    assertCompositeExecutions {
+      getMainAttr(mainClassAttributeKey).isEqualTo(mainClassName)
+    }
+
+    val replaced = projectScriptPath.readText().replace(mainClassName, main2ClassName)
+    projectScriptPath.writeText(replaced)
+
+    assertCompositeExecutions {
+      getMainAttr(mainClassAttributeKey).isEqualTo(main2ClassName)
+    }
+  }
+
+  @Test
+  fun applicationChanged() {
+    val mainClassName = "my.Main"
+    val main2ClassName = "my.Main2"
+
+    projectScriptPath.appendText(
+      """
+        apply plugin: 'application'
+        application {
+          mainClass = '$mainClassName'
+        }
+      """.trimIndent(),
+    )
+
+    assertCompositeExecutions {
+      getMainAttr(mainClassAttributeKey).isEqualTo(mainClassName)
+    }
+
+    val replaced = projectScriptPath.readText().replace(mainClassName, main2ClassName)
+    projectScriptPath.writeText(replaced)
+
+    assertCompositeExecutions {
+      getMainAttr(mainClassAttributeKey).isEqualTo(main2ClassName)
+    }
+  }
+
+  @Issue(
+    "https://github.com/GradleUp/shadow/issues/717",
+  )
+  @Test
+  fun jarIncludesExcludesChanged() {
+    val mainClassEntry = writeClass(className = "Main")
+    val main2ClassEntry = writeClass(className = "Main2")
+    projectScriptPath.appendText(
+      """
+        dependencies {
+          implementation 'my:a:1.0'
+          implementation 'my:b:1.0'
+        }
+      """.trimIndent() + lineSeparator,
+    )
+
+    assertCompositeExecutions {
+      containsOnly(
+        "my/",
+        mainClassEntry,
+        main2ClassEntry,
+        *entriesInAB,
+        *manifestEntries,
+      )
+    }
+
+    projectScriptPath.appendText(
+      """
+        $shadowJar {
+          exclude '**.properties'
+        }
+      """.trimIndent() + lineSeparator,
+    )
+
+    assertCompositeExecutions {
+      containsOnly(
+        "my/",
+        mainClassEntry,
+        main2ClassEntry,
+        *manifestEntries,
+      )
+    }
+
+    projectScriptPath.appendText(
+      """
+        $shadowJar {
+          include '$mainClassEntry'
+        }
+      """.trimIndent() + lineSeparator,
+    )
+
+    assertCompositeExecutions {
+      containsOnly(
+        "my/",
+        mainClassEntry,
+        *manifestEntries,
+      )
+    }
+
+    projectScriptPath.appendText(
+      """
+        $shadowJar {
+          include '$main2ClassEntry'
+        }
+      """.trimIndent() + lineSeparator,
+    )
+
+    assertCompositeExecutions {
+      containsOnly(
+        "my/",
+        mainClassEntry,
+        main2ClassEntry,
+        *manifestEntries,
+      )
+    }
+  }
+
+  @Test
+  fun dependenciesIncludesExcludesChanged() {
+    val mainClassEntry = writeClass(withImports = true)
+    projectScriptPath.appendText(
+      """
+        dependencies {
+          implementation 'junit:junit:3.8.2'
+        }
+      """.trimIndent() + lineSeparator,
+    )
+
+    assertCompositeExecutions {
+      containsOnly(
+        "my/",
+        mainClassEntry,
+        *junitEntries,
+        *manifestEntries,
+      )
+    }
+
+    projectScriptPath.appendText(
+      """
+        $shadowJar {
+          dependencies {
+            exclude(dependency('junit:junit'))
+          }
+        }
+      """.trimIndent(),
+    )
+
+    assertCompositeExecutions {
+      containsOnly(
+        "my/",
+        mainClassEntry,
+        *manifestEntries,
+      )
+    }
+  }
+
+  @Test
+  fun minimizeChanged() {
+    taskPath = serverShadowJarTask
+
+    writeClientAndServerModules()
+    path("server/src/main/java/server/Server.java").writeText(
+      """
+        package server;
+        public class Server {}
+      """.trimIndent(),
+    )
+
+    assertCompositeExecutions(
+      jarPathProvider = { outputServerShadowJar },
+    ) {
+      containsOnly(
+        "client/",
+        "server/",
+        "client/Client.class",
+        "server/Server.class",
+        *junitEntries,
+        *manifestEntries,
+      )
+    }
+
+    path("server/build.gradle").appendText(
+      """
+        $shadowJar {
+          minimize {
+            exclude(dependency('junit:junit:.*'))
+          }
+        }
+      """.trimIndent(),
+    )
+
+    assertCompositeExecutions(
+      jarPathProvider = { outputServerShadowJar },
+    ) {
+      containsOnly(
+        "server/",
+        "server/Server.class",
+        *junitEntries,
+        *manifestEntries,
+      )
+    }
+  }
+
+  /**
+   * Ensure that we get a cache miss when relocation changes and that caching works with relocation
+   */
+  @Test
+  fun relocatorAdded() {
+    projectScriptPath.appendText(
+      """
+        dependencies {
+          implementation 'junit:junit:3.8.2'
+        }
+      """.trimIndent() + lineSeparator,
+    )
+    val mainClassEntry = writeClass(withImports = true)
+
+    assertCompositeExecutions {
+      containsOnly(
+        "my/",
+        mainClassEntry,
+        *junitEntries,
+        *manifestEntries,
+      )
+    }
+
+    projectScriptPath.appendText(
+      """
+        $shadowJar {
+          relocate 'junit.framework', 'foo.junit.framework'
+        }
+      """.trimIndent(),
+    )
+    val relocatedEntries = junitEntries
+      .map { it.replace("junit/framework/", "foo/junit/framework/") }.toTypedArray()
+
+    assertCompositeExecutions {
+      containsOnly(
+        "my/",
+        "foo/",
+        "foo/junit/",
+        mainClassEntry,
+        *relocatedEntries,
+        *manifestEntries,
+      )
+    }
+  }
+
+  @Test
+  fun serviceFileTransformerPropsChanged() {
+    val mainClassEntry = writeClass()
+    val assertions = {
+      assertCompositeExecutions {
+        containsOnly(
+          "my/",
+          mainClassEntry,
+          *manifestEntries,
+        )
+      }
+    }
+
+    assertions()
+
+    projectScriptPath.appendText(
+      transform<ServiceFileTransformer>(
+        transformerBlock = """
+          path = 'META-INF/foo'
+        """.trimIndent(),
+      ),
+    )
+
+    assertions()
+
+    val replaced = projectScriptPath.readText().replace("META-INF/foo", "META-INF/bar")
+    projectScriptPath.writeText(replaced)
+
+    assertions()
+  }
+
+  @Test
+  fun disableCacheIfAnyTransformerIsNotCacheable() {
+    projectScriptPath.appendText(
+      """
+        $shadowJar {
+          mergeServiceFiles()
+        }
+      """.trimIndent() + lineSeparator,
+    )
+
+    assertCompositeExecutions()
+
+    projectScriptPath.appendText(
+      """
+        $shadowJar {
+          mergeGroovyExtensionModules()
+        }
+      """.trimIndent() + lineSeparator,
+    )
+
+    assertCompositeExecutions()
+
+    projectScriptPath.appendText(
+      """
+        $shadowJar {
+          // Use Transformer.Companion (no-op) to mock a custom transformer here, it's not cacheable.
+          transform(${ResourceTransformer.Companion::class.java.name})
+        }
+      """.trimIndent(),
+    )
+
+    assertExecutionSuccess()
+    cleanOutputs()
+    // The shadowJar task should be executed again as the cache is disabled.
+    assertExecutionSuccess()
+  }
+
+  private fun cleanOutputs() {
+    run("clean")
+    @OptIn(ExperimentalPathApi::class)
+    val buildDirs = projectRoot.walk().filter { it.isDirectory() && it.name == "build" }
+    // Make sure build folders are deleted by clean task.
+    assertThat(buildDirs).isEmpty()
+  }
+
+  private fun assertExecutionSuccess() {
+    // The task was executed and not pulled from cache.
+    assertRunWithResult(TaskOutcome.SUCCESS)
+  }
+
+  /**
+   * This should be called after [assertExecutionSuccess] to ensure that the [taskPath] is cached.
+   */
+  private fun assertExecutionsFromCacheAndUpToDate() {
+    cleanOutputs()
+    // Run the task again to ensure it is pulled from cache.
+    assertRunWithResult(TaskOutcome.FROM_CACHE)
+    // Run the task again to ensure it is up-to-date.
+    assertRunWithResult(TaskOutcome.UP_TO_DATE)
+  }
+
+  /**
+   * Combines [assertExecutionSuccess] and [assertExecutionsFromCacheAndUpToDate] for simplifying assertions.
+   */
+  private fun assertCompositeExecutions(
+    jarPathProvider: () -> JarPath = { outputShadowJar },
+    jarPathAssertions: Assert<JarPath>.() -> Unit = {},
+  ) {
+    // First run should execute.
+    assertExecutionSuccess()
+    assertThat(jarPathProvider()).useAll(jarPathAssertions)
+    // Subsequent runs should be from cache and up-to-date after configurations changed.
+    assertExecutionsFromCacheAndUpToDate()
+  }
+
+  private fun assertRunWithResult(expectedOutcome: TaskOutcome) {
+    val result = run(taskPath)
+    assertThat(result).taskOutcomeEquals(taskPath, expectedOutcome)
+  }
+}
