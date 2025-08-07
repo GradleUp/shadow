3058c52b8932294fa450f7d41bc330fd3b39ced7
Goooler <wangzongler@gmail.com>
2025-08-07 18:15:09 +0800

    Test `errorWhenMainClassNotSet`

--- src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/ShadowApplicationPlugin.kt
+++ src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/ShadowApplicationPlugin.kt
@@ -129,7 +129,6 @@ public abstract class ShadowApplicationPlugin : Plugin<Project> {
   protected open fun Project.configureShadowJarMainClass() {
     val mainClassName = applicationExtension.mainClass
     tasks.shadowJar.configure { task ->
-      task.inputs.property("mainClassName", mainClassName)
       task.doFirst {
         // Inject the attribute if it is not already present.
         if (!task.manifest.attributes.contains(mainClassAttributeKey)) {
