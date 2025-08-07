ba2499c51df6d84b18360f2058927f576210104f
Goooler <wangzongler@gmail.com>
2025-08-07 21:43:19 +0800

    Remove `publishArtifactCD` related stuff in `FilteringCachingTest`

--- src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/caching/FilteringCachingTest.kt
+++ src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/caching/FilteringCachingTest.kt
@@ -1,65 +1,11 @@
 package com.github.jengelman.gradle.plugins.shadow.caching
 
-import assertk.Assert
 import com.github.jengelman.gradle.plugins.shadow.util.Issue
-import com.github.jengelman.gradle.plugins.shadow.util.JarPath
 import com.github.jengelman.gradle.plugins.shadow.util.containsOnly
 import kotlin.io.path.appendText
-import kotlin.io.path.readText
-import kotlin.io.path.writeText
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 
 class FilteringCachingTest : BaseCachingTest() {
-  @BeforeAll
-  override fun doFirst() {
-    super.doFirst()
-    publishArtifactCD()
-  }
-
-  @Test
-  fun dependencyExclusionsAffectUpToDateCheck() {
-    dependOnAndExcludeArtifactD()
-
-    assertCompositeExecutions {
-      commonAssertions()
-    }
-
-    val replaced = projectScriptPath.readText()
-      .replace("exclude(dependency('my:d:1.0'))", "exclude(dependency('my:c:1.0'))")
-    projectScriptPath.writeText(replaced)
-
-    assertCompositeExecutions {
-      containsOnly(
-        "d.properties",
-        *entriesInAB,
-        *manifestEntries,
-      )
-    }
-  }
-
-  @Test
-  fun projectExclusionsAffectUpToDateCheck() {
-    dependOnAndExcludeArtifactD()
-
-    assertCompositeExecutions {
-      commonAssertions()
-    }
-
-    val replaced = projectScriptPath.readText()
-      .replace("exclude(dependency('my:d:1.0'))", "exclude 'a.properties'")
-    projectScriptPath.writeText(replaced)
-
-    assertCompositeExecutions {
-      containsOnly(
-        "a2.properties",
-        "b.properties",
-        "c.properties",
-        "d.properties",
-        *manifestEntries,
-      )
-    }
-  }
 
   @Issue(
     "https://github.com/GradleUp/shadow/issues/717",
@@ -175,30 +121,5 @@ class FilteringCachingTest : BaseCachingTest() {
         *manifestEntries,
       )
     }
-  }
-
-  private fun dependOnAndExcludeArtifactD() {
-    projectScriptPath.appendText(
-      """
-        dependencies {
-          implementation 'my:a:1.0'
-          implementation 'my:b:1.0'
-          implementation 'my:d:1.0'
-        }
-        $shadowJar {
-          dependencies {
-            exclude(dependency('my:d:1.0'))
-          }
-        }
-      """.trimIndent(),
-    )
-  }
-
-  private fun Assert<JarPath>.commonAssertions() {
-    containsOnly(
-      "c.properties",
-      *entriesInAB,
-      *manifestEntries,
-    )
   }
 }
