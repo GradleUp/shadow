25b230498bc03f939c2b16f11afc5286b7f2c2ee
Goooler <wangzongler@gmail.com>
2025-08-07 19:08:15 +0800

    Test `shadowJarIsCachedCorrectlyAfterKotlinMainRunChanged`

--- src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/caching/ShadowJarCachingTest.kt
+++ src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/caching/ShadowJarCachingTest.kt
@@ -3,6 +3,7 @@ package com.github.jengelman.gradle.plugins.shadow.caching
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import com.github.jengelman.gradle.plugins.shadow.internal.MinimizeDependencyFilter
+import com.github.jengelman.gradle.plugins.shadow.internal.mainClassAttributeKey
 import com.github.jengelman.gradle.plugins.shadow.util.containsOnly
 import com.github.jengelman.gradle.plugins.shadow.util.getMainAttr
 import kotlin.io.path.appendText
@@ -178,6 +179,39 @@ class ShadowJarCachingTest : BaseCachingTest() {
     assertCompositeExecutions {
       getMainAttr("Foo").isEqualTo("Foo3")
       getMainAttr("Bar").isEqualTo("Bar3")
+    }
+  }
+
+  @Test
+  fun shadowJarIsCachedCorrectlyAfterKotlinMainRunChanged() {
+    val mainClassName = "my.Main"
+    val main2ClassName = "my.Main2"
+
+    val projectBuildScript = getDefaultProjectBuildScript(
+      plugin = "org.jetbrains.kotlin.multiplatform",
+      withGroup = true,
+      withVersion = true,
+    )
+    projectScriptPath.writeText(
+      """
+        $projectBuildScript
+        kotlin {
+          jvm().mainRun {
+            it.mainClass.set('$mainClassName')
+          }
+        }
+      """.trimIndent(),
+    )
+
+    assertCompositeExecutions {
+      getMainAttr(mainClassAttributeKey).isEqualTo(mainClassName)
+    }
+
+    val replaced = projectScriptPath.readText().replace(mainClassName, main2ClassName)
+    projectScriptPath.writeText(replaced)
+
+    assertCompositeExecutions {
+      getMainAttr(mainClassAttributeKey).isEqualTo(main2ClassName)
     }
   }
 }
