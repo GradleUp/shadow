e82de5acf1d96a5287efeebfb5d8590f3f81bd9c
Goooler <wangzongler@gmail.com>
2025-08-07 20:39:37 +0800

    Wrap `applicationExtension.mainClass` as a provider

--- src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/ShadowApplicationPlugin.kt
+++ src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/ShadowApplicationPlugin.kt
@@ -127,17 +127,18 @@ public abstract class ShadowApplicationPlugin : Plugin<Project> {
   }
 
   protected open fun Project.configureShadowJarMainClass() {
-    val mainClassName = applicationExtension.mainClass
+    // Wrap `mainClass` as a provider to avoid the error of it's value not being configured yet.
+    val mainClassName = provider { applicationExtension.mainClass }
     tasks.shadowJar.configure { task ->
       task.inputs.property("mainClassName", mainClassName)
       task.doFirst {
         // Inject the attribute if it is not already present.
         if (!task.manifest.attributes.contains(mainClassAttributeKey)) {
-          task.manifest.attributes[mainClassAttributeKey] = mainClassName.orNull.also { value ->
-            if (value.isNullOrEmpty()) {
-              error("The main class must be specified and not left empty in `application.mainClass` or manifest attributes.")
-            }
+          val realClass = mainClassName.get().orNull
+          if (realClass.isNullOrEmpty()) {
+            error("The main class must be specified and not left empty in `application.mainClass` or manifest attributes.")
           }
+          task.manifest.attributes[mainClassAttributeKey] = realClass
         }
       }
     }
