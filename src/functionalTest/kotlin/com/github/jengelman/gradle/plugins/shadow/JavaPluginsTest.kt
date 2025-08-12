5e97385e40d1b07d7c397945f4cf19731e3cde23
Goooler <wangzongler@gmail.com>
2025-08-12 22:30:55 +0800

    Revert "Update `excludeGradleApiByDefault`"
    This reverts commit 6192a1ddfb34185c2e06f0b9eaebb178c17cf5db.

--- src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/JavaPluginsTest.kt
+++ src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/JavaPluginsTest.kt
@@ -481,6 +481,14 @@ class JavaPluginsTest : BasePluginTest() {
   @ValueSource(booleans = [false, true])
   fun excludeGradleApiByDefault(legacy: Boolean) {
     writeGradlePluginModule(legacy)
+    projectScript.appendText(
+      """
+        dependencies {
+          implementation 'my:a:1.0'
+          compileOnly 'my:b:1.0'
+        }
+      """.trimIndent(),
+    )
 
     run(shadowJarPath)
 
@@ -497,6 +505,7 @@ class JavaPluginsTest : BasePluginTest() {
         "my/plugin/MyPlugin.class",
         "META-INF/gradle-plugins/",
         "META-INF/gradle-plugins/my.plugin.properties",
+        *entriesInA,
         *manifestEntries,
       )
     }
