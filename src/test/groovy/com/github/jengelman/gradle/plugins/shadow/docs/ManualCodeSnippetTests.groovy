13c7f52a386e65fc07b8e3c46ca718429f03c9a4
Goooler <wangzongler@gmail.com>
2025-08-13 21:08:22 +0800

    Disable ManualCodeSnippetTests

--- src/test/groovy/com/github/jengelman/gradle/plugins/shadow/docs/ManualCodeSnippetTests.groovy
+++ src/test/groovy/com/github/jengelman/gradle/plugins/shadow/docs/ManualCodeSnippetTests.groovy
@@ -8,6 +8,7 @@ import com.github.jengelman.gradle.plugins.shadow.docs.internal.snippets.TestCod
 import com.github.jengelman.gradle.plugins.shadow.docs.internal.snippets.executer.SnippetExecuter
 import com.github.jengelman.gradle.plugins.shadow.docs.internal.snippets.fixture.GroovyScriptFixture
 import com.google.common.base.StandardSystemProperty
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.TestFactory
 import org.junit.jupiter.api.io.TempDir
@@ -21,6 +22,7 @@ class ManualCodeSnippetTests {
             "groovy no-run": new NoopExecuter()
     ]
 
+    @Disabled("We have to run doc tests on main branch only")
     @TestFactory
     List<DynamicTest> provideDynamicTests(@TempDir Path tempDir) {
         File cwd = new File(StandardSystemProperty.USER_DIR.value())
