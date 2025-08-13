11dcb93d2bbebc86f2d7b252a87ed88b27361345
Goooler <wangzongler@gmail.com>
2025-08-13 14:20:56 +0800

    Update `doFirst`

--- src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/BasePluginTest.kt
+++ src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/BasePluginTest.kt
@@ -66,19 +66,23 @@ abstract class BasePluginTest {
     localRepo = AppendableMavenRepository(
       createTempDirectory().resolve("local-maven-repo").createDirectories(),
       runner(projectDir = null),
-    )
-    localRepo.jarModule("junit", "junit", "3.8.2") {
-      useJar(junitJar)
-    }.jarModule("my", "a", "1.0") {
-      buildJar {
-        insert("a.properties", "a")
-        insert("a2.properties", "a2")
+    ).apply {
+      jarModule("junit", "junit", "3.8.2") {
+        useJar(junitJar)
       }
-    }.jarModule("my", "b", "1.0") {
-      buildJar {
-        insert("b.properties", "b")
+      jarModule("my", "a", "1.0") {
+        buildJar {
+          insert("a.properties", "a")
+          insert("a2.properties", "a2")
+        }
       }
-    }.publish()
+      jarModule("my", "b", "1.0") {
+        buildJar {
+          insert("b.properties", "b")
+        }
+      }
+    }
+    localRepo.publish()
     localRepo.bomModule("my", "bom", "1.0") {
       addDependency("my", "a", "1.0")
       addDependency("my", "b", "1.0")
