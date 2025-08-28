37f53ae60c6862eea80f19c6a70347f547ba74c4
Goooler <wangzongler@gmail.com>
2025-08-28 15:19:28 +0800

    Cleanups

--- src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/transformers/ServiceFileTransformerTest.kt
+++ src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/transformers/ServiceFileTransformerTest.kt
@@ -8,6 +8,11 @@ import com.github.jengelman.gradle.plugins.shadow.util.getContent
 import kotlin.io.path.appendText
 import kotlin.io.path.writeText
 import org.gradle.api.file.DuplicatesStrategy
+import org.gradle.api.file.DuplicatesStrategy.EXCLUDE
+import org.gradle.api.file.DuplicatesStrategy.FAIL
+import org.gradle.api.file.DuplicatesStrategy.INCLUDE
+import org.gradle.api.file.DuplicatesStrategy.INHERIT
+import org.gradle.api.file.DuplicatesStrategy.WARN
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
@@ -221,8 +226,8 @@ class ServiceFileTransformerTest : BaseTransformerTest() {
   }
 
   @Test
-  fun strategyExcludeCanBeOverriddenByFilesMatching() {
-    writeDuplicatesStrategy(DuplicatesStrategy.EXCLUDE)
+  fun strategyCanBeOverriddenByFilesMatching() {
+    writeDuplicatesStrategy(EXCLUDE)
     projectScript.appendText(
       """
         $shadowJarTask {
@@ -242,8 +247,8 @@ class ServiceFileTransformerTest : BaseTransformerTest() {
   }
 
   @Test
-  fun strategyIncludeCanBeOverriddenByFilesNotMatching() {
-    writeDuplicatesStrategy(DuplicatesStrategy.INCLUDE)
+  fun strategyCanBeOverriddenByFilesNotMatching() {
+    writeDuplicatesStrategy(INCLUDE)
     projectScript.appendText(
       """
         $shadowJarTask {
@@ -307,35 +312,21 @@ class ServiceFileTransformerTest : BaseTransformerTest() {
   companion object {
     @JvmStatic
     fun withThrowingProvider() = listOf(
-      Arguments.of(
-        DuplicatesStrategy.FAIL,
-        "Cannot copy zip entry .* to .* because zip entry .* has already been copied there",
-      ),
-      Arguments.of(
-        DuplicatesStrategy.INHERIT,
-        "Entry .* is a duplicate but no duplicate handling strategy has been set",
-      ),
+      Arguments.of(FAIL, "Cannot copy zip entry .* to .* because zip entry .* has already been copied there"),
+      Arguments.of(INHERIT, "Entry .* is a duplicate but no duplicate handling strategy has been set"),
     )
 
     @JvmStatic
     fun withoutThrowingProvider() = listOf(
-      Arguments.of(DuplicatesStrategy.EXCLUDE, CONTENT_ONE, "one"),
-      Arguments.of(DuplicatesStrategy.INCLUDE, CONTENT_ONE_TWO, "one\ntwo"),
-      Arguments.of(DuplicatesStrategy.WARN, CONTENT_ONE_TWO, "one\ntwo"),
+      Arguments.of(EXCLUDE, CONTENT_ONE, "one"),
+      Arguments.of(INCLUDE, CONTENT_ONE_TWO, "one\ntwo"),
+      Arguments.of(WARN, CONTENT_ONE_TWO, "one\ntwo"),
     )
 
     @JvmStatic
     fun eachFileStrategyProvider() = listOf(
-      Arguments.of(
-        DuplicatesStrategy.EXCLUDE,
-        DuplicatesStrategy.INCLUDE,
-        ENTRY_SERVICES_SHADE,
-      ),
-      Arguments.of(
-        DuplicatesStrategy.INCLUDE,
-        DuplicatesStrategy.EXCLUDE,
-        ENTRY_SERVICES_FOO,
-      ),
+      Arguments.of(EXCLUDE, INCLUDE, ENTRY_SERVICES_SHADE),
+      Arguments.of(INCLUDE, EXCLUDE, ENTRY_SERVICES_FOO),
     )
   }
 }
