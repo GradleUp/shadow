11a56d2495761005f9acbc17a3e2e4e4f5c42b46
Goooler <wangzongler@gmail.com>
2025-10-09 11:48:04 +0800

    Suppress DEPRECATION

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,8 @@ kotlin {
   compilerOptions {
     allWarningsAsErrors = true
     // https://docs.gradle.org/current/userguide/compatibility.html#kotlin
-    apiVersion = KotlinVersion.KOTLIN_2_0 // TODO: upgrade to 2.2 when the min Gradle requirement is 9.0+
+    @Suppress("DEPRECATION") // TODO: upgrade to 2.2 when the min Gradle requirement is 9.0+
+    apiVersion = KotlinVersion.KOTLIN_2_0
     languageVersion = apiVersion
     jvmTarget = JvmTarget.fromTarget(libs.versions.jdkRelease.get())
     jvmDefault = JvmDefaultMode.NO_COMPATIBILITY
