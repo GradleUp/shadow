cba083d489446eb23fda25a7bfa59e3af93e32b1
Zongle Wang <wangzongler@gmail.com>
2025-11-12 15:12:02 +0800

    Prefer KotlinToolingVersion than KotlinVersion (#1841)

--- src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/internal/KotlinCompat.kt
+++ src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/internal/KotlinCompat.kt
@@ -2,6 +2,7 @@ package com.github.jengelman.gradle.plugins.shadow.internal
 
 import org.gradle.api.Project
 import org.jetbrains.kotlin.gradle.plugin.KotlinBasePlugin
+import org.jetbrains.kotlin.tooling.core.KotlinToolingVersion
 
 internal const val KOTLIN_MULTIPLATFORM_PLUGIN_ID = "org.jetbrains.kotlin.multiplatform"
 
@@ -9,12 +10,6 @@ internal fun Project.isAtLeastKgp(
   version: String,
   id: String = KOTLIN_MULTIPLATFORM_PLUGIN_ID,
 ): Boolean {
-  val (major, minor, patch) = version.normalizeVersion()
-  val (actualMajor, actualMinor, actualPatch) = (plugins.getPlugin(id) as KotlinBasePlugin).pluginVersion.normalizeVersion()
-  return KotlinVersion(actualMajor, actualMinor, actualPatch) >= KotlinVersion(major, minor, patch)
-}
-
-private fun String.normalizeVersion(): Triple<Int, Int, Int> {
-  val (major, minor, patch) = takeWhile { it != '-' }.split(".").map { it.toInt() }
-  return Triple(major, minor, patch)
+  val actual = (plugins.getPlugin(id) as KotlinBasePlugin).pluginVersion
+  return KotlinToolingVersion(actual) >= KotlinToolingVersion(version)
 }
