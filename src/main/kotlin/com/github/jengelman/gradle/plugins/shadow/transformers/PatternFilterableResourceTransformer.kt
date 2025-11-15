c344e1603ee298218e8e4f845bcb208d6bcfdeeb
Goooler <wangzongler@gmail.com>
2025-11-15 21:36:16 +0800

    Override `canTransformResource`

--- src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/transformers/PatternFilterableResourceTransformer.kt
+++ src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/transformers/PatternFilterableResourceTransformer.kt
@@ -1,5 +1,6 @@
 package com.github.jengelman.gradle.plugins.shadow.transformers
 
+import org.gradle.api.file.FileTreeElement
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.util.PatternFilterable
@@ -7,8 +8,12 @@ import org.gradle.api.tasks.util.PatternSet
 
 public abstract class PatternFilterableResourceTransformer(
   @Internal public val patternSet: PatternSet,
-) : ResourceTransformer,
+) : ResourceTransformer by ResourceTransformer.Companion,
   PatternFilterable by patternSet {
+
+  override fun canTransformResource(element: FileTreeElement): Boolean {
+    return patternSet.asSpec.isSatisfiedBy(element)
+  }
 
   @Input // Trigger task executions after includes changed.
   override fun getIncludes(): MutableSet<String> = patternSet.includes
--- src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/transformers/ServiceFileTransformer.kt
+++ src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/transformers/ServiceFileTransformer.kt
@@ -4,10 +4,7 @@ import com.github.jengelman.gradle.plugins.shadow.internal.zipEntry
 import com.github.jengelman.gradle.plugins.shadow.relocation.relocateClass
 import com.github.jengelman.gradle.plugins.shadow.transformers.GroovyExtensionModuleTransformer.Companion.PATH_LEGACY_GROOVY_EXTENSION_MODULE_DESCRIPTOR
 import org.apache.tools.zip.ZipOutputStream
-import org.gradle.api.file.FileTreeElement
-import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Internal
-import org.gradle.api.tasks.util.PatternFilterable
 import org.gradle.api.tasks.util.PatternSet
 
 /**
@@ -38,10 +35,6 @@ public open class ServiceFileTransformer(
       field = value
       patternSet.setIncludes(listOf("$value/**"))
     }
-
-  override fun canTransformResource(element: FileTreeElement): Boolean {
-    return patternSet.asSpec.isSatisfiedBy(element)
-  }
 
   override fun transform(context: TransformerContext) {
     val resource = path + "/" +
