6b6143b40b9d215fa06f556a10ff23b738cd6493
Robert Stupp <snazy@snazy.de>
2025-11-15 12:42:45 +0100

    Add transformer to merge licenses into a single license file

---
+++ src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/transformers/MergeLicenseResourceTransformer.kt
@@ -0,0 +1,171 @@
+package com.github.jengelman.gradle.plugins.shadow.transformers
+
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowCopyAction
+import java.io.OutputStream
+import java.nio.charset.StandardCharsets.UTF_8
+import java.util.LinkedHashSet
+import javax.inject.Inject
+import org.apache.tools.zip.ZipEntry
+import org.apache.tools.zip.ZipOutputStream
+import org.gradle.api.file.FileTreeElement
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
+import org.gradle.api.tasks.util.PatternSet
+
+/**
+ * Generates a license file using the configured license text source.
+ *
+ * A mandatory `SPDX-License-Identifier` is placed in front of the license text to avoid ambiguous
+ * license detection by license-detection-tools.
+ *
+ * License texts found in the files names `META-INF/LICENSE`, `META-INF/LICENSE.txt`,
+ * `META-INF/LICENSE.md`, `LICENSE`, `LICENSE.txt`, `LICENSE.md` are included from the shadow jar
+ * sources. Use the [PatternFilterable][org.gradle.api.tasks.util.PatternFilterable] functions to
+ * specify a different set of files to include, the paths mentioned above are then not considered
+ * unless explicitly included.
+ */
+@Suppress("unused")
+@CacheableTransformer
+public open class MergeLicenseResourceTransformer(
+  objectFactory: ObjectFactory,
+  patternSet: PatternSet,
+) : PatternFilterableResourceTransformer(patternSet) {
+  private val initializePatternSet by lazy {
+    if (patternSet.isEmpty) {
+      includeDefaults()
+    }
+  }
+
+  @get:Internal
+  internal val elements: MutableSet<String> = LinkedHashSet()
+
+  public fun includeDefaults(): MergeLicenseResourceTransformer {
+    patternSet.include(
+      "META-INF/LICENSE",
+      "META-INF/LICENSE.txt",
+      "META-INF/LICENSE.md",
+      "LICENSE",
+      "LICENSE.txt",
+      "LICENSE.md",
+    )
+    return this
+  }
+
+  @Input
+  override fun getIncludes(): MutableSet<String> = patternSet.includes
+
+  @Input
+  override fun getExcludes(): MutableSet<String> = patternSet.excludes
+
+  /** Path to write the aggregated license file to. Defaults to `META-INF/LICENSE`. */
+  @get:Input
+  public val outputPath: Property<String> =
+    objectFactory.property(String::class.java).value("META-INF/LICENSE")
+
+  /**
+   * The generated license file is potentially a collection of multiple license texts. To avoid
+   * ambiguous license detection by license-detection-tools, an SPDX license identifier header
+   * (`SPDX-License-Identifier:`) is added at the beginning of the generated file if the value of
+   * this property is present and not empty. Defaults to `Apache-2.0`.
+   */
+  @get:Input
+  public val artifactLicenseSpdxId: Property<String> =
+    objectFactory.property(String::class.java).value("Apache-2.0")
+
+  /** Path to the project's license text, this property *must* be configured. */
+  @get:InputFile
+  @get:PathSensitive(PathSensitivity.RELATIVE)
+  public val artifactLicense: RegularFileProperty = objectFactory.fileProperty()
+
+  /**
+   * Separator between the project's license text and license texts from the included dependencies.
+   */
+  @get:Input
+  public val firstSeparator: Property<String> =
+    objectFactory
+      .property(String::class.java)
+      .value(
+        """
+
+        ${"-".repeat(120)}
+
+        This artifact includes dependencies with the following licenses:
+        ----------------------------------------------------------------
+
+        """
+          .trimIndent(),
+      )
+
+  /** Separator between included dependency license texts. */
+  @get:Input
+  public val separator: Property<String> =
+    objectFactory.property(String::class.java).value("\n${"-".repeat(120)}\n")
+
+  @Inject
+  public constructor(objectFactory: ObjectFactory) : this(
+    objectFactory,
+    patternSet = PatternSet(),
+  )
+
+  override fun canTransformResource(element: FileTreeElement): Boolean {
+    // Init once before patternSpec is accessed.
+    initializePatternSet
+    return super.canTransformResource(element)
+  }
+
+  override fun transform(context: TransformerContext) {
+    transformInternal(context.inputStream.readAllBytes())
+  }
+
+  internal fun transformInternal(bytes: ByteArray) {
+    val content = bytes.toString(UTF_8).trim('\n', '\r')
+    if (!content.isEmpty()) {
+      elements.add(content)
+    }
+  }
+
+  override fun hasTransformedResource(): Boolean = true
+
+  override fun modifyOutputStream(os: ZipOutputStream, preserveFileTimestamps: Boolean) {
+    os.putNextEntry(
+      ZipEntry(outputPath.get()).apply { time = ShadowCopyAction.CONSTANT_TIME_FOR_ZIP_ENTRIES },
+    )
+
+    writeLicenseFile(os)
+
+    os.closeEntry()
+  }
+
+  internal fun writeLicenseFile(os: OutputStream) {
+    if (artifactLicenseSpdxId.isPresent) {
+      val spdxId = artifactLicenseSpdxId.get()
+      if (spdxId.isBlank()) {
+        os.write("SPDX-License-Identifier: $spdxId\n".toByteArray(UTF_8))
+      }
+    }
+    os.write(artifactLicense.get().asFile.readBytes())
+
+    if (!elements.isEmpty()) {
+      os.write("\n".toByteArray(UTF_8))
+      os.write(firstSeparator.get().toByteArray(UTF_8))
+      os.write("\n".toByteArray(UTF_8))
+
+      var first = true
+      val separator = (this.separator.get() + "\n").toByteArray(UTF_8)
+      for (element in elements) {
+        if (!first) {
+          os.write("\n".toByteArray(UTF_8))
+          os.write(separator)
+        }
+        os.write(element.toByteArray(UTF_8))
+        first = false
+      }
+    }
+  }
+}
---
+++ src/test/kotlin/com/github/jengelman/gradle/plugins/shadow/transformers/MergeLicenseResourceTransformerTest.kt
@@ -0,0 +1,135 @@
+package com.github.jengelman.gradle.plugins.shadow.transformers
+
+import assertk.assertThat
+import assertk.assertions.containsExactlyInAnyOrder
+import assertk.assertions.isEmpty
+import assertk.assertions.isEqualTo
+import assertk.assertions.isFalse
+import assertk.assertions.isNotEqualTo
+import assertk.assertions.isTrue
+import java.io.ByteArrayOutputStream
+import java.nio.file.Path
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+
+class MergeLicenseResourceTransformerTest : BaseTransformerTest<MergeLicenseResourceTransformer>() {
+  @Test
+  fun defaultIncludes() {
+    with(transformer) {
+      assertThat(canTransformResource("META-INF/LICENSE")).isTrue()
+      assertThat(canTransformResource("META-INF/LICENSE.txt")).isTrue()
+      assertThat(canTransformResource("META-INF/LICENSE.md")).isTrue()
+      assertThat(canTransformResource("LICENSE")).isTrue()
+      assertThat(canTransformResource("LICENSE.txt")).isTrue()
+      assertThat(canTransformResource("LICENSE.md")).isTrue()
+      assertThat(canTransformResource("something else")).isFalse()
+    }
+  }
+
+  @Test
+  fun customIncludes() {
+    with(transformer) {
+      include("META-INF/FOO")
+      assertThat(canTransformResource("META-INF/FOO")).isTrue()
+      assertThat(canTransformResource("META-INF/LICENSE")).isFalse()
+      assertThat(canTransformResource("META-INF/LICENSE.txt")).isFalse()
+      assertThat(canTransformResource("META-INF/LICENSE.md")).isFalse()
+      assertThat(canTransformResource("LICENSE")).isFalse()
+      assertThat(canTransformResource("LICENSE.txt")).isFalse()
+      assertThat(canTransformResource("LICENSE.md")).isFalse()
+      assertThat(canTransformResource("something else")).isFalse()
+    }
+  }
+
+  @Test
+  fun customIncludesWithDefaults() {
+    with(transformer) {
+      include("META-INF/FOO")
+      includeDefaults()
+      assertThat(canTransformResource("META-INF/FOO")).isTrue()
+      assertThat(canTransformResource("META-INF/LICENSE")).isTrue()
+      assertThat(canTransformResource("META-INF/LICENSE.txt")).isTrue()
+      assertThat(canTransformResource("META-INF/LICENSE.md")).isTrue()
+      assertThat(canTransformResource("LICENSE")).isTrue()
+      assertThat(canTransformResource("LICENSE.txt")).isTrue()
+      assertThat(canTransformResource("LICENSE.md")).isTrue()
+      assertThat(canTransformResource("something else")).isFalse()
+    }
+  }
+
+  @Test
+  fun deduplicateLicenseTexts(@TempDir tempDir: Path) {
+    with(transformer) {
+      transformInternal("license one".toByteArray())
+      transformInternal("\r\nlicense one\r\n".toByteArray())
+      transformInternal("\nlicense one\n".toByteArray())
+      transformInternal("   license two".toByteArray())
+      transformInternal("\r\n\n\r\n\n   license two".toByteArray())
+      transformInternal("   license two\r\n\n\r\n\n".toByteArray())
+      transformInternal("license three".toByteArray())
+
+      val artifactLicenseFile = tempDir.resolve("artifact-license").toFile()
+      artifactLicenseFile.writeText("artifact license file content")
+      artifactLicense.set(artifactLicenseFile)
+
+      assertThat(elements).containsExactlyInAnyOrder("license one", "   license two", "license three")
+
+      val baos = ByteArrayOutputStream()
+      writeLicenseFile(baos)
+      val written = baos.toByteArray().toString(Charsets.UTF_8).lines()
+      assertThat(written).isEqualTo(
+        listOf(
+          "artifact license file content",
+        ) + firstSeparator.get().lines() +
+          "license one" +
+          separator.get().lines() +
+          "   license two" +
+          separator.get().lines() +
+          "license three",
+      )
+    }
+  }
+
+  @Test
+  fun singleAdditionalLicense(@TempDir tempDir: Path) {
+    with(transformer) {
+      transformInternal("license one".toByteArray())
+
+      val artifactLicenseFile = tempDir.resolve("artifact-license").toFile()
+      artifactLicenseFile.writeText("artifact license file content")
+      artifactLicense.set(artifactLicenseFile)
+
+      assertThat(elements).containsExactlyInAnyOrder("license one")
+
+      val baos = ByteArrayOutputStream()
+      writeLicenseFile(baos)
+      val written = baos.toByteArray().toString(Charsets.UTF_8).lines()
+      assertThat(written).isEqualTo(
+        listOf(
+          "artifact license file content",
+        ) + firstSeparator.get().lines() +
+          "license one",
+      )
+    }
+  }
+
+  @Test
+  fun noAdditionalLicenses(@TempDir tempDir: Path) {
+    with(transformer) {
+      val artifactLicenseFile = tempDir.resolve("artifact-license").toFile()
+      artifactLicenseFile.writeText("artifact license file content")
+      artifactLicense.set(artifactLicenseFile)
+
+      assertThat(elements).isEmpty()
+
+      val baos = ByteArrayOutputStream()
+      writeLicenseFile(baos)
+      val written = baos.toByteArray().toString(Charsets.UTF_8).lines()
+      assertThat(written).isEqualTo(
+        listOf(
+          "artifact license file content",
+        ),
+      )
+    }
+  }
+}
