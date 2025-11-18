b483f1bdc4330e4e8733649675ae1df1e903739c
Goooler <wangzongler@gmail.com>
2025-11-18 16:14:24 +0800

    Remove `mergePropertiesWithDifferentStrategiesEXPERIMENT`

--- src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/transformers/PropertiesFileTransformerTest.kt
+++ src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/transformers/PropertiesFileTransformerTest.kt
@@ -39,58 +39,12 @@ class PropertiesFileTransformerTest : BaseTransformerTest() {
       val result = runWithFailure(shadowJarPath)
 
       assertThat(result).taskOutcomeEquals(shadowJarPath, FAILED)
-      assertThat(result.output).contains(
-        "Execution failed for task ':shadowJar'.",
-        "> The following properties files have conflicting property values and cannot be merged:",
-        "   * META-INF/test.properties",
-        "     * Property key2 is duplicated 2 times with different values",
-      )
-    } else {
-      runWithSuccess(shadowJarPath)
-
-      val expected = when (strategy) {
-        MergeStrategy.First -> arrayOf("key1=one", "key2=one", "key3=two")
-        MergeStrategy.Latest -> arrayOf("key1=one", "key2=two", "key3=two")
-        MergeStrategy.Append -> arrayOf("key1=one", "key2=one;two", "key3=two")
-        else -> fail("Unexpected strategy: $strategy")
-      }
-      val content = outputShadowedJar.use { it.getContent("META-INF/test.properties") }
-      assertThat(content).contains(*expected)
-    }
-  }
-
-  @ParameterizedTest
-  @EnumSource(MergeStrategy::class)
-  fun mergePropertiesWithDifferentStrategiesEXPERIMENT(strategy: MergeStrategy) {
-    val one = buildJarOne {
-      insert("META-INF/test.properties", "key1=one\nkey2=one")
-    }
-    val two = buildJarTwo {
-      insert("META-INF/test.properties", "key2=two\nkey3=two")
-    }
-    projectScript.appendText(
-      transform<PropertiesFileTransformer>(
-        dependenciesBlock = implementationFiles(one, two),
-        transformerBlock = """
-          mergeStrategy = $mergeStrategyClassName.$strategy
-          mergeSeparator = ";"
-          paths = ["META-INF/test.properties"]
-        """.trimIndent(),
-      ),
-    )
-
-    if (strategy == MergeStrategy.Fail) {
-      val result = runWithFailure(shadowJarPath)
-
-      assertThat(result).taskOutcomeEquals(shadowJarPath, FAILED)
-      assertThat(result.output).contains(
-        // TODO EXPERIMENT
+      assertThat(result.output.replace(lineSeparator, "\n")).contains(
         """
-          Execution failed for task ':shadowJar'.
-          > The following properties files have conflicting property values and cannot be merged:
-             * META-INF/test.properties
-               * Property key2 is duplicated 2 times with different values
-        """.trimIndent().replace("\n", System.lineSeparator()),
+          Caused by: java.lang.IllegalStateException: The following properties files have conflicting property values and cannot be merged:
+           * META-INF/test.properties
+             * Property key2 is duplicated 2 times with different values
+        """.trimIndent(),
       )
     } else {
       runWithSuccess(shadowJarPath)
