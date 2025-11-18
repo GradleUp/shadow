13e4c0be729847095624be400e52819204a601bc
Goooler <wangzongler@gmail.com>
2025-11-18 17:33:58 +0800

    Use `variantSeparatorsPathString`

--- src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/FindResourceInClasspathTest.kt
+++ src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/FindResourceInClasspathTest.kt
@@ -4,7 +4,7 @@ import assertk.all
 import assertk.assertThat
 import assertk.assertions.contains
 import assertk.assertions.doesNotContain
-import com.github.jengelman.gradle.plugins.shadow.util.invariantSeparatorsPathString
+import com.github.jengelman.gradle.plugins.shadow.util.variantSeparatorsPathString
 import kotlin.io.path.appendText
 import org.junit.jupiter.api.Test
 
@@ -33,20 +33,20 @@ class FindResourceInClasspathTest : BasePluginTest() {
     assertThat(runWithSuccess(":find1").output).contains(
       "> Task :find1",
       "scanning ",
-      "/my/a/1.0/a-1.0.jar".invariantSeparatorsPathString,
-      "/a.properties".invariantSeparatorsPathString,
-      "/a2.properties".invariantSeparatorsPathString,
+      "/my/a/1.0/a-1.0.jar".variantSeparatorsPathString,
+      "/a.properties".variantSeparatorsPathString,
+      "/a2.properties".variantSeparatorsPathString,
     )
 
     assertThat(runWithSuccess(":find2").output).all {
       contains(
         "> Task :find2",
         "scanning ",
-        "/my/a/1.0/a-1.0.jar".invariantSeparatorsPathString,
-        "/a.properties".invariantSeparatorsPathString,
+        "/my/a/1.0/a-1.0.jar".variantSeparatorsPathString,
+        "/a.properties".variantSeparatorsPathString,
       )
       doesNotContain(
-        "/a2.properties".invariantSeparatorsPathString,
+        "/a2.properties".variantSeparatorsPathString,
       )
     }
 
@@ -54,11 +54,11 @@ class FindResourceInClasspathTest : BasePluginTest() {
       contains(
         "> Task :find3",
         "scanning ",
-        "/my/a/1.0/a-1.0.jar".invariantSeparatorsPathString,
-        "/a2.properties".invariantSeparatorsPathString,
+        "/my/a/1.0/a-1.0.jar".variantSeparatorsPathString,
+        "/a2.properties".variantSeparatorsPathString,
       )
       doesNotContain(
-        "/a.properties".invariantSeparatorsPathString,
+        "/a.properties".variantSeparatorsPathString,
       )
     }
   }
--- src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/util/Paths.kt
+++ src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/util/Paths.kt
@@ -1,6 +1,6 @@
 package com.github.jengelman.gradle.plugins.shadow.util
 
-import java.io.File
+import java.nio.file.FileSystems
 import java.nio.file.Path
 import kotlin.io.path.readText
 import kotlin.io.path.writeText
@@ -9,4 +9,6 @@ fun Path.prependText(text: String) = writeText(text + readText())
 
 val String.invariantEolString: String get() = replace(System.lineSeparator(), "\n")
 
-val String.invariantSeparatorsPathString: String get() = replace(File.separator, "/")
+val String.variantSeparatorsPathString: String get() = replace("/", fileSystem.separator)
+
+private val fileSystem = FileSystems.getDefault()
