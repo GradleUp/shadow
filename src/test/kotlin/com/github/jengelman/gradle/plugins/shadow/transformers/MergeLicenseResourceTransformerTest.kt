ff0a30b1b88632b44e155503a61ffb4aa6b1b83a
Zongle Wang <wangzongler@gmail.com>
2025-11-21 10:51:20 +0800

    Reduce nesting for using transformer assertions (#1873)

--- src/test/kotlin/com/github/jengelman/gradle/plugins/shadow/transformers/MergeLicenseResourceTransformerTest.kt
+++ src/test/kotlin/com/github/jengelman/gradle/plugins/shadow/transformers/MergeLicenseResourceTransformerTest.kt
@@ -12,135 +12,123 @@ import org.junit.jupiter.api.io.TempDir
 
 class MergeLicenseResourceTransformerTest : BaseTransformerTest<MergeLicenseResourceTransformer>() {
   @Test
-  fun defaultIncludes() {
-    with(transformer) {
-      assertThat(canTransformResource("META-INF/LICENSE")).isTrue()
-      assertThat(canTransformResource("META-INF/LICENSE.txt")).isTrue()
-      assertThat(canTransformResource("META-INF/LICENSE.md")).isTrue()
-      assertThat(canTransformResource("LICENSE")).isTrue()
-      assertThat(canTransformResource("LICENSE.txt")).isTrue()
-      assertThat(canTransformResource("LICENSE.md")).isTrue()
-      assertThat(canTransformResource("something else")).isFalse()
-    }
+  fun defaultIncludes() = with(transformer) {
+    assertThat(canTransformResource("META-INF/LICENSE")).isTrue()
+    assertThat(canTransformResource("META-INF/LICENSE.txt")).isTrue()
+    assertThat(canTransformResource("META-INF/LICENSE.md")).isTrue()
+    assertThat(canTransformResource("LICENSE")).isTrue()
+    assertThat(canTransformResource("LICENSE.txt")).isTrue()
+    assertThat(canTransformResource("LICENSE.md")).isTrue()
+    assertThat(canTransformResource("something else")).isFalse()
   }
 
   @Test
-  fun customIncludes() {
-    with(transformer) {
-      include("META-INF/FOO")
-      exclude("META-INF/LICENSE*")
-      exclude("LICENSE*")
-      assertThat(canTransformResource("META-INF/FOO")).isTrue()
-      assertThat(canTransformResource("META-INF/LICENSE")).isFalse()
-      assertThat(canTransformResource("META-INF/LICENSE.txt")).isFalse()
-      assertThat(canTransformResource("META-INF/LICENSE.md")).isFalse()
-      assertThat(canTransformResource("LICENSE")).isFalse()
-      assertThat(canTransformResource("LICENSE.txt")).isFalse()
-      assertThat(canTransformResource("LICENSE.md")).isFalse()
-      assertThat(canTransformResource("something else")).isFalse()
-    }
+  fun customIncludes() = with(transformer) {
+    include("META-INF/FOO")
+    exclude("META-INF/LICENSE*")
+    exclude("LICENSE*")
+    assertThat(canTransformResource("META-INF/FOO")).isTrue()
+    assertThat(canTransformResource("META-INF/LICENSE")).isFalse()
+    assertThat(canTransformResource("META-INF/LICENSE.txt")).isFalse()
+    assertThat(canTransformResource("META-INF/LICENSE.md")).isFalse()
+    assertThat(canTransformResource("LICENSE")).isFalse()
+    assertThat(canTransformResource("LICENSE.txt")).isFalse()
+    assertThat(canTransformResource("LICENSE.md")).isFalse()
+    assertThat(canTransformResource("something else")).isFalse()
   }
 
   @Test
-  fun deduplicateLicenseTexts(@TempDir tempDir: Path) {
-    with(transformer) {
-      transformInternal("license one".toByteArray())
-      transformInternal("\r\nlicense one\r\n".toByteArray())
-      transformInternal("\nlicense one\n".toByteArray())
-      transformInternal("   license two".toByteArray())
-      transformInternal("\r\n\n\r\n\n   license two".toByteArray())
-      transformInternal("   license two\r\n\n\r\n\n".toByteArray())
-      transformInternal("license three".toByteArray())
+  fun deduplicateLicenseTexts(@TempDir tempDir: Path) = with(transformer) {
+    transformInternal("license one".toByteArray())
+    transformInternal("\r\nlicense one\r\n".toByteArray())
+    transformInternal("\nlicense one\n".toByteArray())
+    transformInternal("   license two".toByteArray())
+    transformInternal("\r\n\n\r\n\n   license two".toByteArray())
+    transformInternal("   license two\r\n\n\r\n\n".toByteArray())
+    transformInternal("license three".toByteArray())
 
-      val artifactLicenseFile = tempDir.resolve("artifact-license").toFile()
-      artifactLicenseFile.writeText("artifact license file content")
-      artifactLicense.set(artifactLicenseFile)
+    val artifactLicenseFile = tempDir.resolve("artifact-license").toFile()
+    artifactLicenseFile.writeText("artifact license file content")
+    artifactLicense.set(artifactLicenseFile)
 
-      assertThat(elements).containsExactlyInAnyOrder("license one", "   license two", "license three")
+    assertThat(elements).containsExactlyInAnyOrder("license one", "   license two", "license three")
 
-      assertThat(buildLicense()).isEqualTo(
-        """
-          SPDX-License-Identifier: Apache-2.0
-          artifact license file content
+    assertThat(buildLicense()).isEqualTo(
+      """
+        SPDX-License-Identifier: Apache-2.0
+        artifact license file content
 
-          ------------------------------------------------------------------------------------------------------------------------
+        ------------------------------------------------------------------------------------------------------------------------
 
-          This artifact includes dependencies with the following licenses:
-          ----------------------------------------------------------------
+        This artifact includes dependencies with the following licenses:
+        ----------------------------------------------------------------
 
-          license one
+        license one
 
-          ------------------------------------------------------------------------------------------------------------------------
+        ------------------------------------------------------------------------------------------------------------------------
 
-             license two
+           license two
 
-          ------------------------------------------------------------------------------------------------------------------------
+        ------------------------------------------------------------------------------------------------------------------------
 
-          license three
-        """.trimIndent(),
-      )
-    }
+        license three
+      """.trimIndent(),
+    )
   }
 
   @Test
-  fun singleAdditionalLicense(@TempDir tempDir: Path) {
-    with(transformer) {
-      transformInternal("license one".toByteArray())
+  fun singleAdditionalLicense(@TempDir tempDir: Path) = with(transformer) {
+    transformInternal("license one".toByteArray())
 
-      val artifactLicenseFile = tempDir.resolve("artifact-license").toFile()
-      artifactLicenseFile.writeText("artifact license file content")
-      artifactLicense.set(artifactLicenseFile)
+    val artifactLicenseFile = tempDir.resolve("artifact-license").toFile()
+    artifactLicenseFile.writeText("artifact license file content")
+    artifactLicense.set(artifactLicenseFile)
 
-      assertThat(elements).containsExactlyInAnyOrder("license one")
+    assertThat(elements).containsExactlyInAnyOrder("license one")
 
-      assertThat(buildLicense()).isEqualTo(
-        """
-          SPDX-License-Identifier: Apache-2.0
-          artifact license file content
+    assertThat(buildLicense()).isEqualTo(
+      """
+        SPDX-License-Identifier: Apache-2.0
+        artifact license file content
 
-          ------------------------------------------------------------------------------------------------------------------------
+        ------------------------------------------------------------------------------------------------------------------------
 
-          This artifact includes dependencies with the following licenses:
-          ----------------------------------------------------------------
+        This artifact includes dependencies with the following licenses:
+        ----------------------------------------------------------------
 
-          license one
-        """.trimIndent(),
-      )
-    }
+        license one
+      """.trimIndent(),
+    )
   }
 
   @Test
-  fun noAdditionalLicenses(@TempDir tempDir: Path) {
-    with(transformer) {
-      val artifactLicenseFile = tempDir.resolve("artifact-license").toFile()
-      artifactLicenseFile.writeText("artifact license file content")
-      artifactLicense.set(artifactLicenseFile)
+  fun noAdditionalLicenses(@TempDir tempDir: Path) = with(transformer) {
+    val artifactLicenseFile = tempDir.resolve("artifact-license").toFile()
+    artifactLicenseFile.writeText("artifact license file content")
+    artifactLicense.set(artifactLicenseFile)
 
-      assertThat(elements).isEmpty()
+    assertThat(elements).isEmpty()
 
-      assertThat(buildLicense()).isEqualTo(
-        """
-          SPDX-License-Identifier: Apache-2.0
-          artifact license file content
-        """.trimIndent(),
-      )
-    }
+    assertThat(buildLicense()).isEqualTo(
+      """
+        SPDX-License-Identifier: Apache-2.0
+        artifact license file content
+      """.trimIndent(),
+    )
   }
 
   @Test
-  fun noSpdxId(@TempDir tempDir: Path) {
-    with(transformer) {
-      artifactLicenseSpdxId.unsetConvention()
+  fun noSpdxId(@TempDir tempDir: Path) = with(transformer) {
+    artifactLicenseSpdxId.unsetConvention()
 
-      val artifactLicenseFile = tempDir.resolve("artifact-license").toFile()
-      artifactLicenseFile.writeText("artifact license file content")
-      artifactLicense.set(artifactLicenseFile)
+    val artifactLicenseFile = tempDir.resolve("artifact-license").toFile()
+    artifactLicenseFile.writeText("artifact license file content")
+    artifactLicense.set(artifactLicenseFile)
 
-      assertThat(elements).isEmpty()
+    assertThat(elements).isEmpty()
 
-      assertThat(buildLicense()).isEqualTo(
-        "artifact license file content",
-      )
-    }
+    assertThat(buildLicense()).isEqualTo(
+      "artifact license file content",
+    )
   }
 }
