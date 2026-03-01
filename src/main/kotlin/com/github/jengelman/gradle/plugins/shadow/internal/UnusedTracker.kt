45c8590c6097d9b380db4de7935ce58b8bf6d51d
Goooler <wangzongler@gmail.com>
2026-03-01 21:59:53 +0800

    New `Project.getApiJars`

--- src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/internal/UnusedTracker.kt
+++ src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/internal/UnusedTracker.kt
@@ -10,9 +10,42 @@ import org.gradle.api.attributes.LibraryElements
 import org.gradle.api.attributes.Usage
 import org.gradle.api.file.FileCollection
 import org.gradle.api.plugins.JavaPlugin.API_CONFIGURATION_NAME
+import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.InputFiles
 import org.vafer.jdependency.Clazzpath
 import org.vafer.jdependency.ClazzpathUnit
+
+internal fun Project.getApiJars(): Provider<List<File>> {
+  val apiConfiguration =
+    configurations.findByName(API_CONFIGURATION_NAME) ?: return provider { emptyList() }
+
+  val shadowApiConfig =
+    configurations.register(
+      @OptIn(ExperimentalUuidApi::class)
+      "shadowMinimizeApi_${Uuid.random().toString().substring(0, 8)}"
+    ) {
+      it.isCanBeResolved = true
+      it.isCanBeConsumed = false
+      it.attributes { attrs ->
+        attrs.attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage::class.java, Usage.JAVA_API))
+        attrs.attribute(
+          Category.CATEGORY_ATTRIBUTE,
+          objects.named(Category::class.java, Category.LIBRARY),
+        )
+        attrs.attribute(
+          LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE,
+          objects.named(LibraryElements::class.java, LibraryElements.JAR),
+        )
+      }
+      it.extendsFrom(apiConfiguration)
+    }
+
+  return shadowApiConfig.map { shadowApi ->
+    shadowApi.resolvedConfiguration.resolvedArtifacts
+      .filter { artifact -> artifact.id.componentIdentifier !is ModuleComponentIdentifier }
+      .map { it.file }
+  }
+}
 
 /** Tracks unused classes in the project classpath. */
 internal class UnusedTracker(
@@ -41,45 +74,6 @@ internal class UnusedTracker(
   fun addDependency(jarOrDir: File) {
     if (toMinimize.contains(jarOrDir)) {
       cp.addClazzpathUnit(jarOrDir)
-    }
-  }
-
-  companion object {
-    fun getApiJarsFromProject(project: Project): FileCollection {
-      val apiConfiguration =
-        project.configurations.findByName(API_CONFIGURATION_NAME) ?: return project.files()
-
-      val shadowApiConfig =
-        project.configurations.register(
-          @OptIn(ExperimentalUuidApi::class)
-          "shadowMinimizeApi_${Uuid.random().toString().substring(0, 8)}"
-        ) {
-          it.isCanBeResolved = true
-          it.isCanBeConsumed = false
-          it.attributes { attrs ->
-            attrs.attribute(
-              Usage.USAGE_ATTRIBUTE,
-              project.objects.named(Usage::class.java, Usage.JAVA_API),
-            )
-            attrs.attribute(
-              Category.CATEGORY_ATTRIBUTE,
-              project.objects.named(Category::class.java, Category.LIBRARY),
-            )
-            attrs.attribute(
-              LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE,
-              project.objects.named(LibraryElements::class.java, LibraryElements.JAR),
-            )
-          }
-          it.extendsFrom(apiConfiguration)
-        }
-
-      return project.files(
-        shadowApiConfig.map { shadowApi ->
-          shadowApi.resolvedConfiguration.resolvedArtifacts
-            .filter { artifact -> artifact.id.componentIdentifier !is ModuleComponentIdentifier }
-            .map { it.file }
-        }
-      )
     }
   }
 }
