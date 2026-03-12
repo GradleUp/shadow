5095baa69eadd1e6ebe49f3a9133a961627fc754
Goooler <wangzongler@gmail.com>
2026-03-12 15:59:58 +0800

    Rename RelocatorRemapperTest

--- src/test/kotlin/com/github/jengelman/gradle/plugins/shadow/relocation/RelocatorsTest.kt
+++ src/test/kotlin/com/github/jengelman/gradle/plugins/shadow/relocation/RelocatorsTest.kt
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 
-class RelocatorRemapperTest {
+class RelocatorsTest {
   @ParameterizedTest
   @MethodSource("signaturePatternsProvider")
   fun relocateSignaturePatterns(input: String, expected: String) {
