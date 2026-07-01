f6115af7c6b5d5d258a4d9a970c9bd91c7e01ea7
Zac Sweers <pandanomic@gmail.com>
2026-07-01 16:13:04 -0400

    Add MinimizeSpec

---
+++ src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/tasks/MinimizeSpec.kt
@@ -0,0 +1,22 @@
+package com.github.jengelman.gradle.plugins.shadow.tasks
+
+import org.gradle.api.Action
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Nested
+
+/** Configures how [ShadowJar.minimize] removes unused code from the shadowed JAR. */
+public interface MinimizeSpec : DependencyFilter {
+  /**
+   * The tool used to minimize the shadowed JAR.
+   *
+   * Defaults to [MinimizeTool.DEPENDENCY_ANALYZER].
+   */
+  @get:Input public val tool: Property<MinimizeTool>
+
+  /** R8-specific minimization options. */
+  @get:Nested public val r8Spec: R8Spec
+
+  /** Use R8 to minimize the shadowed JAR and configure its options. */
+  public fun r8(action: Action<in R8Spec>)
+}
