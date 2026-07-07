379ca41c5a1229a5f754c984ddbd76a345cc8e9f
Goooler <wangzongler@gmail.com>
2026-07-07 17:59:31 +0800

    Remove redundant ShadowDsl from MinimizeSpec

--- src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/tasks/MinimizeSpec.kt
+++ src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/tasks/MinimizeSpec.kt
@@ -1,12 +1,10 @@
 package com.github.jengelman.gradle.plugins.shadow.tasks
 
-import com.github.jengelman.gradle.plugins.shadow.ShadowDsl
 import org.gradle.api.Action
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
 
 /** Configures how [ShadowJar.minimize] removes unused code from the shadowed JAR. */
-@ShadowDsl
 public interface MinimizeSpec : DependencyFilter {
   /**
    * The tool used to minimize the shadowed JAR.
