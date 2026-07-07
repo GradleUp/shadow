39435a7028c967433ff8afcbfaee8dbce4537873
Goooler <wangzongler@gmail.com>
2026-07-07 17:48:49 +0800

    Remove level

--- src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/ShadowDsl.kt
+++ src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/ShadowDsl.kt
@@ -6,7 +6,6 @@ package com.github.jengelman.gradle.plugins.shadow
 @Deprecated(
   message = "Use `ShadowDsl` instead. This will be removed in Shadow 10.",
   replaceWith = ReplaceWith("ShadowDsl", "com.github.jengelman.gradle.plugins.shadow.ShadowDsl"),
-  level = DeprecationLevel.HIDDEN,
 )
 @DslMarker
 public annotation class ShadowDslMarker
