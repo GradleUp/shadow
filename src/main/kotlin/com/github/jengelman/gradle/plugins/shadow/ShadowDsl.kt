3de62e09bf2a1bef0e639b7497c5dda823e9c987
Goooler <wangzongler@gmail.com>
2026-07-12 09:55:14 +0800

    Mark ERROR level for ShadowDslMarker

--- src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/ShadowDsl.kt
+++ src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/ShadowDsl.kt
@@ -6,6 +6,7 @@ package com.github.jengelman.gradle.plugins.shadow
 @Deprecated(
   message = "Use `ShadowDsl` instead. This will be removed in Shadow 10.",
   replaceWith = ReplaceWith("ShadowDsl", "com.github.jengelman.gradle.plugins.shadow.ShadowDsl"),
+  level = DeprecationLevel.ERROR,
 )
 @DslMarker
 public annotation class ShadowDslMarker
