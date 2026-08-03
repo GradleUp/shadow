e9d2900f14396d84f84b1455db496473b1466a48
Goooler <wangzongler@gmail.com>
2026-08-03 22:20:36 +0800

    Enhance DefaultDependencyFilterTest with Provider, Map, Regex, and Project notation tests

--- src/test/kotlin/com/github/jengelman/gradle/plugins/shadow/internal/DefaultDependencyFilterTest.kt
+++ src/test/kotlin/com/github/jengelman/gradle/plugins/shadow/internal/DefaultDependencyFilterTest.kt
@@ -1,53 +1,66 @@
 package com.github.jengelman.gradle.plugins.shadow.internal
 
 import assertk.assertThat
-import assertk.assertions.isFalse
+import assertk.assertions.isEqualTo
 import assertk.assertions.isTrue
 import com.github.jengelman.gradle.plugins.shadow.util.noOpDelegate
 import org.gradle.api.artifacts.ResolvedDependency
 import org.gradle.testfixtures.ProjectBuilder
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 class DefaultDependencyFilterTest {
   private val project = ProjectBuilder.builder().build()
   private val filter = DefaultDependencyFilter(project)
 
   @ParameterizedTest
-  @ValueSource(
-    strings = ["my:d", "m.*:d", "my:d:.*", "m.*:d:.*", "m.*:d.*:.*", ".*:d:.*", "my:d:1.0"]
-  )
-  fun matchesWildcardNotation(notation: String) {
+  @MethodSource("dependencyNotationProvider")
+  fun matchesDependencyNotation(
+    notation: String,
+    group: String,
+    name: String,
+    version: String,
+    expected: Boolean,
+  ) {
     val spec = filter.dependency(notation)
-    val dep = TestResolvedDependency("my", "d", "1.0")
+    val dep = TestResolvedDependency(group, name, version)
+
+    assertThat(spec.isSatisfiedBy(dep)).isEqualTo(expected)
+  }
+
+  @Test
+  fun matchesProviderDependencyNotation() {
+    val provider = project.provider { "foo:bar:1.0" }
+    val spec = filter.dependency(provider)
+    val dep = TestResolvedDependency("foo", "bar", "1.0")
 
     assertThat(spec.isSatisfiedBy(dep)).isTrue()
   }
 
   @Test
-  fun matchesExactDependency() {
-    val spec = filter.dependency("com.acme:foo:2.1.0")
+  fun matchesMapDependencyNotation() {
+    val mapNotation = mapOf("group" to "foo", "name" to "bar", "version" to "1.0")
+    val spec = filter.dependency(mapNotation)
+    val dep = TestResolvedDependency("foo", "bar", "1.0")
 
-    assertThat(spec.isSatisfiedBy(TestResolvedDependency("com.acme", "foo", "2.1.0"))).isTrue()
-    assertThat(spec.isSatisfiedBy(TestResolvedDependency("com.acme", "bar", "2.1.0"))).isFalse()
-    assertThat(spec.isSatisfiedBy(TestResolvedDependency("org.other", "foo", "2.1.0"))).isFalse()
+    assertThat(spec.isSatisfiedBy(dep)).isTrue()
   }
 
   @Test
-  fun matchesVersionWithPlusSpecialChar() {
-    val spec = filter.dependency("org.foo:bar:1.0.0+1")
+  fun matchesProjectNotation() {
+    val subproject = ProjectBuilder.builder().withName("subproject").build()
+    val projectDep = project.dependencies.project(mapOf("path" to subproject.path))
+    val spec = filter.project(subproject.path)
+    val dep =
+      TestResolvedDependency(
+        group = projectDep.group ?: "",
+        name = projectDep.name,
+        version = projectDep.version ?: "unspecified",
+      )
 
-    assertThat(spec.isSatisfiedBy(TestResolvedDependency("org.foo", "bar", "1.0.0+1"))).isTrue()
-    assertThat(spec.isSatisfiedBy(TestResolvedDependency("org.foo", "bar", "1.0.0+2"))).isFalse()
-  }
-
-  @Test
-  fun excludeFilterMatchesDependency() {
-    val excludeSpec = filter.dependency("my:b")
-
-    assertThat(excludeSpec.isSatisfiedBy(TestResolvedDependency("my", "a", "1.0"))).isFalse()
-    assertThat(excludeSpec.isSatisfiedBy(TestResolvedDependency("my", "b", "1.0"))).isTrue()
+    assertThat(spec.isSatisfiedBy(dep)).isTrue()
   }
 
   private class TestResolvedDependency(
@@ -64,5 +77,28 @@ class DefaultDependencyFilterTest {
     override fun getModuleVersion(): String = version
 
     override fun getConfiguration(): String = "default"
+  }
+
+  private companion object {
+    @JvmStatic
+    fun dependencyNotationProvider() =
+      listOf(
+        Arguments.of("foo:bar", "foo", "bar", "1.0", true),
+        Arguments.of("f.*:bar", "foo", "bar", "1.0", true),
+        Arguments.of("foo:bar:.*", "foo", "bar", "1.0", true),
+        Arguments.of("f.*:bar:.*", "foo", "bar", "1.0", true),
+        Arguments.of("f.*:bar.*:.*", "foo", "bar", "1.0", true),
+        Arguments.of(".*:bar:.*", "foo", "bar", "1.0", true),
+        Arguments.of("foo:bar:2.1.0", "foo", "bar", "2.1.0", true),
+        Arguments.of("foo:bar:2.1.0", "foo", "baz", "2.1.0", false),
+        Arguments.of("foo:bar:2.1.0", "bar", "bar", "2.1.0", false),
+        Arguments.of("foo:bar:1.0.0+1", "foo", "bar", "1.0.0+1", true),
+        Arguments.of("foo:bar:1.0.0+1", "foo", "bar", "1.0.0+2", false),
+        Arguments.of("foo:bar:1\\.0\\..*", "foo", "bar", "1.0.5", true),
+        Arguments.of("foo:bar:1\\.0\\..*", "foo", "bar", "2.0.0", false),
+        Arguments.of("foo:bar:1.0", "baz", "bar", "1.0", false),
+        Arguments.of("foo:bar:1.0", "foo", "bar", "2.0", false),
+        Arguments.of("f.*:bar", "zoo", "bar", "1.0", false),
+      )
   }
 }
