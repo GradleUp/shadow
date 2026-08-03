4f4bd9e711dd9b7d2a0002e376b0299def0d4eb2
Goooler <wangzongler@gmail.com>
2026-08-03 21:39:08 +0800

    Complete KotlinModuleMetadataTransformerTest assertions and add Issue 843 comments

--- src/test/kotlin/com/github/jengelman/gradle/plugins/shadow/transformers/KotlinModuleMetadataTransformerTest.kt
+++ src/test/kotlin/com/github/jengelman/gradle/plugins/shadow/transformers/KotlinModuleMetadataTransformerTest.kt
@@ -1,8 +1,10 @@
 package com.github.jengelman.gradle.plugins.shadow.transformers
 
 import assertk.assertThat
+import assertk.assertions.isEmpty
 import assertk.assertions.isEqualTo
 import assertk.assertions.isFalse
+import assertk.assertions.isNotEmpty
 import assertk.assertions.isNotEqualTo
 import assertk.assertions.isNotNull
 import assertk.assertions.isTrue
@@ -42,7 +44,7 @@ class KotlinModuleMetadataTransformerTest : BaseTransformerTest<KotlinModuleMeta
       assertThat(outputBytes).isEqualTo(originalBytes)
     }
 
-  @Test
+  @Test // #843
   fun transformWithRelocator() =
     with(transformer) {
       val modulePath = "META-INF/kotlin-stdlib.kotlin_module"
@@ -62,14 +64,43 @@ class KotlinModuleMetadataTransformerTest : BaseTransformerTest<KotlinModuleMeta
       val relocatedModule = KotlinModuleMetadata.read(relocatedBytes)
       assertThat(relocatedModule.version.toString()).isEqualTo(originalModule.version.toString())
 
+      // No implementation for writing the optionalAnnotationClasses property yet.
+      // https://github.com/JetBrains/kotlin/blob/81502985ae0a2f5b21e121ffc180c3f4dd467e17/libraries/kotlinx-metadata/jvm/src/kotlin/metadata/jvm/KotlinModuleMetadata.kt#L71
+      assertThat(relocatedModule.kmModule.optionalAnnotationClasses).isEmpty()
+
       val originalPkgParts = originalModule.kmModule.packageParts.entries
       val relocatedPkgParts = relocatedModule.kmModule.packageParts.entries
       assertThat(relocatedPkgParts).isNotEqualTo(originalPkgParts)
       assertThat(relocatedPkgParts.size).isEqualTo(originalPkgParts.size)
 
-      relocatedPkgParts.forEachIndexed { index, (relocatedPkg, _) ->
-        val (originalPkg, _) = originalPkgParts.elementAt(index)
+      relocatedPkgParts.forEachIndexed { index, (relocatedPkg, relocatedParts) ->
+        val (originalPkg, originalParts) = originalPkgParts.elementAt(index)
+        assertThat(relocatedPkg).isNotEqualTo(originalPkg)
         assertThat(relocatedPkg).isEqualTo(originalPkg.replace("kotlin", "my.kotlin"))
+
+        if (originalParts.fileFacades.isEmpty()) {
+          assertThat(relocatedParts.fileFacades).isEmpty()
+        } else {
+          assertThat(relocatedParts.fileFacades).isNotEmpty()
+          assertThat(relocatedParts.fileFacades).isNotEqualTo(originalParts.fileFacades)
+          assertThat(relocatedParts.fileFacades)
+            .isEqualTo(originalParts.fileFacades.map { it.replace("kotlin/", "my/kotlin/") })
+        }
+
+        if (originalParts.multiFileClassParts.isEmpty()) {
+          assertThat(relocatedParts.multiFileClassParts).isEmpty()
+        } else {
+          assertThat(relocatedParts.multiFileClassParts).isNotEmpty()
+          assertThat(relocatedParts.multiFileClassParts)
+            .isNotEqualTo(originalParts.multiFileClassParts)
+          assertThat(relocatedParts.multiFileClassParts)
+            .isEqualTo(
+              originalParts.multiFileClassParts.entries.associateTo(mutableMapOf()) { (name, facade)
+                ->
+                name.replace("kotlin/", "my/kotlin/") to facade.replace("kotlin/", "my/kotlin/")
+              }
+            )
+        }
       }
     }
 }
