1c228b84456296f56c5db35fc7e16c892c6b67fc
Goooler <wangzongler@gmail.com>
2026-08-13 12:03:56 +0800

    Revert "Use resolvable role for shadowR8 configuration"
    This reverts commit c1b1829e013659aabed4c572aacfc9493e53038b.

--- src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/ShadowBasePlugin.kt
+++ src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/ShadowBasePlugin.kt
@@ -25,8 +25,11 @@ public abstract class ShadowBasePlugin : Plugin<Project> {
       configurations.create(CONFIGURATION_NAME) {
         it.description = "Specify runtime dependencies that are not merged into the final JAR."
       }
-      configurations.resolvable(R8_CONFIGURATION_NAME) {
+      configurations.register(R8_CONFIGURATION_NAME) {
         it.description = "R8 executable used by ShadowJar R8 minimization."
+        it.isCanBeConsumed = false
+        // Defer the dependency resolving.
+        it.isCanBeResolved = false
         it.defaultDependencies { dependencies ->
           dependencies.add(project.dependencies.create(DEFAULT_R8_DEPENDENCY))
         }
