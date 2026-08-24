5f5e4c74f0b5cf85f3038a84a0605369422f0174
Goooler <wangzongler@gmail.com>
2026-08-24 12:52:12 +0800

    Enable org.gradle.tooling.parallel for tests

--- src/testKit/kotlin/com/github/jengelman/gradle/plugins/shadow/testkit/GradleRunner.kt
+++ src/testKit/kotlin/com/github/jengelman/gradle/plugins/shadow/testkit/GradleRunner.kt
@@ -42,6 +42,7 @@ val commonGradleArgs =
     "--stacktrace",
     "--warning-mode=fail",
     "-Dorg.gradle.kotlin.dsl.allWarningsAsErrors=true",
+    "-Dorg.gradle.tooling.parallel=true",
     // https://docs.gradle.org/current/userguide/configuration_cache.html#config_cache:usage:parallel
     "-Dorg.gradle.configuration-cache.parallel=true",
     // https://docs.gradle.org/current/userguide/isolated_projects.html#how_do_i_use_it
