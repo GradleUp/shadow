6d0db27a2be5ef23c162b3b624bab0035f0c16e0
Goooler <wangzongler@gmail.com>
2026-09-09 18:18:05 +0800

    Extract createDefaultLocalMavenRepository helper

--- src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/BasePluginTest.kt
+++ src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/BasePluginTest.kt
@@ -18,6 +18,7 @@ import com.github.jengelman.gradle.plugins.shadow.transformers.ResourceTransform
 import com.github.jengelman.gradle.plugins.shadow.util.AppendableMavenRepository
 import com.github.jengelman.gradle.plugins.shadow.util.JarBuilder
 import com.github.jengelman.gradle.plugins.shadow.util.JvmLang
+import com.github.jengelman.gradle.plugins.shadow.util.createDefaultLocalMavenRepository
 import java.io.Closeable
 import java.nio.file.Path
 import java.util.Properties
@@ -27,7 +28,6 @@ import kotlin.io.path.appendText
 import kotlin.io.path.createDirectories
 import kotlin.io.path.createDirectory
 import kotlin.io.path.createFile
-import kotlin.io.path.createTempDirectory
 import kotlin.io.path.deleteExisting
 import kotlin.io.path.deleteRecursively
 import kotlin.io.path.exists
@@ -78,55 +78,7 @@ abstract class BasePluginTest {
 
   @BeforeAll
   fun beforeAll() {
-    localRepo =
-      AppendableMavenRepository(
-          root = createTempDirectory().resolve("local-maven-repo").createDirectories()
-        )
-        .apply {
-          jarModule("junit", "junit", "3.8.2") { useJar(junitJar) }
-          val a =
-            jarModule("my", "a", "1.0") {
-              buildJar {
-                insert("a.properties", "a")
-                insert("a2.properties", "a2")
-              }
-            }
-          val b = jarModule("my", "b", "1.0") { buildJar { insert("b.properties", "b") } }
-          val c = jarModule("my", "c", "1.0") { buildJar { insert("c.properties", "c") } }
-          val d =
-            jarModule("my", "d", "1.0") {
-              buildJar { insert("d.properties", "d") }
-              // Depends on c but c does not depend on d.
-              addDependency(c)
-            }
-          val e =
-            jarModule("my", "e", "1.0") {
-              buildJar { insert("e.properties", "e") }
-              // Circular dependency with f.
-              addDependency("my:f:1.0")
-            }
-          val f =
-            jarModule("my", "f", "1.0") {
-              buildJar { insert("f.properties", "f") }
-              // Circular dependency with e.
-              addDependency(e)
-            }
-          val g =
-            jarModule("my", "g", "1.0") {
-              buildJar { insert("g.properties", "g") }
-              addDependency(pomModule("my", "pom-dep", "1.0"))
-            }
-          bomModule("my", "bom", "1.0") {
-            addDependency(a)
-            addDependency(b)
-            addDependency(c)
-            addDependency(d)
-            addDependency(e)
-            addDependency(f)
-            addDependency(g)
-          }
-        }
-    localRepo.publish()
+    localRepo = createDefaultLocalMavenRepository(junitJar).apply { publish() }
 
     artifactAJar = path("my/a/1.0/a-1.0.jar", parent = localRepo.root)
     artifactBJar = path("my/b/1.0/b-1.0.jar", parent = localRepo.root)
