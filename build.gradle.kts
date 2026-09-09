20fb09d39d66163107df33dc233f8135cd98df05
Goooler <wangzongler@gmail.com>
2026-09-09 14:20:06 +0800

    Call configurations.apiElements accessor

--- build.gradle.kts
+++ build.gradle.kts
@@ -108,7 +108,7 @@ publishing.publications.withType<MavenPublication>().configureEach {
   suppressPomMetadataWarningsFor(SOURCES_ELEMENTS_CONFIGURATION_NAME)
 }
 
-configurations.named(API_ELEMENTS_CONFIGURATION_NAME) {
+configurations.apiElements {
   attributes.attribute(
     // TODO: https://github.com/gradle/gradle/issues/24608
     GradlePluginApiVersion.GRADLE_PLUGIN_API_VERSION_ATTRIBUTE,
