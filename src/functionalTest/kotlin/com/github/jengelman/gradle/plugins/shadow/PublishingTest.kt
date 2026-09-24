1010dd7cef6296c2b6c23c32376ab0e9bf718403
Goooler <wangzongler@gmail.com>
2026-09-24 10:05:52 +0800

    Place shadow jar block after publishing block to cover more lazy cases

--- src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/PublishingTest.kt
+++ src/functionalTest/kotlin/com/github/jengelman/gradle/plugins/shadow/PublishingTest.kt
@@ -403,45 +403,6 @@ class PublishingTest : BasePluginTest() {
   }
 
   @Test
-  fun publishWithSourcesJarAndCustomClassifierAfterPublishingBlock() {
-    projectScript.appendText(
-      """
-      |apply plugin: 'maven-publish'
-      |java {
-      |  withSourcesJar()
-      |}
-      |publishing {
-      |  repositories {
-      |    maven { url = '${remoteRepoPath.toUri()}' }
-      |  }
-      |  publications {
-      |    shadow(MavenPublication) {
-      |      from components.shadow
-      |    }
-      |  }
-      |}
-      |tasks.named('shadowJar', com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar) {
-      |  archiveClassifier = 'shaded'
-      |  archiveSourcesFile = layout.buildDirectory.file('custom.jar')
-      |}
-      """
-        .trimMargin()
-    )
-
-    publish()
-
-    val artifactRoot = "my/maven/1.0"
-    assertThat(repoPath(artifactRoot).entries.filter { it.endsWith(".jar") })
-      .containsOnly(
-        "maven-1.0-shaded.jar",
-        "maven-1.0-shaded-sources.jar",
-      )
-    val gmm = gmmAdapter.fromJson(repoPath("$artifactRoot/maven-1.0.module"))
-    assertThat(gmm.shadowSourcesElementsVariant.fileNames.single())
-      .isEqualTo("maven-1.0-shaded-sources.jar")
-  }
-
-  @Test
   fun publishJavaComponentWithShadowAndSourcesVariants() {
     projectScript.appendText(
       publishConfiguration(
@@ -1064,10 +1025,11 @@ class PublishingTest : BasePluginTest() {
            |dependencies {
            |  $dependenciesBlock
            |}
+           |${publishingBlock(projectBlock = projectBlock, publicationsBlock = publicationsBlock)}
+           |// Place shadow jar block after publishing block to cover more lazy cases.
            |$shadowJarTask {
            |  $shadowBlock
            |}
-           |${publishingBlock(projectBlock = projectBlock, publicationsBlock = publicationsBlock)}
            |
            """
       .trimMargin()
